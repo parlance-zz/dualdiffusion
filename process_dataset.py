b2ded850efe4f6bcc3761d61dd2395a7b5b4fb93
Christopher Friesen <parlance@fifth-harmonic.com>
2024-01-31 16:16:24 -0700

    remove loudness norm, cleanup

--- process_dataset.py
+++ process_dataset.py
@@ -26,31 +26,24 @@ from dotenv import load_dotenv
 
 import torchaudio
 
-load_dotenv()
-
-FFMPEG_PATH = os.environ.get("FFMPEG_PATH")
-SOURCE_DIR = os.environ.get("DATASOURCE_PATH")
-SOURCE_FORMATS = [x.strip().lower() for x in os.environ.get("DATASOURCE_FORMATS").split(",")]
-OUTPUT_SAMPLE_DIR = os.environ.get("DATASET_PATH")
-OUTPUT_SAMPLE_FORMAT=os.environ.get('DATASET_FORMAT').lower()
-OUTPUT_SAMPLE_RATE = int(os.environ.get("DATASET_SAMPLE_RATE"))
-OUTPUT_NUM_CHANNELS = int(os.environ.get("DATASET_NUM_CHANNELS"))
-MINIMUM_SAMPLE_LENGTH = int(os.environ.get("DATASET_MINIMUM_SAMPLE_LENGTH"))
-MAXIMUM_SAMPLE_LENGTH = os.environ.get("DATASET_MAXIMUM_SAMPLE_LENGTH")
-
-RESUME_FROM = -1
-
 def decode_source_file(input_file, sample_num):
 
-    output_file = os.path.join(OUTPUT_SAMPLE_DIR, f"{sample_num}{OUTPUT_SAMPLE_FORMAT}")
+    input_dir, input_filename = os.path.split(input_file)
+    input_filename, _ = os.path.splitext(input_filename)
+    input_dir = os.path.basename(input_dir)
+    output_filename = f"{input_dir} - {input_filename}{OUTPUT_SAMPLE_FORMAT}"
+    output_file = os.path.join(OUTPUT_SAMPLE_DIR, output_filename)
     
+    if os.path.exists(output_file):
+        return False
+
     args = [FFMPEG_PATH,
             '-loglevel', 'error','-hide_banner',
             '-i', input_file,
             '-t', MAXIMUM_SAMPLE_LENGTH,
             '-ac', str(OUTPUT_NUM_CHANNELS),
             '-ar', str(OUTPUT_SAMPLE_RATE),
-            '-filter:a', 'loudnorm']
+            ]#'-filter:a', 'loudnorm']
     
     if OUTPUT_SAMPLE_FORMAT == '.raw': args += ['-f', 's16le']
     if OUTPUT_SAMPLE_FORMAT == '.flac': args += ['-sample_fmt', 's16']
@@ -67,18 +60,20 @@ def decode_source_file(input_file, sample_num):
         if OUTPUT_SAMPLE_FORMAT == '.raw':       
             output_file_length = os.path.getsize(output_file) // 2 // OUTPUT_NUM_CHANNELS
             if output_file_length < MINIMUM_SAMPLE_LENGTH:
-                print(f"Skipping '{input_file}' due to insufficient length ({output_file_length // OUTPUT_SAMPLE_RATE}s)")
+                print(f"Skipping '{input_file}' due to insufficient length")
                 os.remove(output_file)
                 return False
 
         else:
-            audio, _ = torchaudio.load(output_file)
-            if audio.shape[1] < MINIMUM_SAMPLE_LENGTH:
-                print(f"Skipping '{input_file}' due to insufficient length ({audio.shape[1] // OUTPUT_SAMPLE_RATE}s)")
+            #audio, _ = torchaudio.load(output_file)
+            #if audio.abs().amax() >= 0.99:
+            #    print("WARNING - CLIPPING in file:", output_file)
+            #    _ = input("Press enter to continue...")
+
+            if torchaudio.info(output_file).num_frames < MINIMUM_SAMPLE_LENGTH:
+                print(f"Skipping '{input_file}' due to insufficient length")
                 os.remove(output_file)
                 return False
-            else:
-                del audio
             
         return True
 
@@ -98,9 +93,8 @@ def decode_source_to_samples():
     total_processed = 0
     sample_num = 0
     for input_file in input_files:
-        if sample_num > RESUME_FROM:
-            processed = decode_source_file(input_file, sample_num)
-            total_processed += int(processed)
+        processed = decode_source_file(input_file, sample_num)
+        total_processed += int(processed)
         sample_num += 1
 
     print("")
@@ -110,16 +104,27 @@ def decode_source_to_samples():
 
 if __name__ == "__main__":
 
-    if os.path.exists(OUTPUT_SAMPLE_DIR):
-        print(f"Warning: Output folder already exists '{OUTPUT_SAMPLE_DIR}'")
-        if input("Resume processing? (y/n): ").lower() not in ["y","yes"]: exit()
-        
+    load_dotenv(override=True)
+
+    FFMPEG_PATH = os.environ.get("FFMPEG_PATH")
+    SOURCE_DIR = os.environ.get("DATASOURCE_PATH")
+    SOURCE_FORMATS = [x.strip().lower() for x in os.environ.get("DATASOURCE_FORMATS").split(",")]
+    OUTPUT_SAMPLE_DIR = os.environ.get("DATASET_PATH")
+    OUTPUT_SAMPLE_FORMAT=os.environ.get('DATASET_FORMAT').lower()
+    OUTPUT_SAMPLE_RATE = int(os.environ.get("DATASET_SAMPLE_RATE"))
+    OUTPUT_NUM_CHANNELS = int(os.environ.get("DATASET_NUM_CHANNELS"))
+    MINIMUM_SAMPLE_LENGTH = int(os.environ.get("DATASET_MINIMUM_SAMPLE_LENGTH"))
+    MAXIMUM_SAMPLE_LENGTH = os.environ.get("DATASET_MAXIMUM_SAMPLE_LENGTH")
+
+    RESUME_FROM = -1
+
+    if os.path.exists(OUTPUT_SAMPLE_DIR):        
         existing_files = os.listdir(OUTPUT_SAMPLE_DIR)
         if len(existing_files) > 0:
-            existing_files.sort()
-            RESUME_FROM = int(os.path.splitext(existing_files[-1])[0])
-            print(f"Resuming from sample num {RESUME_FROM}...")
 
+            print(f"Warning: Output folder already exists '{OUTPUT_SAMPLE_DIR}'")
+            if input("Resume processing? (y/n): ").lower() not in ["y","yes"]: exit()
     else:
         os.makedirs(OUTPUT_SAMPLE_DIR, exist_ok=True)
-        decode_source_to_samples()
+
+    decode_source_to_samples()
