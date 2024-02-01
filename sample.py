7e0d3923db5732061d49bbeb9a8468081c769f05
Christopher Friesen <parlance@fifth-harmonic.com>
2024-01-31 18:39:51 -0700

    cleaned up code

--- sample.py
+++ sample.py
@@ -26,260 +26,27 @@ from dotenv import load_dotenv
 
 import numpy as np
 import torch
-import json
 import datetime
 
-from dual_diffusion_pipeline import DualDiffusionPipeline, DualMCLTFormat
-from attention_processor_dual import SeparableAttnProcessor2_0
-from autoencoder_kl_dual import AutoencoderKLDual
-from dual_diffusion_utils import save_flac, save_raw, load_raw, load_flac
-
-def get_dataset_stats():
-    model_params = {
-        "sample_raw_length": 65536*2,
-        "num_chunks": 256,
-        "sample_format": "mcltbce",
-        #"complex": False,
-        #"u": 255,
-        #"sample_std": 1,
-    }
-    
-    format = DualDiffusionPipeline.get_sample_format(model_params)
-    crop_width = format.get_sample_crop_width(model_params)
-    
-    if format == DualMCLTFormat:
-        pos_examples = 0.
-        neg_examples = 0.
-    else:
-        sample_std = 0.
-    num_samples = 0
-    
-    dataset_path = os.environ.get("DATASET_PATH", "./dataset/samples")
-    sample_list = os.listdir(dataset_path)
-    for filename in sample_list:
-        if filename.endswith(".raw"):
-            raw_sample = np.fromfile(os.path.join(dataset_path, filename), dtype=np.int16, count=crop_width) / 32768.
-            raw_sample = torch.from_numpy(raw_sample.astype(np.float32)).unsqueeze(0).to("cuda")
-            
-            sample = format.raw_to_sample(raw_sample, model_params)
-
-            if format == DualMCLTFormat:
-                sample_abs = sample[:, 0, :, :]
-                sample_abs /= sample_abs.amax(dim=(1,2), keepdim=True)
-                pos_examples += sample_abs.sum().item()
-                neg_examples += (1-sample_abs).sum().item()
-            else:
-                sample_std += sample.std().item()
-
-            num_samples += 1
-            if num_samples % 100 == 0:
-                print(f"Processed {num_samples}/{len(sample_list)} samples")
-
-    if format == DualMCLTFormat:
-        print(f"pos_examples: {pos_examples}")
-        print(f"neg_examples: {neg_examples}")
-        print(f"total samples processed: {num_samples}")
-    else:
-        sample_std /= num_samples
-        print(f"sample_std: {sample_std}")
-        print(f"total samples processed: {num_samples}")
-        
-    exit()
-
-def get_embedding_response(query_embed, key_embed, exp_scale):
-    response = (query_embed * key_embed).sum(dim=(0))
-    response -= response.max()
-    ln_response = response.clone()
-    response = (response*exp_scale).exp()
-    return response / response.max(), ln_response
-
-def get_query(query_embed, weight):
-    return (weight.view(1, -1) * query_embed).sum(dim=1).view(-1, 1)
-
-def embedding_test():
-    base_n_channels = 128
-    freq_embedding_dim = 512
-    time_embedding_dim = 512
-    sample_resolution_freq = 256
-    sample_resolution_time = 256
-    freq_exp_scale = (base_n_channels + freq_embedding_dim)**-0.5 #* 0.5
-    time_exp_scale = (base_n_channels + time_embedding_dim)**-0.5 #* 0.5
-    sample_shape = (1, base_n_channels, sample_resolution_freq, sample_resolution_time)
-
-    embeddings = SeparableAttnProcessor2_0.get_embeddings(sample_shape, freq_embedding_dim, time_embedding_dim, dtype=torch.float32, device="cpu")
-    freq_embed = embeddings[0, :freq_embedding_dim,  :, 0]
-    time_embed = embeddings[0,  freq_embedding_dim:, 0, :]
-    
-    print("freq_embed_std: ", freq_embed.std().item(), "freq_embed_mean: ", freq_embed.mean().item())
-    print("time_embed_std: ", time_embed.std().item(), "time_embed_mean: ", time_embed.mean().item())
-    print("")
-
-    def g(dim, x, std):
-        x = torch.linspace(-1, 1, dim) - x
-        w = torch.exp(-0.5*(x/std)**2)
-        return w / w.square().sum() ** 0.5
-    
-    def lg(dim, x, std):
-        x = torch.linspace(0, 1, dim) / x
-        w = torch.exp(-0.5*(torch.log2(x)/std)**2)
-        return w / w.square().sum() ** 0.5
-    
-    #freq_test_weight = lg(sample_resolution_freq, 0.4, 0.05)
-    #freq_test_weight += lg(sample_resolution_freq, 0.2, 0.05)
-    #freq_test_weight += lg(sample_resolution_freq, 0.1, 0.05)
-
-    freq_test_weight  = lg(sample_resolution_freq, 0.1, 0.03)# /0.1
-    freq_test_weight += lg(sample_resolution_freq, 0.24, 0.03) #/ 0.24
-    freq_test_weight += lg(sample_resolution_freq, 0.63, 0.03)# / 0.63
-    #freq_test_weight /= torch.arange(0, len(freq_test_weight)) +1e-5#e-5 # + 1
-
-    freq_test_weight /= freq_test_weight.max()
-    freq_test_weight.cpu().numpy().tofile("./debug/debug_embed_freq_weight.raw")
-
-    freq_query = get_query(freq_embed, freq_test_weight)
-    freq_response, freq_ln_response = get_embedding_response(freq_query, freq_embed, freq_exp_scale)
-    freq_response.cpu().numpy().tofile("./debug/debug_embed_freq_response.raw")
-    
-    time_test_weight_std = 0.01#0.003
-    time_test_weight = g(sample_resolution_time, -0.5, time_test_weight_std)
-    time_test_weight += g(sample_resolution_time, -0.3, time_test_weight_std)
-    time_test_weight += g(sample_resolution_time, -0.1, time_test_weight_std)
-    time_test_weight += g(sample_resolution_time, 0., time_test_weight_std)
-    time_test_weight += g(sample_resolution_time, 0.3, time_test_weight_std)
-    time_test_weight /= time_test_weight.max()
-    time_test_weight.cpu().numpy().tofile("./debug/debug_embed_time_weight.raw")
-
-    time_query = get_query(time_embed, time_test_weight)
-    time_query.cpu().numpy().tofile("./debug/debug_embed_time_query.raw")
-    time_response, time_ln_response = get_embedding_response(time_query, time_embed, time_exp_scale)
-    time_response.cpu().numpy().tofile("./debug/debug_embed_time_response.raw")
-
-    freq_ln_test_weight = freq_test_weight.log()
-    freq_nan_mask = torch.isnan(freq_ln_test_weight).logical_or(torch.isinf(freq_ln_test_weight)).logical_or(torch.isneginf(freq_ln_test_weight))
-    freq_ln_response[freq_nan_mask] = 0
-    freq_ln_test_weight[freq_nan_mask] = 0
-
-    time_ln_test_weight = time_test_weight.log()
-    time_nan_mask = torch.isnan(time_ln_test_weight).logical_or(torch.isinf(time_ln_test_weight)).logical_or(torch.isneginf(time_ln_test_weight))
-    time_ln_response[time_nan_mask] = 0
-    time_ln_test_weight[time_nan_mask] = 0
-
-    print("freq response ln error:", (freq_ln_response - freq_ln_test_weight).mean().item())
-    print("time response ln error:", (time_ln_response - time_ln_test_weight).mean().item())
-    print("freq response mse error:", (freq_response - freq_test_weight).square().mean().item())
-    print("time response mse error:", (time_response - time_test_weight).square().mean().item())
-    exit()
-
-def vae_test():
-
-    model_name = "dualdiffusion2d_900_4"
-    num_samples = 1
-    device = "cuda"
-    #device = "cpu"
-    #fp16 = False
-    fp16 = True
-
-    model_path = os.path.join(os.environ.get("MODEL_PATH", "./"), model_name)
-    with open(os.path.join(model_path, "model_index.json"), "r") as f:
-        model_index = json.load(f)
-    model_params = model_index["model_params"]
-    sample_rate = model_params["sample_rate"]
-
-    # 44.1khz settings
-    model_params["num_chunks"] = 352 # best quality @ 44.1khz (multiply num_chunks by new_sample_rate / original_sample_rate)
-    model_params["sample_raw_length"] = 65536*32
-    sample_rate = 44100
-
-    output_path = os.path.join(model_path, "output")
-    os.makedirs(output_path, exist_ok=True)
-
-    format = DualDiffusionPipeline.get_sample_format(model_params)
-    crop_width = format.get_sample_crop_width(model_params)
-    print("Sample shape: ", format.get_sample_shape(model_params))
-
-    dataset_path = os.environ.get("DATASET_PATH", "./dataset/samples")
-    test_samples = np.random.choice(os.listdir(dataset_path), num_samples, replace=False)
-    
-    #test_samples = ["27705.raw"] # extremely heavy noise
-    #test_samples = ["23655.raw"] #som
-    #test_samples = ["33927.raw"]
-    #test_samples = ["29235.raw"] 
-
-    # try to use most recent checkpoint if one exists
-    vae_checkpoints = [f for f in os.listdir(model_path) if os.path.isdir(os.path.join(model_path, f)) and f.startswith("vae_checkpoint")]
-    if len(vae_checkpoints) > 0:
-        vae_checkpoints = sorted(vae_checkpoints, key=lambda x: int(x.split("-")[1]))
-        model_path = os.path.join(model_path, vae_checkpoints[-1])
-
-    vae_path = os.path.join(model_path, "vae")
-    model_dtype = torch.float16 if fp16 else torch.float32
-    vae = AutoencoderKLDual.from_pretrained(vae_path, torch_dtype=model_dtype).to(device)
-    last_global_step = vae.config["last_global_step"]
-
-    start_time = datetime.datetime.now()
-
-    for filename in test_samples:
-        #input_raw_sample = load_raw(os.path.join(dataset_path, filename), dtype=np.int16, count=crop_width)
-        #input_raw_sample = input_raw_sample.unsqueeze(0).to(device)
-
-        input_raw_sample = load_flac("./dataset/samples_hq/23654.flac", count=crop_width).to(device)
-        
-        input_sample = format.raw_to_sample(input_raw_sample, model_params)
-        posterior = vae.encode(input_sample.type(model_dtype), return_dict=False)[0]
-        #from autoencoder_kl_dual import DiagonalGaussianDistribution
-        #posterior = DiagonalGaussianDistribution(torch.zeros_like(posterior.parameters))
-        latents = posterior.sample()
-        output_sample = vae.decode(latents, return_dict=False)[0]
-        output_raw_sample = format.sample_to_raw(output_sample.type(torch.float32), model_params)
-
-        output_latents_file_path = os.path.join(output_path, f"step_{last_global_step}_{filename.replace('.raw', '_latents.raw')}")
-        save_raw(latents, output_latents_file_path)
-        output_input_sample_file_path = os.path.join(output_path, f"step_{last_global_step}_{filename.replace('.raw', '_input_sample.raw')}")
-        save_raw(input_sample, output_input_sample_file_path)
-        output_sample_file_path = os.path.join(output_path, f"step_{last_global_step}_{filename.replace('.raw', '_sample.raw')}")
-        save_raw(output_sample, output_sample_file_path)
-        output_posterior_file_path = os.path.join(output_path, f"step_{last_global_step}_{filename.replace('.raw', '_posterior.raw')}")
-        save_raw(posterior.parameters, output_posterior_file_path)
-
-        original_raw_sample = format.raw_to_sample(input_raw_sample, model_params, return_dict=True)["raw_samples"]
-        output_flac_file_path = os.path.join(output_path, f"step_{last_global_step}_{filename.replace('.raw', '_original.flac')}")
-        save_flac(original_raw_sample, sample_rate, output_flac_file_path)
-        print(f"Saved flac output to {output_flac_file_path}")
-
-        output_flac_file_path = os.path.join(output_path, f"step_{last_global_step}_{filename.replace('.raw', '_decoded.flac')}")
-        save_flac(output_raw_sample, sample_rate, output_flac_file_path)
-        print(f"Saved flac output to {output_flac_file_path}")
-
-    print(f"Finished in: {datetime.datetime.now() - start_time}")
-
-    exit()
+from dual_diffusion_pipeline import DualDiffusionPipeline
+from dual_diffusion_utils import init_cuda, save_audio
 
 if __name__ == "__main__":
 
-    if not torch.cuda.is_available():
-        print("Error: PyTorch not compiled with CUDA support or CUDA unavailable")
-        exit(1)
-    else:
-        torch.backends.cuda.matmul.allow_tf32 = True
-        torch.backends.cuda.cufft_plan_cache[0].max_size = 250 # stupid cufft memory leak
+    init_cuda()
+    load_dotenv(override=True)
 
-    load_dotenv()
+    model_name = "dualdiffusion2d_1000_1"
 
-    #get_dataset_stats()
-    #embedding_test()
-    vae_test()
-
-    model_name = "dualdiffusion2d_900_4"
-
-    num_samples = 4
+    num_samples = 1
     batch_size = 1
     length = 1
-    scheduler = "dpms++"     # generates nearly identical outputs to ddim, latent diffusion or not
-    #scheduler = "ddim"       # these 2 are probably the best overall
-    #scheduler = "kdpm2_a"     # wow! does waaayyy better with latent diffusion! probably still not as good as dpms++ or ddim
-    #scheduler = "euler_a"    # different, unclear if good
-    #scheduler = "dpms++_sde" # on par with dpms++ and ddim, but more unpredictable
-    steps = 550 #337 #250
+    scheduler = "dpms++"
+    #scheduler = "ddim"
+    #scheduler = "kdpm2_a"
+    #scheduler = "euler_a"
+    #scheduler = "dpms++_sde"
+    steps = 250
     loops = 0
     fp16 = False
     #fp16 = True
@@ -291,9 +58,12 @@ if __name__ == "__main__":
     model_path = os.path.join(os.environ.get("MODEL_PATH", "./"), model_name)
     print(f"Loading DualDiffusion model from '{model_path}' (dtype={model_dtype})...")
     pipeline = DualDiffusionPipeline.from_pretrained(model_path, torch_dtype=model_dtype).to("cuda")
+    last_global_step = pipeline.unet.config["last_global_step"]
 
-    sample_rate = pipeline.config["model_params"]["sample_rate"]
-    last_global_step = pipeline.unet.config.get("last_global_step", 0)
+    output_path = os.path.join(model_path, "output")
+    os.makedirs(output_path, exist_ok=True)
+
+    start_time = datetime.datetime.now()
 
     for i in range(num_samples):
         print(f"Generating sample {i+1}/{num_samples}...")
@@ -304,14 +74,13 @@ if __name__ == "__main__":
                           seed=seed,
                           loops=loops,
                           batch_size=batch_size,
-                          length=length).cpu()
+                          length=length)
         print(f"Time taken: {time.time()-start}")
 
-        output_path = os.path.join(model_path, "output")
-        os.makedirs(output_path, exist_ok=True)
-
         output_flac_file_path = os.path.join(output_path, f"step_{last_global_step}_{scheduler}{steps}_s{seed}.flac")
-        save_flac(output, sample_rate, output_flac_file_path)
+        save_audio(output, pipeline.config["model_params"]["sample_rate"], output_flac_file_path)
         print(f"Saved flac output to {output_flac_file_path}")
 
         seed += 1
+    
+    print(f"Finished in: {datetime.datetime.now() - start_time}")
