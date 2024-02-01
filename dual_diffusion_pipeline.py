205893671e75527332439c8f7946143f082d508f
Christopher Friesen <parlance@fifth-harmonic.com>
2024-01-31 18:04:40 -0700

    cleanup, revised mclt format with an extra channel for phase normalization, added tentative and optional direct / format loss

--- dual_diffusion_pipeline.py
+++ dual_diffusion_pipeline.py
@@ -133,7 +133,7 @@ class DualMCLTFormat:
     @staticmethod
     def get_num_channels(model_params):
         in_channels = model_params["sample_raw_channels"] * 2
-        out_channels = model_params["sample_raw_channels"] * 2
+        out_channels = model_params["sample_raw_channels"] * 3
 
         return (in_channels, out_channels)
 
@@ -145,23 +145,28 @@ class DualMCLTFormat:
         u = model_params["u"]
         block_width = model_params["num_chunks"] * 2
 
-        samples_mdct = mdct(raw_samples, block_width, window_degree=1)[..., 1:-2, :]
-        samples_mdct = samples_mdct.permute(0, 2, 1)
+        samples_mdct = mdct(raw_samples, block_width, window_degree=1)[:, :, 1:-2, :]
+        samples_mdct = samples_mdct.permute(0, 1, 3, 2)
 
         samples_mdct *= torch.exp(2j * torch.pi * torch.rand(1, device=samples_mdct.device))
         samples_mdct_abs = samples_mdct.abs()
-        samples_mdct_abs = (samples_mdct_abs / samples_mdct_abs.amax(dim=(1,2), keepdim=True).clip(min=1e-8)).clip(min=noise_floor)
+        samples_mdct_abs = (samples_mdct_abs / samples_mdct_abs.amax(dim=(1,2,3), keepdim=True).clip(min=1e-8)).clip(min=noise_floor)
         
-        samples_abs_ln = ((samples_mdct_abs * u).log1p() / np.log1p(u)).unsqueeze(1)
-        samples_qphase = (samples_mdct.angle().abs() / torch.pi).unsqueeze(1)
-        samples = torch.cat((samples_abs_ln, samples_qphase), dim=1)
+        samples_abs_ln = (samples_mdct_abs * u).log1p() / np.log1p(u) * 2 - 1
+        samples_qphase1 = samples_mdct.angle().abs() / torch.pi * 2 - 1
+        #samples_qphase2 = (1j*samples_mdct).angle().abs() / torch.pi * 2 - 1
+        #samples = torch.cat((samples_abs_ln, samples_qphase1, samples_qphase2), dim=1).requires_grad_(False)
+        samples = torch.cat((samples_abs_ln, samples_qphase1), dim=1).requires_grad_(False)
+
+        raw_samples = imdct(samples_mdct.permute(0, 1, 3, 2), window_degree=1).real.requires_grad_(False)
 
         if return_dict:
             samples_dict = {
                 "samples": samples,
-                "raw_samples": imdct(samples_mdct.permute(0, 2, 1), window_degree=1).real.requires_grad_(False),
-                #"samples_abs": samples_mdct_abs.requires_grad_(False),
-                #"samples_phase": samples_qphase.squeeze(1).requires_grad_(False),
+                "raw_samples": raw_samples,
+                "samples_abs_ln": samples_abs_ln.requires_grad_(False),
+                "samples_qphase1": samples_qphase1.requires_grad_(False),
+                #"samples_qphase2": samples_qphase2.requires_grad_(False),
             }
             return samples_dict
         else:
@@ -170,68 +175,57 @@ class DualMCLTFormat:
     @staticmethod
     def sample_to_raw(samples, model_params, return_dict=False):
         
-        noise_floor = model_params["noise_floor"]
+        u = model_params["u"]
+        num_channels = model_params["sample_raw_channels"]
 
-        samples_abs = samples[:, 0, :, :].sigmoid()
-        samples_qphase = samples[:, 1, :, :].sigmoid()
+        samples = samples.tanh()
 
-        samples_abs = (samples_abs.permute(0, 2, 1) / samples_abs.amax(dim=(1,2), keepdim=True).clip(min=1e-8)).clip(min=noise_floor)
-        samples_phase = (samples_qphase.permute(0, 2, 1) * torch.pi).cos()
-        raw_samples = imdct(samples_abs * samples_phase, window_degree=1).real
+        samples_abs_ln, samples_qphase1, samples_qphase2 = samples.chunk(num_channels, dim=1)        
+        #phase_norm = (samples_qphase1.abs() + samples_qphase2.abs()).clip(min=1e-10)
+        phase_norm = (samples_qphase1.abs() + (samples_qphase2+1)/2).clip(min=1e-10)
+        phase = (((samples_qphase1 / phase_norm) + 1) / 2 * torch.pi).cos()
+        abs = ((1 + u) ** (samples_abs_ln+1)/2 - 1) / u
+        raw_samples = imdct((abs * phase).permute(0, 2, 1), window_degree=1).real
 
         if not return_dict:
             return raw_samples
         else:
             samples_dict = {
-                #"samples_abs": samples_abs.permute(0, 2, 1),
-                #"samples_phase": samples_qphase,
+                "samples": samples,
                 "raw_samples": raw_samples,
+                "samples_abs_ln": samples_abs_ln,
+                "samples_qphase1": samples_qphase1,
+                #"samples_qphase2": samples_qphase2,
             }
             return samples_dict
 
     @staticmethod
     def get_loss(sample, target, model_params):
         
-        loss = torch.zeros(1, device=sample["raw_samples"].device)
-        return loss, loss
-
-        noise_floor = model_params["noise_floor"]
-        block_width = model_params["num_chunks"] * 2
         sample_rate = model_params["sample_rate"]
-        #u = model_params["u"]
+        num_chunks = model_params["num_chunks"]
 
-        samples_abs = sample["samples_abs"]
-        samples_phase = sample["samples_phase"]
-        target_abs = target["samples_abs"]
-        target_phase = target["samples_phase"]
+        samples_abs_ln = sample["samples_abs_ln"]
+        samples_qphase1 = sample["samples_qphase1"]
+        #samples_qphase2 = sample["samples_qphase2"]
 
-        block_hz = torch.arange(1, block_width//2+1, device=target_abs.device) * (sample_rate/2 / (block_width//2))
-        mel_density = get_mel_density(block_hz).requires_grad_(False).view(1, -1, 1).requires_grad_(False)
+        target_abs_ln = target["samples_abs_ln"]
+        target_qphase1 = target["samples_qphase1"]
+        #target_qphase2 = target["samples_qphase2"]
 
-        #samples_wave = samples_abs * (samples_phase * torch.pi).cos()
-        #target_wave = target_abs * (target_phase * torch.pi).cos()
-        #loss = (((samples_wave - target_wave).abs() * (u / 2)).log1p() / np.log1p(u) * mel_density).mean()
+        block_hz = torch.arange(1, num_chunks+1, device=target_abs_ln.device) * (sample_rate/2 / num_chunks)
+        mel_density = get_mel_density(block_hz).square().view(1, 1,-1, 1).requires_grad_(False)
+        mel_density /= mel_density.mean()
 
-        samples_wave = samples_abs * (samples_phase * torch.pi).cos()
-        target_wave = target_abs * (target_phase * torch.pi).cos()
+        imag_loss_mask = (target_abs_ln + 1) > ((target_abs_ln.amin(dim=1, keepdim=True) + 1) * 1.05)
+        real_loss = (samples_abs_ln - target_abs_ln).square() * 2 #4
+        #imag_loss = ((samples_qphase1 - target_qphase1).square() + (samples_qphase2 - target_qphase2).square()) * (imag_loss_mask * mel_density)
+        imag_loss = (samples_qphase1 - target_qphase1).square() * (imag_loss_mask * mel_density) 
 
-        #target_freq_pow = target_abs.square().mean(dim=1, keepdim=True)
-        #target_time_pow = target_abs.square().mean(dim=2, keepdim=True)
-        #target_pow = ((target_freq_pow * target_time_pow) ** (1/4)).clip(min=noise_floor*10)
+        #real_loss = real_loss.clip(min=1/32).log() - np.log(1/32) # i should try this with a non-log format
+        #imag_loss = imag_loss.clip(min=1/32).log() - np.log(1/32)
 
-        #target_mask = target_abs > noise_floor
-        #loss = ((samples_wave - target_wave).abs() / target_pow * mel_density * target_mask).mean() / 16
-
-        target_mask = target_abs > noise_floor
-        loss = ((samples_wave - target_wave).abs() / (samples_wave.abs().detach() + target_wave.abs() + noise_floor*10) * mel_density * target_mask).mean() / 8
-
-
-        #target_abs_ln = target_abs.log()
-        #target_abs_ln -= target_abs_ln.amin(dim=(1,2), keepdim=True)
-        #target_abs_ln /= -np.log(noise_floor)
-        #loss = ((samples_phase - target_phase).abs() * target_abs_ln * mel_density).mean()
-
-        return loss, torch.zeros_like(loss)
+        return real_loss, imag_loss
     
     @staticmethod
     def get_sample_shape(model_params, bsz=1, length=1):
@@ -241,8 +235,7 @@ class DualMCLTFormat:
         num_chunks = model_params["num_chunks"]
         chunk_len = crop_width // num_chunks - 2
 
-        return (bsz, num_output_channels, num_chunks, chunk_len*length,)   #2d
-        #return (bsz, num_output_channels * num_chunks, chunk_len*length,) #1d
+        return (bsz, num_output_channels, num_chunks, chunk_len*length,)
 
 class DualDiffusionPipeline(DiffusionPipeline):
 
@@ -286,18 +279,11 @@ class DualDiffusionPipeline(DiffusionPipeline):
         
     @staticmethod
     @torch.no_grad()
-    def create_new(model_params, unet_params, vae_params=None):
+    def create_new(model_params, unet_params, scheduler_params, vae_params=None):
         
-        beta_schedule = model_params["beta_schedule"]
-        if beta_schedule == "trained_betas":
+        if scheduler_params["beta_schedule"] == "trained_betas":
             raise NotImplementedError()
-
-        scheduler = DDIMScheduler(clip_sample_range=20.,
-                                  prediction_type=model_params["prediction_type"],
-                                  beta_schedule=beta_schedule,
-                                  beta_start=model_params["beta_start"],
-                                  beta_end=model_params["beta_end"],
-                                  rescale_betas_zero_snr=model_params["rescale_betas_zero_snr"],)
+        scheduler = DDIMScheduler(clip_sample_range=10., **scheduler_params)
         
         snr = compute_snr(scheduler, scheduler.timesteps)
         debug_path = os.environ.get("DEBUG_PATH", None)
@@ -312,15 +298,6 @@ class DualDiffusionPipeline(DiffusionPipeline):
             vae = None
 
         return DualDiffusionPipeline(unet, scheduler, vae, model_params=model_params)
-
-    @staticmethod
-    @torch.no_grad()
-    def add_embeddings(freq_samples, freq_embedding_dim, time_embedding_dim, format_hint="normal", pitch_augmentation=1., tempo_augmentation=1.):
-        raise NotImplementedError()
-    
-    @torch.no_grad()
-    def upscale(self, raw_sample):
-        raise NotImplementedError()
     
     @torch.no_grad()
     def __call__(
@@ -386,22 +363,8 @@ class DualDiffusionPipeline(DiffusionPipeline):
         model_params = self.config["model_params"]
         
         sample_shape = self.format.get_sample_shape(model_params, bsz=batch_size, length=length)
-        if getattr(self, "vae", None) is not None:
-
-            vae_latent_channels = self.vae.config.latent_channels
-            vae_downsample_ratio = self.vae.config.downsample_ratio
-            vae_num_blocks = len(self.vae.config.block_out_channels)
-
-            if len(sample_shape) == 4:
-                sample_shape = (sample_shape[0],
-                                vae_latent_channels,
-                                sample_shape[2] // vae_downsample_ratio[0] ** (vae_num_blocks-1),
-                                sample_shape[3] // vae_downsample_ratio[1] ** (vae_num_blocks-1))
-            else:
-                if isinstance(vae_downsample_ratio, tuple):
-                    vae_downsample_ratio = vae_downsample_ratio[0]
-                sample_shape = (sample_shape[0], vae_latent_channels, sample_shape[2] // vae_downsample_ratio ** (vae_num_blocks-1))
-
+        if getattr(self, "vae", None) is not None:    
+            sample_shape = self.vae.get_latent_shape(sample_shape)
         print(f"Sample shape: {sample_shape}")
 
         sample = torch.randn(sample_shape, device=self.device, dtype=self.unet.dtype, generator=generator)
@@ -429,12 +392,14 @@ class DualDiffusionPipeline(DiffusionPipeline):
             save_raw(sample, os.path.join(debug_path, "debug_sample.raw"))
         
         if getattr(self, "vae", None) is not None:
+            sample = sample - sample.mean(dim=(1,2,3), keepdim=True)
+            sample = sample / sample.std(dim=(1,2,3), keepdim=True).clip(min=1e-8)
             sample = self.vae.decode(sample).sample
             save_raw(sample, os.path.join(debug_path, "debug_decoded_sample.raw"))
 
         raw_sample = self.format.sample_to_raw(sample.float(), model_params)
-        raw_sample *= 0.18215 / raw_sample.std(dim=1, keepdim=True).clip(min=1e-8)
-        if loops > 0: raw_sample = raw_sample.repeat(1, loops+1)
+        raw_sample /= raw_sample.std(dim=(1,2), keepdim=True).clip(min=1e-8)
+        if loops > 0: raw_sample = raw_sample.repeat(2, loops+1)
         return raw_sample
     
     @torch.no_grad()
