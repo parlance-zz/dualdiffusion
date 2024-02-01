4b62276a44dbd07c8f48ae3dbb5da16637e2656c
Christopher Friesen <parlance@fifth-harmonic.com>
2024-01-31 23:17:31 -0700

    updated train script for 32khz

--- train_1000_1_4vae_256mclt.cmd
+++ train_1000_1_4vae_256mclt.cmd
@@ -4,15 +4,14 @@ accelerate launch^
  --mixed_precision="fp16"^
  --pretrained_model_name_or_path="D:/dualdiffusion/models/dualdiffusion2d_1000_1"^
  --module="vae"^
- --train_batch_size=4^
+ --train_batch_size=2^
  --num_train_epochs=5000^
  --gradient_accumulation_steps=4^
  --learning_rate=1e-5^
  --seed=400^
  --lr_scheduler="constant_with_warmup"^
  --max_grad_norm=25^
- --dataloader_num_workers=4^
- --max_train_samples=1000
+ --dataloader_num_workers=4
 
 REM --gradient_checkpointing
 REM --use_ema
