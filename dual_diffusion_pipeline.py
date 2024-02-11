e58986ea69f46d8dc759ed2ca78a1e2b802af5da
parlance-zz <parlance@fifth-harmonic.com>
2024-02-11 10:58:19 -0700

    add matrix stereo transform for mclt format, revised and dramatically improved pseudo-gan direct loss

--- dual_diffusion_pipeline.py
+++ dual_diffusion_pipeline.py
@@ -33,7 +33,7 @@ from diffusers.pipelines.pipeline_utils import DiffusionPipeline
 
 from unet_dual import UNetDualModel
 from autoencoder_kl_dual import AutoencoderKLDual
-from dual_diffusion_utils import compute_snr, mdct, imdct, save_raw, get_mel_density, MSPSD
+from dual_diffusion_utils import compute_snr, mdct, imdct, save_raw, get_mel_density, MSPSD, get_hann_window
 
 class DualMSPSDFormat:
 
@@ -141,6 +141,15 @@ class DualMCLTFormat:
         return (in_channels, out_channels)
 
     @staticmethod
+    def multichannel_transform(wave):
+        if wave.shape[1] == 1:
+            return wave
+        elif wave.shape[1] == 2:
+            return torch.stack((wave[:, 0] + wave[:, 1], wave[:, 0] - wave[:, 1]), dim=1) / (2 ** 0.5)
+        else: # we would need to do a (ortho normalized) dct/idct over the channel dim here
+            raise NotImplementedError("Multichannel transform not implemented for > 2 channels")
+
+    @staticmethod
     @torch.no_grad()
     def raw_to_sample(raw_samples, model_params, return_dict=False):
         
@@ -151,22 +160,23 @@ class DualMCLTFormat:
         samples_mdct = mdct(raw_samples, block_width, window_degree=1)[:, :, 1:-2, :]
         samples_mdct = samples_mdct.permute(0, 1, 3, 2)
         samples_mdct *= torch.exp(2j * torch.pi * torch.rand(1, device=samples_mdct.device))
+        samples_mdct = DualMCLTFormat.multichannel_transform(samples_mdct)
 
         samples_mdct_abs = samples_mdct.abs()
         samples_mdct_abs_amax = samples_mdct_abs.amax(dim=(1,2,3), keepdim=True).clip(min=1e-5)
         samples_mdct_abs = (samples_mdct_abs / samples_mdct_abs_amax).clip(min=noise_floor)
         samples_abs_ln = (samples_mdct_abs * u).log1p() / np.log1p(u)
         samples_qphase1 = samples_mdct.angle().abs() / torch.pi
-
         samples = torch.cat((samples_abs_ln, samples_qphase1), dim=1)
 
         samples_mdct /= samples_mdct_abs_amax
-        raw_samples = imdct(samples_mdct.permute(0, 1, 3, 2), window_degree=1).real.requires_grad_(False)
+        raw_samples = imdct(DualMCLTFormat.multichannel_transform(samples_mdct).permute(0, 1, 3, 2), window_degree=1).real.requires_grad_(False)
 
         if return_dict:
             samples_dict = {
                 "samples": samples,
                 "raw_samples": raw_samples,
+                "samples_wave": samples_mdct.real,
             }
             return samples_dict
         else:
@@ -180,33 +190,25 @@ class DualMCLTFormat:
 
         samples = samples.sigmoid()
         samples_abs, samples_phase1 = samples.chunk(2, dim=1)
-        samples_abs = (((1 + u) ** samples_abs - 1) / u).clip(min=noise_floor)
-        samples_phase = (samples_phase1 * torch.pi).cos()
-        raw_samples = imdct((samples_abs * samples_phase).permute(0, 1, 3, 2), window_degree=1).real
 
-        if original_samples_dict is not None:
-            orig_samples_abs, orig_samples_phase1 = original_samples_dict["samples"].chunk(2, dim=1)
-            orig_samples_abs = ((1 + u) ** orig_samples_abs - 1) / u
-            orig_samples_phase = (orig_samples_phase1 * torch.pi).cos()
+        # phase normalization, probably not needed
+        #samples_phase1 = samples_phase1 * 2 - 1
+        #samples_phase1 = samples_phase1 / samples_phase1.abs().amax(dim=(2), keepdim=True).clip(min=1e-5)
+        #samples_phase1 = (samples_phase1 + 1) / 2
 
-            raw_samples_orig_phase = imdct((samples_abs * orig_samples_phase).permute(0, 1, 3, 2), window_degree=1).real
-            raw_samples_orig_abs = imdct((orig_samples_abs * samples_phase).permute(0, 1, 3, 2), window_degree=1).real
-        else:
-            raw_samples_orig_phase = None
-            raw_samples_orig_abs = None
-            #samples_phase = samples_phase1 * 2 - 1
-            #samples_phase = ((samples_phase / samples_phase.abs().amax(dim=2, keepdim=True))+1) / 2
-            #samples_phase = (samples_phase * torch.pi).cos()
-            #raw_samples = imdct((samples_abs * samples_phase).permute(0, 1, 3, 2), window_degree=1).real
+        _samples_abs = (((1 + u) ** samples_abs - 1) / u).clip(min=noise_floor)
+        _samples_phase = (samples_phase1 * torch.pi).cos()
+        samples_wave = _samples_abs * _samples_phase
+        _samples_wave = DualMCLTFormat.multichannel_transform(samples_wave)
+        raw_samples = imdct(_samples_wave.permute(0, 1, 3, 2), window_degree=1).real
 
-        if not return_dict:            
+        if not return_dict:         
             return raw_samples
         else:
             samples_dict = {
                 "samples": samples,
                 "raw_samples": raw_samples,
-                "raw_samples_orig_phase": raw_samples_orig_phase,
-                "raw_samples_orig_abs": raw_samples_orig_abs,
+                "samples_wave": samples_wave,
             }
             return samples_dict
 
@@ -214,21 +216,32 @@ class DualMCLTFormat:
     def get_loss(sample, target, model_params):
         
         sample_rate = model_params["sample_rate"]
-        num_chunks = model_params["num_chunks"]
+        noise_floor = model_params["noise_floor"]
 
-        samples_abs = sample["samples_abs"]
-        samples_phase = sample["samples_phase"]
-        target_abs = target["samples_abs"]
-        target_phase = target["samples_phase"]
+        samples_wave = sample["samples_wave"]
+        target_wave = target["samples_wave"]
 
-        block_hz = torch.arange(1, num_chunks+1, device=target_abs.device) * (sample_rate/2 / num_chunks)
-        mel_density = get_mel_density(block_hz).view(1, 1,-1, 1).requires_grad_(False)
+        block_size = (8, 8); stride = (2, 2)
+        window = get_hann_window(block_size[0], device=samples_wave.device).sqrt()
+        window = (window.view(1, -1) * window.view(-1, 1)).view(1, 1, block_size[0], block_size[1])
+        samples_wave_unfolded = samples_wave.unfold(2, block_size[0], stride[0]).unfold(3, block_size[1], stride[1])
+        target_wave_unfolded = target_wave.unfold(2, block_size[0], stride[0]).unfold(3, block_size[1], stride[1])
+        samples_fft = torch.fft.rfft2(samples_wave_unfolded * window, norm="ortho")
+        target_fft = torch.fft.rfft2(target_wave_unfolded * window, norm="ortho")
+
+        block_hz = torch.linspace(0, sample_rate/2, samples_fft.shape[2], device=samples_fft.device)
+        mel_density = get_mel_density(block_hz).view(1, 1,-1, 1, 1, 1).requires_grad_(False)
         mel_density /= mel_density.mean()
 
-        real_loss = (samples_abs - target_abs).abs()
-        imag_loss = (samples_phase - target_phase).abs() * mel_density
+        error_real = (samples_fft.abs().clip(min=noise_floor) / target_fft.abs().clip(min=noise_floor)).log()
+        real_loss = (error_real.abs() * mel_density).mean()
 
-        return real_loss.square().mean(), imag_loss.square().mean()
+        error_imag = (samples_fft.angle() - target_fft.angle()).abs()
+        error_imag_wrap_mask = (error_imag > torch.pi).detach().requires_grad_(False)
+        error_imag[error_imag_wrap_mask] = 2*torch.pi - error_imag[error_imag_wrap_mask]
+        imag_loss = (error_imag * mel_density).mean()
+
+        return real_loss, imag_loss
     
     @staticmethod
     def get_sample_shape(model_params, bsz=1, length=0):
