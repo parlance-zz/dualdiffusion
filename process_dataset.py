b7a48ac8fca3fc2a4903b48b9b5e78eaa47ed29d
Christopher Friesen <parlance@fifth-harmonic.com>
2024-02-18 20:57:55 -0700

    add code to push dataset to hf hub

--- process_dataset.py
+++ process_dataset.py
@@ -24,6 +24,7 @@ import os
 import subprocess
 from dotenv import load_dotenv
 
+from huggingface_hub import HfApi
 import torchaudio
 
 def decode_source_file(input_file, sample_num):
@@ -128,3 +129,8 @@ if __name__ == "__main__":
         os.makedirs(OUTPUT_SAMPLE_DIR, exist_ok=True)
 
     decode_source_to_samples()
+    
+    # push to huggingface hub
+    #api = HfApi()
+    #api.upload_folder(folder_path=OUTPUT_SAMPLE_DIR, repo_id="parlance/spc_audio", repo_type="dataset", 
+    #                  multi_commits=True, multi_commits_verbose=True)
