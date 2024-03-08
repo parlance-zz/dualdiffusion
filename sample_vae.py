8f25bcd972154e8a1afd839e5176566945ef21ea
Christopher Friesen <parlance@fifth-harmonic.com>
2024-03-08 14:31:05 -0700

    small changes for spectrogram format

--- sample_vae.py
+++ sample_vae.py
@@ -28,7 +28,7 @@ import numpy as np
 import torch
 
 from dual_diffusion_pipeline import DualDiffusionPipeline
-from dual_diffusion_utils import init_cuda, save_audio, save_raw, load_raw, load_audio, save_raw_img
+from dual_diffusion_utils import init_cuda, save_audio, save_raw, load_raw, load_audio, save_raw_img, quantize_tensor
         
 if __name__ == "__main__":
 
@@ -36,12 +36,12 @@ if __name__ == "__main__":
     load_dotenv(override=True)
     #np.random.seed(0)
 
-    model_name = "dualdiffusion2d_1000_20"
+    model_name = "dualdiffusion2d_1000_21"
     num_samples = 1
-    #device = "cuda"
-    device = "cpu"
-    fp16 = False
-    #fp16 = True
+    device = "cuda"
+    #device = "cpu"
+    #fp16 = False
+    fp16 = True
     start = 0
     length = 720896
     #save_output = False
@@ -49,9 +49,10 @@ if __name__ == "__main__":
     sample_latents = False
     #sample_latents = True
     #normalize_latents = False
-    normalize_latents = True
+    normalize_latents = False
     quantize_latents = 0
     #quantize_latents = 6
+    add_latent_noise = 0
 
     model_path = os.path.join(os.environ.get("MODEL_PATH", "./"), model_name)
     model_dtype = torch.float16 if fp16 else torch.float32
@@ -61,7 +62,7 @@ if __name__ == "__main__":
                                                      load_latest_checkpoints=True,
                                                      device=device)
     model_params = pipeline.config["model_params"]
-    crop_width = pipeline.format.get_sample_crop_width(model_params, length=length)
+    crop_width = pipeline.format.get_sample_crop_width(length=length)
     vae = pipeline.vae.to(device)
     last_global_step = vae.config["last_global_step"]
 
@@ -71,8 +72,8 @@ if __name__ == "__main__":
     test_samples = np.random.choice(os.listdir(dataset_path), num_samples, replace=False)
 
     #"""
-    test_samples = []
-    test_samples += ["Star Fox - 141 Training Mode.flac"] # good bass test
+    #test_samples = []
+    #test_samples += ["Star Fox - 141 Training Mode.flac"] # good bass test
     #test_samples += ["Final Fantasy VI - 217 Mog.flac"] # good bass test
     #test_samples += ["Vortex - 10 Magmemo.flac"]  # good stereo test
     #test_samples += ["Mega Man X3 - 09 Blast Hornet.flac"] # messy mix and stereo test
@@ -86,7 +87,7 @@ if __name__ == "__main__":
     #test_samples += ["Pilotwings - 04 Light Plane.flac"]
     #"""
     
-    sample_shape = pipeline.format.get_sample_shape(model_params, length=length)
+    sample_shape = pipeline.format.get_sample_shape(length=length)
     print(f"Sample shape: {sample_shape}  Latent shape: {vae.get_latent_shape(sample_shape)}")
     
     output_path = os.path.join(model_path, "output")
@@ -104,7 +105,7 @@ if __name__ == "__main__":
             input_raw_sample = load_audio(os.path.join(dataset_path, filename), start=start, count=crop_width)
         input_raw_sample = input_raw_sample.unsqueeze(0).to(device)
 
-        input_sample_dict = pipeline.format.raw_to_sample(input_raw_sample, model_params, return_dict=True)
+        input_sample_dict = pipeline.format.raw_to_sample(input_raw_sample, return_dict=True)
         input_sample = input_sample_dict["samples"]
 
         posterior = vae.encode(input_sample.type(model_dtype), return_dict=False)[0]
@@ -116,23 +117,27 @@ if __name__ == "__main__":
             latents = (latents - latents.mean()) / latents.std()
             latents = latents * model_params["latent_std"] + model_params["latent_mean"]
         if quantize_latents > 0:
-            latents = (latents * quantize_latents).round() / quantize_latents
+            latents = quantize_tensor(latents, quantize_latents)
+        if add_latent_noise > 0:
+            latents += torch.randn_like(latents) * add_latent_noise * latents.std()
         model_output = vae.decode(latents, return_dict=False)[0]
 
-        output_sample_dict = pipeline.format.sample_to_raw(model_output.type(torch.float32), model_params, return_dict=True)
+        output_sample_dict = pipeline.format.sample_to_raw(model_output.type(torch.float32), return_dict=True)
         output_raw_sample = output_sample_dict["raw_samples"]
         output_sample = output_sample_dict["samples"]
 
-        point_similarity += (output_raw_sample - input_sample_dict["raw_samples"]).abs().mean().item()
+        point_similarity += (output_sample - input_sample_dict["samples"]).abs().mean().item()
         latents_mean += latents.mean().item()
         latents_std += latents.std().item()
 
         if save_output:
             save_raw(latents, os.path.join(output_path,f"step_{last_global_step}_{filename.replace(file_ext, '_latents.raw')}"))
-            save_raw(input_sample, os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_input_sample.raw')}"))
-            save_raw(output_sample, os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_sample.raw')}"))
-            save_raw(posterior.parameters, os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_posterior.raw')}"))
             save_raw_img(latents, os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_latents.png')}"))
+            save_raw(input_sample, os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_input_sample.raw')}"))
+            save_raw_img(input_sample_dict["samples"], os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_input_sample.png')}"))
+            save_raw(output_sample, os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_sample.raw')}"))
+            save_raw_img(output_sample, os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_sample.png')}"))
+            save_raw(posterior.parameters, os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_posterior.raw')}"))
 
             output_flac_file_path = os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_original.flac')}")
             save_audio(input_sample_dict["raw_samples"].squeeze(0), model_params["sample_rate"], output_flac_file_path)
