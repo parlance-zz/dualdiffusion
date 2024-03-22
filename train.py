849dbec1b9a64123e34848f4098e44086bf85b71
Christopher Friesen <parlance@fifth-harmonic.com>
2024-03-22 11:01:07 -0600

    fix mistake in channel kl loss calculation

--- train.py
+++ train.py
@@ -956,8 +956,8 @@ def do_training_loop(args,
                     kl_loss = posterior.kl()
 
                     latents_channel_mean = latents.mean(dim=(2,3))
-                    latents_channel_std = latents.std(dim=(2,3)).clip(min=1e-5)
-                    channel_kl_loss = (latents_channel_mean.square() + latents_channel_std - 1 - latents_channel_std.log()).mean()
+                    latents_channel_var = latents.std(dim=(2,3)).clip(min=1e-5).square()
+                    channel_kl_loss = (latents_channel_mean.square() + latents_channel_var - 1 - latents_channel_var.log()).mean()
                     
                     loss = real_nll_loss + imag_nll_loss + kl_loss * kl_loss_weight + channel_kl_loss * channel_kl_loss_weight + point_similarity * point_loss_weight
                 else:
