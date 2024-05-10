9e802b86db2f047729729aa85344a33e296d7f67
Christopher Friesen <parlance@fifth-harmonic.com>
2024-05-10 14:17:35 -0600

    add histogram plots for comparison, add theta formulation - surprisingly similar to log normal

--- log_normal_sampling_test.py
+++ log_normal_sampling_test.py
@@ -1,15 +1,21 @@
 import torch
 import numpy as np
 
-from dual_diffusion_utils import get_expected_max_normal
+from dual_diffusion_utils import get_expected_max_normal, multi_plot
 
 total_batch_size = 132
 reference_batch_size = 2048
 
+target_snr = 32
+sigma_max = 80
+sigma_min = 0.5 / target_snr
+
 P_std = 1.
 P_mean = -0.4
-mean_correction_amount = 0.25
+mean_correction_amount = 1 # best match overall with 1
 use_stratified_sampling = True
+use_theta_formulation = True#False
+n_iter = 1000
 
 print("total_batch_size:", total_batch_size, "reference_batch_size:", reference_batch_size)
 print("")
@@ -36,20 +42,35 @@ print("")
 mean_error = min_error = max_error = 0
 avg_batch_mean = avg_batch_min = avg_batch_max = 0
 avg_reference_mean = avg_reference_min = avg_reference_max = 0
-n_iter = 10000
+
+batch_sigma_histo = torch.zeros(100)
+reference_sigma_histo = torch.zeros(100)
+theta_min = np.arctan(1/sigma_max); theta_max = np.arctan(1/sigma_min)
 
 for i in range(n_iter):
 
     if use_stratified_sampling:
+
         batch_normal = (torch.arange(total_batch_size) + 0.5) / total_batch_size
         batch_normal += (torch.rand(1) - 0.5) / total_batch_size
-        batch_normal = P_corrected_mean + (P_corrected_std * (2 ** 0.5)) * (batch_normal * 2 - 1).erfinv().clip(min=-5, max=5)
+
+        if use_theta_formulation:
+            batch_normal = batch_normal * (theta_max - theta_min) + theta_min
+        else:
+            batch_normal = P_corrected_mean + (P_corrected_std * (2 ** 0.5)) * (batch_normal * 2 - 1).erfinv().clip(min=-5, max=5)
     else:
-        batch_normal = torch.randn(total_batch_size) * P_corrected_std + P_corrected_mean
-    batch_sigma = batch_normal.exp()
+        if use_theta_formulation:
+            batch_normal = torch.rand(total_batch_size) * (theta_max - theta_min) + theta_min
+        else:
+            batch_normal = torch.randn(total_batch_size) * P_corrected_std + P_corrected_mean
+
+    if use_theta_formulation:
+        batch_sigma = 1/batch_normal.tan()
+    else:
+        batch_sigma = batch_normal.exp().clip(min=sigma_min, max=sigma_max)
 
     reference_normal = torch.randn(reference_batch_size)
-    reference_sigma = (reference_normal * P_std + P_mean).exp()
+    reference_sigma = (reference_normal * P_std + P_mean).exp().clip(min=sigma_min, max=sigma_max)
 
     mean_error += (batch_sigma.mean() - reference_sigma.mean()).item()
     min_error  += (batch_sigma.amin() - reference_sigma.amin()).item()
@@ -62,6 +83,9 @@ for i in range(n_iter):
     avg_reference_min  += reference_sigma.amin().item()
     avg_reference_max  += reference_sigma.amax().item()
 
+    batch_sigma_histo += batch_sigma.histc(bins=100, min=sigma_min, max=sigma_max)
+    reference_sigma_histo += reference_sigma.histc(bins=100, min=sigma_min, max=sigma_max)
+
 print(f"mean error: {mean_error / n_iter:{5}f}")
 print(f"min  error: {min_error / n_iter:{5}f}")
 print(f"max  error: {max_error / n_iter:{5}f}")
@@ -69,3 +93,7 @@ print(f"max  error: {max_error / n_iter:{5}f}")
 print("")
 print(f"avg batch     mean: {avg_batch_mean / n_iter:{5}f}, min: {avg_batch_min / n_iter:{5}f}, max: {avg_batch_max / n_iter:{5}f}")
 print(f"avg reference mean: {avg_reference_mean / n_iter:{5}f}, min: {avg_reference_min / n_iter:{5}f}, max: {avg_reference_max / n_iter:{5}f}")
+
+batch_sigma_histo /= total_batch_size
+reference_sigma_histo /= reference_batch_size
+multi_plot((batch_sigma_histo, "batch sigma"), (reference_sigma_histo, "reference sigma"), x_log_scale=True, y_log_scale=True, x_axis_range=(sigma_min, sigma_max))
