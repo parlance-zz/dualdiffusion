b4b0bd8755057d3edad1bc3881b34b7bf0dd8f12
parlance-zz <parlance@fifth-harmonic.com>
2024-05-11 17:53:15 -0600

    add automatic retry for HF upload

--- process_dataset_hf_upload.py
+++ process_dataset_hf_upload.py
@@ -1,20 +1,31 @@
 import os
 from dotenv import load_dotenv
 from huggingface_hub import HfApi
+import time
 
-TARGET_HF_REPOSITORY = "parlance/spc_audio"
+TARGET_HF_REPOSITORY = "parlance/spc_audio_latents"
+#OUTPUT_SAMPLE_DIR = os.environ.get("DATASET_PATH")
+OUTPUT_SAMPLE_DIR = os.environ.get("LATENTS_DATASET_PATH")
 
 if __name__ == "__main__":
 
     load_dotenv(override=True)
-
-    OUTPUT_SAMPLE_DIR = os.environ.get("DATASET_PATH")
 
     if not os.path.exists(OUTPUT_SAMPLE_DIR):        
         print(f"Dataset path {OUTPUT_SAMPLE_DIR} does not exist")
         exit(1)
     
     # push to huggingface hub
-    api = HfApi()
-    api.upload_folder(folder_path=OUTPUT_SAMPLE_DIR, repo_id=TARGET_HF_REPOSITORY, repo_type="dataset", 
-                      multi_commits=True, multi_commits_verbose=True)
+    while True:
+        try:
+            api = HfApi()
+            api.upload_folder(folder_path=OUTPUT_SAMPLE_DIR, repo_id=TARGET_HF_REPOSITORY, repo_type="dataset", 
+                            multi_commits=True, multi_commits_verbose=True)
+        except Exception as e:
+            print(f"Error: {e}")
+            time.sleep(15)
+            continue
+
+        break
+
+    print(f"Uploaded dataset to {TARGET_HF_REPOSITORY} successfully")
