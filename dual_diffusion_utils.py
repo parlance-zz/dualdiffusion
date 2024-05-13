d765b890d951d1ac0d501c44ea0d06c0d523d8c8
parlance-zz <parlance@fifth-harmonic.com>
2024-05-13 10:59:30 -0600

    add slerp, get_cos_angle and normalize (moved from geodesic_flow)

--- dual_diffusion_utils.py
+++ dual_diffusion_utils.py
@@ -665,6 +665,39 @@ def save_raw_img(x, img_path, allow_inversion=False, allow_colormap=True, allow_
 
     return cv2_img
 
+def slerp(start, end, t, dtype=torch.float64):
+
+    if torch.is_tensor(t):
+        t = t.to(dtype)
+        if t.ndim < start.ndim:
+            t = t.view(*t.shape, *((1,) * (start.ndim - t.ndim)))
+    
+    start, end = start.to(dtype), end.to(dtype)
+    omega = get_cos_angle(start, end, keepdim=True, dtype=dtype)
+    so = torch.sin(omega)
+
+    return (torch.sin((1 - t) * omega) / so) * start + (torch.sin(t * omega) / so) * end
+
+def get_cos_angle(start, end, keepdim=False, dtype=torch.float64):
+    
+    reduction_dims = tuple(range(1, start.ndim)) if start.ndim > 1 else (0,)
+
+    start, end = start.to(dtype), end.to(dtype)
+    start_len = torch.linalg.vector_norm(start, dim=reduction_dims, keepdim=True, dtype=dtype)
+    end_len = torch.linalg.vector_norm(end, dim=reduction_dims, keepdim=True, dtype=dtype)
+
+    return (start / start_len * end / end_len).sum(dim=reduction_dims, keepdim=keepdim).clamp(-1, 1).acos()
+
+def normalize(x, zero_mean=False, dtype=torch.float64):
+
+    reduction_dims = tuple(range(1, x.ndim)) if x.ndim > 1 else (0,)
+    x = x.to(dtype)
+
+    if zero_mean:
+        x = x - x.mean(dim=reduction_dims, keepdim=True)
+
+    return x / x.square().mean(dim=reduction_dims, keepdim=True).sqrt()
+
 class ScaleNorm(nn.Module):
     def __init__(self, num_features, init_scale=1):
         super(ScaleNorm, self).__init__()
