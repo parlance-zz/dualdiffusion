c7f2b3c6ab154c8fcbb140abc0a139631b9d805f
parlance-zz <parlance@fifth-harmonic.com>
2024-05-29 19:36:36 -0600

    fix distributed inference

--- process_dataset_latents.py
+++ process_dataset_latents.py
@@ -46,13 +46,13 @@ if __name__ == "__main__":
     init_cuda()
     load_dotenv(override=True)
 
-    model_name = "edm2_vae_test7_4"
+    model_name = "edm2_vae7_5"
     num_encode_offsets = 8 # should be equal to latent downsample factor
     pitch_shifts = [-1, 1]
-    batch_size = 2 # num_encode_offsets should be divisible by batch_size
+    batch_size = 8 # num_encode_offsets should be divisible by batch_size
     sample_latents = True
     seed = 2000
-    write_debug_files = True
+    write_debug_files = False
     fp16 = True
 
     distributed_state = PartialState()
@@ -98,98 +98,119 @@ if __name__ == "__main__":
         with open(os.path.join(dataset_path, split_metadata_file), "r") as f:
             split_metadata = [json.loads(line) for line in f.readlines()]
 
-        print(f"Processing {len(split_metadata)} samples from {split_metadata_file}...")
-        progress_bar = tqdm(total=len(split_metadata))
-        progress_bar.set_description(f"Split: {split_metadata_file}")
-
-        #for i, sample in enumerate(split_metadata):
-        with distributed_state.split_between_processes(split_metadata) as sample:
-
-            game_id = sample["game_id"]
+        # filter split samples that already encoded
+        encode_samples = []
+        for sample in split_metadata:
             file_name = sample["file_name"]
             output_filename = f"{os.path.splitext(file_name)[0]}.safetensors"
             output_path = os.path.join(latents_dataset_path, output_filename)
 
             if os.path.exists(output_path):
-                progress_bar.update(1)
                 continue
-
-            file_ext = os.path.splitext(file_name)[1]
-            if dataset_format == ".raw":
-                input_raw_sample = load_raw(os.path.join(dataset_path, file_name), dtype=dataset_raw_format)
-            else:
-                input_raw_sample = load_audio(os.path.join(dataset_path, file_name))
-            crop_width = pipeline.format.get_sample_crop_width(length=input_raw_sample.shape[-1] - encode_offset_padding)
-
-            input_raw_samples = []
-            for offset in encode_offsets:
-                input_raw_offset_sample = input_raw_sample[:, offset:offset+crop_width].unsqueeze(0).to(device)
-                input_raw_samples.append(input_raw_offset_sample)
-            input_raw_sample = torch.cat(input_raw_samples, dim=0)
-
-            input_samples = []
-            for format in formats:
-                for j in range(num_batches_per_sample):
-                    batch_input_raw_sample = input_raw_sample[j*batch_size:(j+1)*batch_size]
-                    input_sample = format.raw_to_sample(batch_input_raw_sample, return_dict=True)["samples"].type(model_dtype)
-                    input_samples.append(input_sample)
-            input_sample = torch.cat(input_samples, dim=0)
+            encode_samples.append(sample)
+        
+        if distributed_state.is_main_process:
+            print(f"Processing {len(split_metadata)} samples from {split_metadata_file} ({len(encode_samples)} samples left to process)...")
             
-            class_labels = pipeline.get_class_labels(game_id)
-            vae_class_embeddings = pipeline.vae.get_class_embeddings(class_labels)            
-            latents = []
-            for j in range(input_sample.shape[0] // batch_size):
-                batch_input_sample = input_sample[j*batch_size:(j+1)*batch_size]
-                posterior = pipeline.vae.encode(batch_input_sample, vae_class_embeddings, pipeline.format)
-                batch_latents = posterior.sample(torch.randn) if sample_latents else posterior.mode()
-                latents.append(batch_latents)
-            latents = torch.cat(latents, dim=0)
-
-            latents_quantized, offset_and_range = quantize_tensor(latents, 256)
-            latents_dict = {"latents": latents_quantized.type(torch.uint8), "offset_and_range": offset_and_range}
-            save_safetensors(latents_dict, output_path)
-
-            # debug
-            if write_debug_files:
-                latents_dict = load_safetensors(output_path)
-                latents = dequantize_tensor(latents_dict["latents"], latents_dict["offset_and_range"]).to(device)
-                for j, latent in enumerate(latents.unbind(0)):
-                    save_raw_img(latent, os.path.join(debug_path, "latents", f"latents_{j:02}.png"))
-
-                    decoded = pipeline.vae.decode(latent.unsqueeze(0).to(pipeline.vae.dtype), vae_class_embeddings, pipeline.format)
-                    raw_sample = pipeline.format.sample_to_raw(decoded.float()).squeeze(0)
-                    save_audio(raw_sample, model_params["sample_rate"], os.path.join(debug_path, "latents", f"audio_{j:02}.flac"))
-                exit()
+        with distributed_state.split_between_processes(encode_samples) as samples:
 
             if distributed_state.is_main_process:
-                progress_bar.update(1)
-                progress_bar.set_postfix({"file_name": file_name})
+                progress_bar = tqdm(total=len(samples))
+                progress_bar.set_description(f"Split: {split_metadata_file}")
+            
+            for sample in samples:
+                game_id = sample["game_id"]
+                file_name = sample["file_name"]
+                output_filename = f"{os.path.splitext(file_name)[0]}.safetensors"
+                output_path = os.path.join(latents_dataset_path, output_filename)
 
-        progress_bar.close()
+                if os.path.exists(output_path):
+                    if distributed_state.is_main_process:
+                        progress_bar.update(1)
+                    continue
 
-        # update filename in split metadatato new safetensors latents
-        for i, sample in enumerate(split_metadata):
-            file_name = sample["file_name"]
-            output_filename = f"{os.path.splitext(file_name)[0]}.safetensors"
-            split_metadata[i]["file_name"] = output_filename
+                file_ext = os.path.splitext(file_name)[1]
+                if dataset_format == ".raw":
+                    input_raw_sample = load_raw(os.path.join(dataset_path, file_name), dtype=dataset_raw_format)
+                else:
+                    input_raw_sample = load_audio(os.path.join(dataset_path, file_name))
+                crop_width = pipeline.format.get_sample_crop_width(length=input_raw_sample.shape[-1] - encode_offset_padding)
 
-        # save split metadata for latents dataset
-        split_metadata_output_path = os.path.join(latents_dataset_path, split_metadata_file)
-        print(f"Saving split metadata to {split_metadata_output_path}...")
-        with open(split_metadata_output_path, "w") as f:
-            for sample in split_metadata:
-                f.write(json.dumps(sample) + "\n")
+                input_raw_samples = []
+                for offset in encode_offsets:
+                    input_raw_offset_sample = input_raw_sample[:, offset:offset+crop_width].unsqueeze(0).to(device)
+                    input_raw_samples.append(input_raw_offset_sample)
+                input_raw_sample = torch.cat(input_raw_samples, dim=0)
 
-    # save model info so we know which model / checkpoint was used to generate the latents
-    vae_model_info_path = os.path.join(latents_dataset_path, "vae_model_info.md")
-    print(f"Saving model info to {vae_model_info_path}...")
-    with open(vae_model_info_path, "w") as f:
-        f.write(f"model_name: {model_name}\n")
-        f.write(f"last_global_step: {last_global_step}\n")
-        f.write(f"dtype: {model_dtype}\n")
+                input_samples = []
+                for format in formats:
+                    for j in range(num_batches_per_sample):
+                        batch_input_raw_sample = input_raw_sample[j*batch_size:(j+1)*batch_size]
+                        input_sample = format.raw_to_sample(batch_input_raw_sample, return_dict=True)["samples"].type(model_dtype)
+                        input_samples.append(input_sample)
+                input_sample = torch.cat(input_samples, dim=0)
+                
+                class_labels = pipeline.get_class_labels(game_id)
+                vae_class_embeddings = pipeline.vae.get_class_embeddings(class_labels)            
+                latents = []
+                for j in range(input_sample.shape[0] // batch_size):
+                    batch_input_sample = input_sample[j*batch_size:(j+1)*batch_size]
+                    posterior = pipeline.vae.encode(batch_input_sample, vae_class_embeddings, pipeline.format)
+                    batch_latents = posterior.sample(torch.randn) if sample_latents else posterior.mode()
+                    latents.append(batch_latents)
+                latents = torch.cat(latents, dim=0)
 
-    #lastly, copy the dataset_info folder to the latents dataset
-    dataset_infos_path = os.path.join(dataset_path, "dataset_infos")
-    latents_dataset_infos_path = os.path.join(latents_dataset_path, "dataset_infos")
-    print(f"Copying dataset_infos from {dataset_infos_path} to {latents_dataset_infos_path}...")
-    shutil.copytree(dataset_infos_path, latents_dataset_infos_path, dirs_exist_ok=True)
+                latents_quantized, offset_and_range = quantize_tensor(latents, 256)
+                latents_dict = {"latents": latents_quantized.type(torch.uint8), "offset_and_range": offset_and_range}
+                save_safetensors(latents_dict, output_path)
+
+                # debug
+                if write_debug_files:
+                    if distributed_state.is_main_process:
+                        latents_dict = load_safetensors(output_path)
+                        latents = dequantize_tensor(latents_dict["latents"], latents_dict["offset_and_range"]).to(device)
+                        for j, latent in enumerate(latents.unbind(0)):
+                            save_raw_img(latent, os.path.join(debug_path, "latents", f"latents_{j:02}.png"))
+
+                            decoded = pipeline.vae.decode(latent.unsqueeze(0).to(pipeline.vae.dtype), vae_class_embeddings, pipeline.format)
+                            raw_sample = pipeline.format.sample_to_raw(decoded.float()).squeeze(0)
+                            save_audio(raw_sample, model_params["sample_rate"], os.path.join(debug_path, "latents", f"audio_{j:02}.flac"))
+                    exit()
+
+                if distributed_state.is_main_process:
+                    progress_bar.update(1)
+                    progress_bar.set_postfix({"file_name": file_name})
+
+        distributed_state.wait_for_everyone()
+
+        if distributed_state.is_main_process:
+
+            progress_bar.close()
+
+            # update filename in split metadatato new safetensors latents
+            for i, sample in enumerate(split_metadata):
+                file_name = sample["file_name"]
+                output_filename = f"{os.path.splitext(file_name)[0]}.safetensors"
+                split_metadata[i]["file_name"] = output_filename
+
+            # save split metadata for latents dataset
+            split_metadata_output_path = os.path.join(latents_dataset_path, split_metadata_file)
+            print(f"Saving split metadata to {split_metadata_output_path}...")
+            with open(split_metadata_output_path, "w") as f:
+                for sample in split_metadata:
+                    f.write(json.dumps(sample) + "\n")
+
+    if distributed_state.is_main_process:
+        # save model info so we know which model / checkpoint was used to generate the latents
+        vae_model_info_path = os.path.join(latents_dataset_path, "vae_model_info.md")
+        print(f"Saving model info to {vae_model_info_path}...")
+        with open(vae_model_info_path, "w") as f:
+            f.write(f"model_name: {model_name}\n")
+            f.write(f"last_global_step: {last_global_step}\n")
+            f.write(f"dtype: {model_dtype}\n")
+
+        #lastly, copy the dataset_info folder to the latents dataset
+        dataset_infos_path = os.path.join(dataset_path, "dataset_infos")
+        latents_dataset_infos_path = os.path.join(latents_dataset_path, "dataset_infos")
+        print(f"Copying dataset_infos from {dataset_infos_path} to {latents_dataset_infos_path}...")
+        shutil.copytree(dataset_infos_path, latents_dataset_infos_path, dirs_exist_ok=True)
