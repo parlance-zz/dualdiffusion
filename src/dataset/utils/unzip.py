bab2d3f2f1f43591605d718760998c52b47df12e
Christopher Friesen <parlance@fifth-harmonic.com>
2024-12-07 00:34:48 -0700

    improve unzip dataset util robustness

--- src/dataset/utils/unzip.py
+++ src/dataset/utils/unzip.py
@@ -1,8 +1,8 @@
 import os
 import py7zr
 
-source_folder = 'D:/dualdiffusion/dataset/usf/zip'
-target_folder = 'D:/dualdiffusion/dataset/usf/to_transcode'
+source_folder = 'Y:/3do/zip'
+target_folder = 'Y:/3do/to_transcode'
 
 def extract_7z_files(source_folder, target_folder):
     # Ensure the target folder exists
@@ -26,16 +26,20 @@ def extract_7z_files(source_folder, target_folder):
                 subfolder_name = subfolder_name[:-1]
 
             subfolder_path = os.path.join(target_folder, subfolder_name)
-            if os.path.isdir(subfolder_path):
-                print(f"Skipping {filename} as {subfolder_path} already exists")
-                continue
+            if os.path.isdir(subfolder_path) == True:
+                if len(os.listdir(subfolder_path)) > 0: # only skip if folder is not empty                    
+                    print(f"Skipping {filename} as {subfolder_path} already exists")
+                    continue
 
             os.makedirs(subfolder_path, exist_ok=True)
 
             # Extract the 7z file into the subfolder
             print(f'Extracting {filename} to {subfolder_path}')
-            with py7zr.SevenZipFile(file_path, mode='r') as archive:
-                archive.extractall(path=subfolder_path)
+            try:
+                with py7zr.SevenZipFile(file_path, mode='r') as archive:
+                    archive.extractall(path=subfolder_path)
+            except Exception as e:
+                print(f'Error extracting {filename}, skipping: {e}')
 
 if __name__ == "__main__":
     extract_7z_files(source_folder, target_folder)
