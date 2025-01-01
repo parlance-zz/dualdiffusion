8610cdce18dad5850fee3b5aa2b5fe4c6248dd2a
Christopher Friesen <parlance@fifth-harmonic.com>
2025-01-01 13:44:05 -0700

    add CriticalSection context manager to all i/o write operations in dual_diffusion_utils to avoid terminating while writing to file. add init_logging to dual_diffusion_utils

--- src/utils/dual_diffusion_utils.py
+++ src/utils/dual_diffusion_utils.py
@@ -25,23 +25,24 @@ from utils import config
 from io import BytesIO
 from typing import Optional, Union, Any
 from json import dumps as json_dumps
+from datetime import datetime
 import os
+import logging
+import signal
+import threading
 
 import numpy as np
 import torch
 import torchaudio
-import torchaudio.functional as AF
 import cv2
-import safetensors.torch as ST
+import safetensors.torch as safetensors
 import matplotlib.pyplot as plt
-from scipy.special import erfinv
 import mutagen
 import mutagen.flac
-from safetensors import safe_open
 
 from utils.roseus_colormap import ROSEUS_COLORMAP
 
-class TF32_Disabled:
+class TF32_Disabled: # disables reduced precision tensor cores inside the context
     def __enter__(self):
         self.original_matmul_allow_tf32 = torch.backends.cuda.matmul.allow_tf32
         self.original_cudnn_allow_tf32 = torch.backends.cudnn.allow_tf32
@@ -51,7 +52,32 @@ class TF32_Disabled:
     def __exit__(self, exc_type, exc_value, traceback):
         torch.backends.cuda.matmul.allow_tf32 = self.original_matmul_allow_tf32
         torch.backends.cudnn.allow_tf32 = self.original_cudnn_allow_tf32
+        return False
+    
+_CRITICAL_SIGNALS = [signal.SIGABRT, signal.SIGINT, signal.SIGTERM]
+if hasattr(signal, "SIGKILL"): _CRITICAL_SIGNALS += [signal.SIGKILL]
+if hasattr(signal, "SIGQUIT"): _CRITICAL_SIGNALS += [signal.SIGQUIT]
 
+class CriticalSection: # ignore signals that would terminate the process inside the context
+    def __enter__(self):
+
+        self.original_handlers = {}
+
+        self.lock = threading.Lock()
+        self.lock.acquire()
+
+        # save original signal handlers
+        for sig in _CRITICAL_SIGNALS:
+            self.original_handlers[sig] = signal.signal(sig, signal.SIG_IGN)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+
+        # restore original signal handlers
+        for sig, handler in self.original_handlers.items():
+            signal.signal(sig, handler)
+        
+        self.lock.release()
+        return False
 
 def init_cuda(default_device: Optional[torch.device] = None) -> None:
 
@@ -64,11 +90,49 @@ def init_cuda(default_device: Optional[torch.device] = None) -> None:
         #torch.backends.cuda.allow_fp16_bf16_reduction_math_sdp(True) # improves perf by ~2.5%, seems stable
         torch.backends.cuda.matmul.allow_tf32 = True
         torch.backends.cudnn.allow_tf32 = True
-        torch.backends.cuda.cufft_plan_cache[0].max_size = 250 # cufft memory leak
+        torch.backends.cuda.cufft_plan_cache[0].max_size = 250 # avoid cufft memory leak
         torch.backends.cudnn.benchmark = True
         
         if default_device is not None:
             torch.cuda.set_device(default_device)
+
+def init_logging(name: Optional[str] = None, group_name: Optional[str] = None,
+                 format: bool = False, verbose: bool = False) -> logging.Logger:
+
+    if name is None:
+        name = globals().get("__file__", None)
+        if name is not None: name = os.path.splitext(os.path.basename(name))[0]
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.handlers.clear()
+
+    if format == True:
+        formatter = logging.Formatter(f"{name}: %(message)s")
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG if verbose else logging.INFO)
+    if format == True:
+        console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    if config.DEBUG_PATH is not None and name is not None:
+        logging_dir = os.path.join(config.DEBUG_PATH, group_name or "logs")
+        os.makedirs(logging_dir, exist_ok=True)
+
+        datetime_str = datetime.now().strftime(r"%Y-%m-%d_%H_%M_%S")
+        log_path = os.path.join(logging_dir, f"{name}_{datetime_str}.log")
+
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setLevel(logging.DEBUG)
+        if format == True:
+            file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        logger.debug(f"\nStarted {name} at {datetime_str}")
+        logger.debug(f"Logging to {log_path}")
+
+    return logger
 
 def multi_plot(*args, layout: Optional[tuple[int, int]] = None,
                figsize: Optional[tuple[int, int]] = None,
@@ -155,7 +219,9 @@ def save_tensor_raw(tensor: torch.Tensor, output_path: str) -> None:
         tensor = tensor.float()
     elif tensor.dtype == torch.complex32:
         tensor = tensor.complex64()
-    tensor.detach().resolve_conj().cpu().numpy().tofile(output_path)
+    
+    with CriticalSection():
+        tensor.detach().resolve_conj().cpu().numpy().tofile(output_path)
 
 @torch.inference_mode()
 def normalize_lufs(raw_samples: torch.Tensor,
@@ -175,7 +241,7 @@ def normalize_lufs(raw_samples: torch.Tensor,
     raw_samples /= raw_samples.abs().amax(
         dim=tuple(range(1, len(raw_samples.shape))), keepdim=True).clip(min=1e-16)
     
-    current_lufs = AF.loudness(raw_samples, sample_rate)
+    current_lufs = torchaudio.functional.loudness(raw_samples, sample_rate)
     gain = 10. ** ((target_lufs - current_lufs) / 20.0)
     gain = gain.view((*gain.shape,) + (1,) * (raw_samples.ndim - gain.ndim))
 
@@ -227,11 +293,13 @@ def save_audio(raw_samples: torch.Tensor,
     
     audio_format = os.path.splitext(output_path)[1].lower()
     bits_per_sample = 16 if audio_format in [".wav", ".flac"] else None
-    torchaudio.save(output_path, raw_samples.cpu(),
-        sample_rate, bits_per_sample=bits_per_sample, compression=compression)
 
-    if metadata is not None:
-        update_audio_metadata(output_path, metadata)
+    with CriticalSection():
+        torchaudio.save(output_path, raw_samples.cpu(),
+            sample_rate, bits_per_sample=bits_per_sample, compression=compression)
+
+        if metadata is not None:
+            update_audio_metadata(output_path, metadata)
 
     return output_path
 
@@ -273,7 +341,8 @@ def update_audio_metadata(audio_path: str, metadata: Optional[dict] = None,
             audio_file["POPM:Windows Media Player 9 Series"] = mutagen.id3.POPM(
                 email="Windows Media Player 9 Series", rating=rating)
 
-        audio_file.save()
+        with CriticalSection():
+            audio_file.save()
 
 def get_audio_metadata(audio_path: str) -> dict:
     audio_format = os.path.splitext(audio_path)[1].lower()
@@ -353,17 +422,18 @@ def save_safetensors(tensors_dict: dict[str, torch.Tensor], output_path: str,
             val = torch.tensor(val)
         tensors_dict[key] = val
 
-    ST.save_file(tensors_dict, output_path, metadata=metadata)
+    with CriticalSection():
+        safetensors.save_file(tensors_dict, output_path, metadata=metadata)
 
 def load_safetensors(input_path: str, device: Optional[torch.device] = None) -> dict[str, torch.Tensor]:
-    return ST.load_file(input_path, device=device)
+    return safetensors.load_file(input_path, device=device)
 
 def load_safetensors_ex(input_path: str, # returns metadata
         device: Optional[torch.device] = None) -> tuple[dict[str, torch.Tensor], dict[str, str]]:  
     
     with open(input_path,'rb') as f:
-        tensors_dict = ST.load(f.read())
-    with safe_open(input_path, framework="pt", device=device) as f:
+        tensors_dict = safetensors.load(f.read())
+    with safetensors.safe_open(input_path, framework="pt", device=device) as f:
         metadata = f.metadata()
         if metadata is not None:
             metadata = dict(metadata)
@@ -376,7 +446,9 @@ def update_safetensors_metadata(safetensors_path: str,
     tensors_dict, metadata = load_safetensors_ex(safetensors_path)
     metadata = metadata or {}
     metadata.update(new_metadata)
-    ST.save_file(tensors_dict, safetensors_path, metadata=metadata)
+
+    with CriticalSection():
+        safetensors.save_file(tensors_dict, safetensors_path, metadata=metadata)
 
 # recursively (through dicts and class instances) move all tensors to CPU
 def move_tensors_to_cpu(instance: Union[dict, torch.Tensor, Any]) -> Any:
@@ -389,9 +461,6 @@ def move_tensors_to_cpu(instance: Union[dict, torch.Tensor, Any]) -> Any:
             instance_dict[key] = move_tensors_to_cpu(value)
 
     return instance
-
-def get_expected_max_normal(n: int) -> float:
-    return erfinv((n - np.pi/8) / (n - np.pi/4 + 1))
 
 @torch.inference_mode()
 def get_fractal_noise2d(shape: tuple, degree: int = 1, **kwargs) -> torch.Tensor:
@@ -494,9 +563,14 @@ def tensor_to_img(x: torch.Tensor,
 
     return img
 
-def save_img(np_img: np.ndarray, img_path: str) -> None:
+def save_img(np_img: np.ndarray, img_path: str, critical: bool = False) -> None:
     os.makedirs(os.path.dirname(img_path), exist_ok=True)
-    cv2.imwrite(img_path, np_img)
+
+    if critical == True:
+        with CriticalSection():
+            cv2.imwrite(img_path, np_img)
+    else:
+        cv2.imwrite(img_path, np_img)
 
 def open_img_window(name: str,
                     width:  Optional[int] = None,
