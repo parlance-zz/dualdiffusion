fix dataset process stage log labels, simplify max output queue size limit calculation and remove DatasetProcessStage.get_max_queue_size,

src/dataset/dataset_processor.py
@@ -70,7 +70,8 @@ def _process_worker(stage: "DatasetProcessStage", rank: int,
     stage_name = stage.__class__.__name__
     logger = logging.getLogger(stage_name)
     log_handler = WorkerLogHandler(stage.log_queue, stage.warning_queue, stage.error_queue, stage_name)
-    logging.basicConfig(level=logging.DEBUG, handlers=[log_handler])
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(log_handler)
 
     # init process
     try:
@@ -217,7 +218,9 @@ class WorkerLogHandler(logging.Handler):
     def emit(self, record: Optional[logging.LogRecord]) -> None:
         if record is not None:
             if record.msg not in ("" , "\n"):
-                record.msg = f"{self.process_name}: {record.msg}"
+                if not hasattr(record, "label"):
+                    setattr(record, "label", self.process_name)
+                    record.msg = f"{record.label}: {record.msg}"
 
             if record.levelno >= logging.ERROR:
                 self.error_queue.put(record)
@@ -227,8 +230,7 @@ class WorkerLogHandler(logging.Handler):
         self.log_queue.put(record)
 
 # main workhorse class for dataset processing. processes should subclass DatasetProcessStage and
-# implement process (required), and get_stage_type, start_process, finish_process,
-# get_max_output_queue_size, info_banner (optional)
+# implement process (required), and get_stage_type, start_process, finish_process, info_banner, summary_banner (optional)
 class DatasetProcessStage(ABC):
 
     def __init__(self) -> None:
@@ -245,7 +247,7 @@ class DatasetProcessStage(ABC):
         self.error_queue = mp.Queue()
 
         self.input_queue = input_queue
-        self.output_queue = WorkQueue(self.get_max_output_queue_size())
+        self.output_queue = WorkQueue(max(num_proc * processor_config.buffer_memory_level, 1))
 
         critical_locks = [mp.Lock() for _ in range(num_proc)]
         finish_events = [mp.Event() for _ in range(num_proc)]
@@ -324,10 +326,6 @@ class DatasetProcessStage(ABC):
     # evenly divided between any cpu stages until max_num_proc is reached
     def get_stage_type(self) -> Literal["io", "cpu", "cuda"]:
         return "io"
-
-    # subclass can return non-zero value if the output items consume significant memory
-    def get_max_output_queue_size(self) -> int:
-        return 0
     
     @torch.inference_mode()
     def start_process(self) -> None:

src/dataset/processes/normalize.py
@@ -35,6 +35,9 @@ from utils.dual_diffusion_utils import (
 
 class NormalizeLoad(DatasetProcessStage):
 
+    def get_stage_type(self):
+        return "cpu"
+    
     def info_banner(self, logger: logging.Logger) -> None:
         logger.info(f"Normalizing to target_lufs: {self.processor_config.target_lufs}")
 
@@ -63,14 +66,11 @@ class NormalizeLoad(DatasetProcessStage):
         
         return None
 
-    def get_max_output_queue_size(self):
-        return 5 * self.processor_config.buffer_memory_level
-    
-    def get_stage_type(self):
-        return "cpu"
-
 class NormalizeProcess(DatasetProcessStage):
 
+    def get_stage_type(self):
+        return "cpu"
+    
     @torch.inference_mode()
     def process(self, input_dict: dict) -> Optional[Union[dict, list[dict]]]:
         
@@ -98,14 +98,11 @@ class NormalizeProcess(DatasetProcessStage):
             "target_lufs": target_lufs,
         }
 
-    def get_max_output_queue_size(self):
-        return 5 * self.processor_config.buffer_memory_level
-    
-    def get_stage_type(self):
-        return "cpu"
-
 class NormalizeSave(DatasetProcessStage):
 
+    def get_stage_type(self):
+        return "cpu"
+    
     def summary_banner(self, logger: logging.Logger) -> None:
         verb = "Normalized"
         if self.processor_config.test_mode == True:
@@ -135,10 +132,8 @@ class NormalizeSave(DatasetProcessStage):
                 return {}
 
         return None
-    
-    def get_stage_type(self):
-        return "cpu"
-    
+
+
 if __name__ == "__main__":
 
     dataset_processor = DatasetProcessor()

src/tests/dataset_processor.py
@@ -26,25 +26,47 @@ import os
 
 import torch
 
-from dataset.dataset_processor import DatasetProcessor, DatasetProcessStage
+from dataset.dataset_processor import DatasetProcessor, DatasetProcessStage, WorkQueue
 
 
 class TestStage(DatasetProcessStage):
 
+    def info_banner(self, logger):
+        logger.info("<Info Banner>")
+    
+    def summary_banner(self, logger):
+        logger.info("<Summary Banner>")
+    
+    @torch.inference_mode()
+    def start_process(self):
+        self.test_errors = 0
+        self.test_warnings = 0
+    
     @torch.inference_mode()
     def process(self, input_dict: dict) -> Optional[Union[dict, list[dict]]]:
         self.logger.debug(input_dict)
-        time.sleep(0.01)
+        time.sleep(0.1)
+
+        if self.test_errors < 5:
+            self.test_errors += 1
+            self.logger.error(f"<Test Error {self.test_errors}>")
+        elif self.test_warnings < 10:
+            self.test_warnings += 1
+            self.logger.warning(f"<Test Warning {self.test_warnings}>")
+
         return None
 
 
 if __name__ == "__main__":
 
+    input_queue = WorkQueue()
+    for i in range(50):
+        input_queue.put({"test_data": i})
+
     DatasetProcessor().process(
         "TestProcess",
-        [
-            TestStage()
-        ],
+        [TestStage()],
+        input=input_queue
     )
 
     os._exit(0)