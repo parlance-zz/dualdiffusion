af343d90956fcaee57e17133997676747e218517
Christopher Friesen <parlance@fifth-harmonic.com>
2025-01-06 17:56:29 -0700

    remove some old code

--- SETUP.md
+++ SETUP.md
@@ -13,19 +13,18 @@
 - download / create dataset in the path specified in .env
     - the folder structure should be dataset_path/system_name/game_name/music.flac
 - edit ./config/dataset/dataset.json as needed
-- run process_dataset.py to setup dataset metadata
+- tbd: dataset processing
 
 # new model setup
 - default model config is in ./config/default, create a copy named ./config/my_model_name
 - edit config files as needed
-- run create_new_model.py and enter my_model_name when prompted, your new model will be created in ./models/my_model_name
+- run create_new_model.py and enter my_model_name when prompted, your new model will be created in $MODELS_PATH/my_model_name
 
 # model training
-- the model creation process will generate a shell script for training any relevant modules in ./models/my_model_name/training/
+- the model creation process will generate a shell script for training any relevant modules in $MODELS_PATH/my_model_name/training/
 - edit the training and accelerate config files as needed and then run the shell script to begin training
 
-# sampling
-- sampling code is still being overhauled / cleaned up at the moment
-- run sample.py to generate samples from a trained model
-- sampling tests / presets are in ./config/sampling
-- the vae module can be sampled / tested by running ./src/tests/vae.py, config for the vae test is in ./config/tests/vae_test.json
+# sampling / testing
+- run sample.py to launch an interactive local web ui
+- sampling tests / presets are in $CONFIG_PATH/sampling
+- the vae module can be sampled / tested by running src/tests/vae.py, config for the vae test is in $CONFIG_PATH/tests/vae_test.json
--- src/sample.py
+++ src/sample.py
@@ -22,12 +22,8 @@
 
 from utils import config
 
-import os
-import time
-import datetime
 import argparse
 
-import numpy as np
 import torch
 
 from sampling.nicegui_app import NiceGUIApp
@@ -45,104 +41,10 @@ def sample(args: argparse.Namespace) -> None:
         raise ValueError("Must either launch with interactive enabled or provide a sampling configuration file.")
     
     raise NotImplementedError("Non-interactive sampling is not yet implemented.")
-    """
 
-    # load sampling model server config
-    sampling_config = config.load_json(
-        os.path.join(config.CONFIG_PATH, "sampling", "sampling.json"))
-    
-    model_name:str = sampling_config["model_name"]
-    load_latest_checkpoints: bool = sampling_config["load_latest_checkpoints"]
-    load_ema: str = sampling_config["load_ema"]
-    device: torch.device = sampling_config["device"]
-    fp16: bool = sampling_config["fp16"]
+    init_cuda()
 
-    # load model
-    model_dtype = torch.bfloat16 if fp16 else torch.float32
-    model_path = os.path.join(config.MODELS_PATH, model_name)
-    print(f"Loading DualDiffusion model from '{model_path}' (dtype={model_dtype}) (ema={load_ema})...")
-    pipeline = DualDiffusionPipeline.from_pretrained(model_path,
-                                                     torch_dtype=model_dtype,
-                                                     device=device,
-                                                     load_latest_checkpoints=load_latest_checkpoints,
-                                                     load_emas={"unet": load_ema})
-    # load sampling params
-    last_global_step = pipeline.unet.config["last_global_step"]
-
-    sample_params_path = os.path.join(config.CONFIG_PATH, "sampling", args.sample_cfg_file)
-    if not os.path.exists(sample_params_path):
-        raise FileNotFoundError(f"Sampling configuration file '{sample_params_path}' not found.")
-    sample_params = SampleParams(**config.load_json(sample_params_path))
-
-    # validate / pre-process sampling params
-    if sample_params.seed is None:
-        sample_params.seed = np.random.randint(
-            10000, 99999 - sample_params.num_batches * sample_params.batch_size)
-
-    game_names = {
-        pipeline.dataset_game_names[game_id]: sample_params.game_ids[game_id]
-        for game_id in sample_params.game_ids.keys()}
-    print("Game IDs:")
-    for game_name, weight in game_names.items():
-        print(f"{game_name:<{max(len(name) for name in game_names)}} : {weight}")
-    sorted_game_ids = sorted(sampling_params.game_ids.items(), key=lambda x:x[1])[-1]
-
-    if sample_params.img2img_input is not None:
-        crop_width = pipeline.format.sample_raw_crop_width(length=sample_params.length)
-        sample_params.img2img_input = load_audio(
-            os.path.join(config.DATASET_PATH, sample_params.img2img_input), start=0, count=crop_width)
-
-    # setup metadata to be saved in output
-    sampling_params = {
-        "steps": steps,
-        "seed": seed,
-        "batch_size": batch_size,
-        "length": length,
-        "cfg_scale": cfg_scale,
-        "sigma_max": sigma_max,
-        "sigma_min": sigma_min,
-        "rho": rho,
-        "slerp_cfg": slerp_cfg,
-        "game_ids": game_ids,
-        "use_midpoint_integration": use_midpoint_integration,
-        "input_perturbation": input_perturbation,
-        "img2img_strength": img2img_strength,
-        "img2img_input": input_audio,
-        "schedule": schedule,
-        "show_debug_plots": show_debug_plots
-    }
-    metadata = sampling_params.copy()
-    metadata["model_name"] = model_name
-    metadata["ema_checkpoint"] = load_ema
-    metadata["global_step"] = last_global_step
-    metadata["fp16"] = fp16
-    metadata["fgla_iterations"] = fgla_iterations
-    metadata["img2img_input"] = img2img_input_path
-    metadata["timestamp"] = datetime.datetime.now().strftime("%m/%d/%Y %I:%M:%S %p")
-    metadata["game_names"] = game_names
-
-    # sample
-
-    output_path = os.path.join(model_path, f"output/step_{last_global_step}")
-
-    start_time = datetime.datetime.now()
-    for i in range(num_samples):
-        print(f"Generating batch {i+1}/{num_samples}...")
-
-        start = time.time()
-        output = pipeline(**sampling_params)
-        print(f"Time taken: {time.time()-start}")
-
-        batch_output_path = os.path.join(output_path, f"step_{last_global_step}_{steps}_{'ema'+ema_std+'_' if load_ema else ''}{'s' if slerp_cfg else 'l'}cfg{cfg_scale}_sgm{sigma_max}-{sigma_min}_r{rho}_g{top_game_id}_s{seed}")
-        for i, sample in enumerate(output.unbind(0)):
-            output_flac_file_path = f"{batch_output_path}_b{i}.flac"
-            save_audio(sample, pipeline.config["model_params"]["sample_rate"], output_flac_file_path, metadata={"diffusion_metadata": dict_str(metadata)})
-            print(f"Saved flac output to {output_flac_file_path}")
-
-        seed += batch_size
-
-    print(f"Finished in: {datetime.datetime.now() - start_time}")
-    """
+    # todo: non-interactive sampling scripts
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="DualDiffusion sampling script.")
@@ -162,6 +64,4 @@ def parse_args() -> argparse.Namespace:
 
 
 if __name__ == "__main__":
-
-    init_cuda()
     sample(parse_args())
