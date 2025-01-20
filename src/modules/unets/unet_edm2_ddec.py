c6492b2dcbffaa7e12c1421bfb30136bf6a787f6
parlance-zz <parlance@fifth-harmonic.com>
2025-01-20 13:53:50 -0700

    add unet_edm2_ddec model and associated trainer for use with vae_edm2_d1, merging these 2 models into one is TBD

---
+++ src/modules/unets/unet_edm2_ddec.py
@@ -0,0 +1,239 @@
+# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# This work is licensed under a Creative Commons
+# Attribution-NonCommercial-ShareAlike 4.0 International License.
+# You should have received a copy of the license along with this
+# work. If not, see http://creativecommons.org/licenses/by-nc-sa/4.0/
+
+"""Improved diffusion model architecture proposed in the paper
+"Analyzing and Improving the Training Dynamics of Diffusion Models"."""
+
+# Modifications under MIT License
+#
+# Copyright (c) 2023 Christopher Friesen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from dataclasses import dataclass
+from typing import Union, Optional, Literal
+
+import torch
+
+from modules.unets.unet import DualDiffusionUNet, DualDiffusionUNetConfig
+from modules.mp_tools import MPConv3D, MPFourier, mp_cat, mp_silu, mp_sum, normalize, resample_3d
+from modules.formats.format import DualDiffusionFormat
+
+@dataclass
+class DDec_UNetConfig(DualDiffusionUNetConfig):
+
+    latent_channels: int = 4
+    model_channels: int  = 32                # Base multiplier for the number of channels.
+    logvar_channels: int = 128               # Number of channels for training uncertainty estimation.
+    channel_mult: list[int]    = (1,2,4,4)   # Per-resolution multipliers for the number of channels.
+    channel_mult_noise: Optional[int] = None # Multiplier for noise embedding dimensionality.
+    channel_mult_emb: Optional[int]   = None # Multiplier for final embedding dimensionality.
+    num_layers_per_block: int = 2            # Number of resnet blocks per resolution.
+    label_balance: float      = 0.5          # Balance between noise embedding (0) and class embedding (1).
+    concat_balance: float     = 0.5          # Balance between skip connections (0) and main path (1).
+    res_balance: float        = 0.3          # Balance between main branch (0) and residual branch (1).
+    mlp_multiplier: int = 1                  # Multiplier for the number of channels in the MLP.
+    mlp_groups: int     = 1                  # Number of groups for the MLPs.
+
+class Block(torch.nn.Module):
+
+    def __init__(self,
+        level: int,                             # Resolution level.
+        in_channels: int,                       # Number of input channels.
+        out_channels: int,                      # Number of output channels.
+        emb_channels: int,                      # Number of embedding channels.
+        flavor: Literal["enc", "dec"] = "enc",
+        resample_mode: Literal["keep", "up", "down"] = "keep",
+        dropout: float         = 0.,       # Dropout probability.
+        res_balance: float     = 0.3,      # Balance between main branch (0) and residual branch (1).
+        clip_act: float        = 256,      # Clip output activations. None = do not clip.
+        mlp_multiplier: int    = 1,        # Multiplier for the number of channels in the MLP.
+        mlp_groups: int        = 1,        # Number of groups for the MLP.
+    ) -> None:
+        super().__init__()
+
+        self.level = level
+        self.out_channels = out_channels
+        self.flavor = flavor
+        self.resample_mode = resample_mode
+        self.dropout = dropout
+        self.res_balance = res_balance
+        self.clip_act = clip_act
+        
+        self.conv_res0 = MPConv3D(out_channels if flavor == "enc" else in_channels,
+                                out_channels * mlp_multiplier, kernel=(2,3,3), groups=mlp_groups)
+        self.conv_res1 = MPConv3D(out_channels * mlp_multiplier, out_channels, kernel=(2,3,3), groups=mlp_groups)
+        self.conv_skip = MPConv3D(in_channels, out_channels, kernel=(1,1,1), groups=1)
+
+        self.emb_gain = torch.nn.Parameter(torch.zeros([]))
+        self.emb_linear = MPConv3D(emb_channels, out_channels * mlp_multiplier,
+                                 kernel=(1,1,1), groups=1) if emb_channels != 0 else None
+
+    def forward(self, x: torch.Tensor, emb: torch.Tensor) -> torch.Tensor:
+        
+        x = resample_3d(x, mode=self.resample_mode)
+
+        if self.flavor == "enc":
+            if self.conv_skip is not None:
+                x = self.conv_skip(x)
+            x = normalize(x, dim=1) # pixel norm
+
+        y = self.conv_res0(mp_silu(x))
+
+        c = self.emb_linear(emb, gain=self.emb_gain) + 1.
+        y = mp_silu(y * c)
+
+        if self.dropout != 0 and self.training == True: # magnitude preserving fix for dropout
+            y = torch.nn.functional.dropout(y, p=self.dropout) * (1. - self.dropout)**0.5
+
+        y = self.conv_res1(y)
+
+        if self.flavor == "dec" and self.conv_skip is not None:
+            x = self.conv_skip(x)
+        x = mp_sum(x, y, t=self.res_balance)
+        
+        if self.clip_act is not None:
+            x = x.clip_(-self.clip_act, self.clip_act)
+        return x
+
+class DDec_UNet(DualDiffusionUNet):
+
+    supports_channels_last: Union[bool, Literal["3d"]] = "3d"
+
+    def __init__(self, config: DDec_UNetConfig) -> None:
+        super().__init__()
+        self.config = config
+
+        block_kwargs = {"dropout": config.dropout,
+                        "mlp_multiplier": config.mlp_multiplier,
+                        "mlp_groups": config.mlp_groups,
+                        "res_balance": config.res_balance}
+
+        cblock = [config.model_channels * x for x in config.channel_mult]
+        cnoise = config.model_channels * config.channel_mult_noise if config.channel_mult_noise is not None else max(cblock)
+        cemb = config.model_channels * config.channel_mult_emb if config.channel_mult_emb is not None else max(cblock)
+        cemb *= self.config.mlp_multiplier
+
+        self.num_levels = len(config.channel_mult)
+
+        # Embedding.
+        self.emb_fourier = MPFourier(cnoise)
+        self.emb_noise = MPConv3D(cnoise, cemb, kernel=())
+        self.emb_label = MPConv3D(config.in_channels_emb, cemb, kernel=())
+        self.emb_label_unconditional = MPConv3D(1, cemb, kernel=())
+
+        # Training uncertainty estimation.
+        self.logvar_fourier = MPFourier(config.logvar_channels)
+        self.logvar_linear = MPConv3D(config.logvar_channels, 1, kernel=(), disable_weight_norm=True)
+
+        # Encoder.
+        self.enc = torch.nn.ModuleDict()
+        cout = 2
+
+        for level, channels in enumerate(cblock):
+            
+            if level == 0:
+                cin = cout
+                cout = channels
+                self.enc[f"conv_in"] = MPConv3D(cin, cout, kernel=(2,3,3))
+            else:
+                self.enc[f"block{level}_down"] = Block(level, cout, cout, cemb, flavor="enc", resample_mode="down", **block_kwargs)
+            
+            for idx in range(config.num_layers_per_block):
+                cin = cout
+                cout = channels
+                self.enc[f"block{level}_layer{idx}"] = Block(level, cin, cout, cemb, flavor="enc", **block_kwargs)
+
+        # Decoder.
+        self.dec = torch.nn.ModuleDict()
+        skips = [block.out_channels for block in self.enc.values()]
+        
+        for level, channels in reversed(list(enumerate(cblock))):
+            
+            if level == len(cblock) - 1:
+                self.dec[f"block{level}_in0"] = Block(level, cout, cout, cemb, flavor="dec", **block_kwargs)
+                #self.dec[f"block{level}_in1"] = Block(level, cout, cout, cemb, flavor="dec", **block_kwargs)
+            else:
+                self.dec[f"block{level}_up"] = Block(level, cout, cout, cemb, flavor="dec", resample_mode="up", **block_kwargs)
+
+            for idx in range(config.num_layers_per_block + 1):
+                cin = cout + skips.pop()
+                cout = channels
+                self.dec[f"block{level}_layer{idx}"] = Block(level, cin, cout, cemb, flavor="dec", **block_kwargs)
+                
+        self.out_gain = torch.nn.Parameter(torch.zeros([]))
+        self.conv_out = MPConv3D(cout, 1, kernel=(2,3,3))
+
+    def get_embeddings(self, emb_in: torch.Tensor, conditioning_mask: torch.Tensor) -> torch.Tensor:
+        u_embedding = self.emb_label_unconditional(torch.ones(1, device=self.device, dtype=self.dtype))
+        c_embedding = self.emb_label(normalize(emb_in).to(device=self.device, dtype=self.dtype))
+        return mp_sum(u_embedding, c_embedding, t=conditioning_mask.unsqueeze(1).to(self.device, self.dtype))
+        
+    def get_sigma_loss_logvar(self, sigma: Optional[torch.Tensor] = None) -> torch.Tensor:
+        return self.logvar_linear(self.logvar_fourier(sigma.flatten().log() / 4)).view(-1, 1, 1, 1).float()
+    
+    def get_latent_shape(self, latent_shape: Union[torch.Size, tuple[int, int, int, int]]) -> torch.Size:
+        return latent_shape[0:2] + ((latent_shape[2] // 2**(self.num_levels-1)) * 2**(self.num_levels-1),
+                                    (latent_shape[3] // 2**(self.num_levels-1)) * 2**(self.num_levels-1))
+
+    def forward(self, x_in: torch.Tensor,
+                sigma: torch.Tensor,
+                format: DualDiffusionFormat,
+                embeddings: torch.Tensor,
+                x_ref: Optional[torch.Tensor] = None) -> torch.Tensor:
+
+        with torch.no_grad():
+            sigma = sigma.view(-1, 1, 1, 1, 1)
+            
+            # Preconditioning weights.
+            c_skip = self.config.sigma_data ** 2 / (sigma ** 2 + self.config.sigma_data ** 2)
+            c_out = sigma * self.config.sigma_data / (sigma ** 2 + self.config.sigma_data ** 2).sqrt()
+            c_in = 1 / (self.config.sigma_data ** 2 + sigma ** 2).sqrt()
+            c_noise = (sigma.flatten().log() / 4).to(self.dtype)
+
+            x = (c_in * x_in.unsqueeze(1)).to(dtype=torch.bfloat16)#.to(self.dtype)
+ 
+        # Embedding.
+        emb = self.emb_noise(self.emb_fourier(c_noise))
+        emb = mp_sum(emb, embeddings.to(emb.dtype), t=self.config.label_balance)
+        emb = mp_silu(emb).unsqueeze(-1).unsqueeze(-1).unsqueeze(-1).to(dtype=torch.bfloat16)#.to(x.dtype)
+
+        # Encoder.
+        x = torch.cat((x, x_ref.unsqueeze(1)), dim=1)
+
+        skips = []
+        for name, block in self.enc.items():
+            x = block(x) if "conv" in name else block(x, emb)
+            skips.append(x)
+
+        # Decoder.
+        for name, block in self.dec.items():
+            if "layer" in name:
+                x = mp_cat(x, skips.pop(), t=self.config.concat_balance)
+            x = block(x, emb)
+
+        x = self.conv_out(x, gain=self.out_gain)
+        D_x = c_skip * x_in.unsqueeze(1).float() + c_out * x.float()
+
+        return D_x.squeeze(1)
+    
---
+++ src/training/module_trainers/ddec_vae_trainer.py
@@ -0,0 +1,259 @@
+# MIT License
+#
+# Copyright (c) 2023 Christopher Friesen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from dataclasses import dataclass
+from typing import Literal, Optional
+#import os
+
+import torch
+
+from training.sigma_sampler import SigmaSamplerConfig, SigmaSampler
+from training.trainer import DualDiffusionTrainer
+from .module_trainer import ModuleTrainerConfig, ModuleTrainer
+from modules.unets.unet import DualDiffusionUNet
+from modules.formats.spectrogram import SpectrogramFormat
+from modules.vaes.vae_edm2_d1 import DualDiffusionVAE
+from modules.mp_tools import normalize
+from utils.dual_diffusion_utils import dict_str #, load_safetensors
+
+@dataclass
+class DiffusionDecoderTrainerConfig(ModuleTrainerConfig):
+
+    sigma_distribution: Literal["ln_normal", "ln_sech", "ln_sech^2",
+                                "ln_linear", "ln_pdf"] = "ln_sech"
+    sigma_override_max: Optional[float] = None
+    sigma_override_min: Optional[float] = None
+    sigma_dist_scale: float = 1.0
+    sigma_dist_offset: float = 0.3
+    use_stratified_sigma_sampling: bool = True
+    sigma_pdf_resolution: Optional[int] = 128
+    sigma_pdf_warmup_steps: Optional[int] = 30000
+
+    validation_sigma_distribution: Literal["ln_normal", "ln_sech", "ln_sech^2",
+                                            "ln_linear", "ln_pdf"] = "ln_sech"
+    validation_sigma_override_max: Optional[float] = None
+    validation_sigma_override_min: Optional[float] = None
+    validation_sigma_dist_scale: float = 1.0
+    validation_sigma_dist_offset: float = 0.3
+
+    num_loss_buckets: int = 10
+    latents_perturbation: float = 0.1
+    conditioning_dropout: float = 0.1
+
+class DiffusionDecoder_VAETrainer(ModuleTrainer):
+    
+    @torch.no_grad()
+    def __init__(self, config: DiffusionDecoderTrainerConfig, trainer: DualDiffusionTrainer) -> None:
+
+        self.config = config
+        self.trainer = trainer
+        self.logger = trainer.logger
+        self.module: DualDiffusionUNet = trainer.module
+
+        self.is_validation_batch = False
+        self.device_generator = None
+        self.cpu_generator = None
+
+        self.format: SpectrogramFormat = trainer.pipeline.format.to(self.trainer.accelerator.device)
+        self.vae: DualDiffusionVAE = trainer.pipeline.vae.to(
+            device=self.trainer.accelerator.device, dtype=torch.bfloat16).requires_grad_(False).eval()
+
+        if trainer.config.enable_channels_last == True:
+            self.vae = self.vae.to(memory_format=torch.channels_last)
+        if trainer.config.enable_model_compilation:
+            self.module.compile(**trainer.config.compile_params)
+            self.format.compile(**trainer.config.compile_params)
+
+        if self.vae.config.last_global_step == 0:
+            self.logger.error("VAE model has not been trained, aborting training..."); exit(1)
+
+        self.unconditional_audio_embedding = None
+        """
+        dataset_embeddings_path = os.path.join(self.trainer.dataset.config.data_dir, "dataset_infos", "dataset_embeddings.safetensors")
+        if not os.path.isfile(dataset_embeddings_path):
+            self.logger.error(f"  Dataset embeddings not found at {dataset_embeddings_path}, aborting training..."); exit(1)
+        dataset_embeddings = load_safetensors(dataset_embeddings_path)
+        self.unconditional_audio_embedding = normalize(dataset_embeddings["_unconditional_audio"][:].to(self.trainer.accelerator.device).unsqueeze(0)).float()
+        self.logger.info(f"  Loaded dataset embeddings successfully from {dataset_embeddings_path}")
+        """  
+        
+        if self.config.num_loss_buckets > 0:
+            self.logger.info(f"Using {self.config.num_loss_buckets} loss buckets")
+            self.unet_loss_buckets = torch.zeros(
+                self.config.num_loss_buckets, device="cpu", dtype=torch.float32)
+            self.unet_loss_bucket_counts = torch.zeros(
+                self.config.num_loss_buckets, device="cpu", dtype=torch.float32)
+        else:
+            self.logger.info("UNet loss buckets are disabled")
+
+        if self.config.latents_perturbation > 0:
+            self.logger.info(f"Using latents perturbation: {self.config.latents_perturbation}")
+        else: self.logger.info("Latents perturbation is disabled")
+
+        self.logger.info(f"Dropout: {self.module.config.dropout} Conditioning dropout: {self.config.conditioning_dropout}")
+
+        # sigma schedule / distribution for train batches
+        sigma_sampler_config = SigmaSamplerConfig(
+            sigma_max=self.config.sigma_override_max or self.module.config.sigma_max,
+            sigma_min=self.config.sigma_override_min or self.module.config.sigma_min,
+            sigma_data=self.module.config.sigma_data,
+            distribution=self.config.sigma_distribution,
+            dist_scale=self.config.sigma_dist_scale,
+            dist_offset=self.config.sigma_dist_offset,
+            use_stratified_sigma_sampling=self.config.use_stratified_sigma_sampling,
+            sigma_pdf_resolution=self.config.sigma_pdf_resolution,
+        )
+        self.sigma_sampler = SigmaSampler(sigma_sampler_config)
+        self.logger.info("SigmaSampler config:")
+        self.logger.info(dict_str(sigma_sampler_config.__dict__))
+
+        # separate noise schedule / sigma distribution for validation batches
+        validation_sigma_sampler_config = SigmaSamplerConfig(
+            sigma_max=self.config.validation_sigma_override_max or self.module.config.sigma_max,
+            sigma_min=self.config.validation_sigma_override_min or self.module.config.sigma_min,
+            sigma_data=self.module.config.sigma_data,
+            distribution=self.config.validation_sigma_distribution,
+            dist_scale=self.config.validation_sigma_dist_scale,
+            dist_offset=self.config.validation_sigma_dist_offset,
+            use_static_sigma_sampling=True,
+            sigma_pdf_resolution=self.config.sigma_pdf_resolution,
+        )
+        self.validation_sigma_sampler = SigmaSampler(validation_sigma_sampler_config)
+        self.logger.info("Validation SigmaSampler config:")
+        self.logger.info(dict_str(validation_sigma_sampler_config.__dict__))
+
+        # pre-calculate the per-sigma loss bucket names
+        if self.config.num_loss_buckets > 0:
+            bucket_sigma = 2 / torch.linspace(torch.pi/2, 0, self.config.num_loss_buckets+1).tan()
+            bucket_sigma[0] = 0; bucket_sigma[-1] = float("inf")
+            self.bucket_names = [f"loss_σ_buckets/{bucket_sigma[i]:.2f} - {bucket_sigma[i+1]:.2f}"
+                                 for i in range(self.config.num_loss_buckets)]
+
+    @torch.no_grad()
+    def init_batch(self, validation: bool = False) -> None:
+        
+        if validation == True: # use the same random values for every validation batch
+            self.is_validation_batch = True
+            total_batch_size = self.trainer.validation_total_batch_size
+            sigma_sampler = self.validation_sigma_sampler
+
+            self.device_generator = torch.Generator(device=self.trainer.accelerator.device).manual_seed(0)
+            self.cpu_generator = torch.Generator(device="cpu").manual_seed(0)
+        else:
+            self.is_validation_batch = False
+            total_batch_size = self.trainer.total_batch_size
+            sigma_sampler = self.sigma_sampler
+
+            self.device_generator = None
+            self.cpu_generator = None
+
+        # reset sigma-bucketed loss for new batch
+        if self.config.num_loss_buckets > 0:
+            self.unet_loss_buckets.zero_()
+            self.unet_loss_bucket_counts.zero_()
+
+        # if using dynamic sigma sampling with ln_pdf, update the pdf using the learned per-sigma error estimate
+        if self.config.sigma_distribution == "ln_pdf" and validation == False:
+            ln_sigma = torch.linspace(self.sigma_sampler.config.ln_sigma_min,
+                                      self.sigma_sampler.config.ln_sigma_max,
+                                      self.config.sigma_pdf_resolution,
+                                      device=self.trainer.accelerator.device)
+            ln_sigma_error = self.module.logvar_linear(
+                self.module.logvar_fourier(ln_sigma/4)).float().flatten().detach()
+            pdf_warmup_factor = min(1, self.trainer.global_step / (self.config.sigma_pdf_warmup_steps or 1))
+            sigma_distribution_pdf = (-pdf_warmup_factor * self.config.sigma_dist_scale * ln_sigma_error).exp()
+            self.sigma_sampler.update_pdf(sigma_distribution_pdf)
+        
+        # sample whole-batch sigma and sync across all ranks / processes
+        self.global_sigma = sigma_sampler.sample(total_batch_size, device=self.trainer.accelerator.device)
+        self.global_sigma = self.trainer.accelerator.gather(self.global_sigma.unsqueeze(0))[0]
+
+    def train_batch(self, batch: dict) -> dict[str, torch.Tensor]:
+
+        samples = self.format.raw_to_sample(batch["audio"]).detach().clone()
+        if self.trainer.config.enable_channels_last == True:
+            samples = samples.to(memory_format=torch.channels_last)
+
+        sample_audio_embeddings = normalize(batch["audio_embeddings"])
+        vae_emb = self.vae.get_embeddings(sample_audio_embeddings)
+        enc_states, dec_states = self.vae(samples, vae_emb, add_latents_noise=self.config.latents_perturbation)
+
+        latents: torch.Tensor = enc_states[-1][1].float().detach()
+        ref_samples: torch.Tensor = dec_states[-1][1].squeeze(1).float().detach()
+
+        if self.is_validation_batch == False:
+            device_batch_size = self.trainer.config.device_batch_size
+            assert latents.shape == self.trainer.latent_shape, f"Expected shape {self.trainer.latent_shape}, got {latents.shape}"
+            assert samples.shape == self.trainer.sample_shape, f"Expected shape {self.trainer.sample_shape}, got {samples.shape}"
+        else:
+            device_batch_size = self.trainer.config.validation_device_batch_size
+            assert latents.shape == self.trainer.validation_latent_shape, f"Expected shape {self.trainer.validation_latent_shape}, got {latents.shape}"
+            assert samples.shape == self.trainer.validation_sample_shape, f"Expected shape {self.trainer.validation_sample_shape}, got {samples.shape}"
+
+        conditioning_mask = (torch.rand(device_batch_size,
+            generator=self.device_generator, device=self.trainer.accelerator.device) > self.config.conditioning_dropout).float()
+        unet_class_embeddings = self.module.get_embeddings(sample_audio_embeddings, conditioning_mask)
+
+        # get the noise level for this sub-batch from the pre-calculated whole-batch sigma (required for stratified sampling)
+        local_sigma = self.global_sigma[self.trainer.accelerator.local_process_index::self.trainer.accelerator.num_processes]
+        batch_sigma = local_sigma[self.trainer.accum_step * device_batch_size:(self.trainer.accum_step+1) * device_batch_size]
+
+        # prepare model inputs
+        noise = torch.randn(samples.shape, device=samples.device, generator=self.device_generator)
+        noise = (noise * batch_sigma.view(-1, 1, 1, 1)).detach()
+
+        # convert model inputs to channels_last memory format for performance, if enabled
+        if self.trainer.config.enable_channels_last == True:
+            noise = noise.to(memory_format=torch.channels_last)
+
+        denoised = self.module(samples + noise, batch_sigma, self.format, unet_class_embeddings, ref_samples)
+        batch_loss_weight = (batch_sigma ** 2 + self.module.config.sigma_data ** 2) / (batch_sigma * self.module.config.sigma_data) ** 2
+        batch_weighted_loss = torch.nn.functional.mse_loss(denoised, samples, reduction="none").mean(dim=(1,2,3)) * batch_loss_weight
+
+        if self.is_validation_batch == True:
+            batch_loss = batch_weighted_loss
+        else: # for train batches this takes the loss as the gaussian NLL with sigma-specific learned variance
+            error_logvar = self.module.get_sigma_loss_logvar(sigma=batch_sigma)
+            batch_loss = batch_weighted_loss / error_logvar.exp() + error_logvar
+        
+        # log loss bucketed by noise level range
+        if self.config.num_loss_buckets > 0:
+            global_weighted_loss = self.trainer.accelerator.gather(batch_weighted_loss.detach()).cpu()
+            global_sigma_quantiles = 1 - self.trainer.accelerator.gather(self.module.config.sigma_data / batch_sigma.detach() * 2).cpu().arctan() / (torch.pi/2)
+
+            target_buckets = (global_sigma_quantiles * self.unet_loss_buckets.shape[0]).long().clip(min=0, max=self.unet_loss_buckets.shape[0] - 1)
+            self.unet_loss_buckets.index_add_(0, target_buckets, global_weighted_loss)
+            self.unet_loss_bucket_counts.index_add_(0, target_buckets, torch.ones_like(global_weighted_loss))
+
+        return {"loss": batch_loss}
+
+    @torch.no_grad()
+    def finish_batch(self) -> dict[str, torch.Tensor]:
+        logs = {}
+
+        # added sigma-bucketed loss to logs
+        if self.config.num_loss_buckets > 0:
+            for i in range(self.config.num_loss_buckets):
+                if self.unet_loss_bucket_counts[i].item() > 0:
+                    logs[self.bucket_names[i]] = (self.unet_loss_buckets[i] / self.unet_loss_bucket_counts[i]).item()
+
+        return logs
