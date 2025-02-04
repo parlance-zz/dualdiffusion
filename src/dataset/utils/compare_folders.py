f3398a1783547e35f6ce9ee83b3dfa1494474da0
parlance-zz <parlance@fifth-harmonic.com>
2025-02-03 22:14:33 -0700

    refactor compare_folders dataset util to find duplicates

--- src/dataset/utils/compare_folders.py
+++ src/dataset/utils/compare_folders.py
@@ -1,51 +1,76 @@
-from difflib import get_close_matches
+from difflib import get_close_matches, SequenceMatcher
 import os
 import shutil
 import webbrowser
 
 
-# this script is useful for merging 2 large scrapes of the same content
-# it will compare folders under path a to folders under path b, and if no match is found you can copy it to path c
-path_a = "Y:\\spc\\old_zophars_to_transcode"
-path_b = "Y:\\spc\\to_transcode"
-path_c = "Y:\\spc\\new_to_transcode_pre_spc_fix_backup"
+root_path = "/mnt/vault/dataset"
+cutoff = 0.73
 
 def get_folders(path):
     """Retrieve the list of folders in the given path."""
     return [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
 
-def compare_folders(path_a, path_b, path_c):
-    """Compare folders in path A to those in path B."""
-    folders_a = get_folders(path_a)
-    folders_b = get_folders(path_b)
+def get_folder_size(path):
+    total_size = 0
+    with os.scandir(path) as it:
+        for entry in it:
+            if entry.is_file(follow_symlinks=False):
+                total_size += entry.stat().st_size
+            elif entry.is_dir(follow_symlinks=False):
+                total_size += get_folder_size(entry.path)
+    return total_size
+
+def get_similarity_score(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+def decide(full_path_a, full_path_b):
+    while True:
+        _input = input(f"Keep (a/b)?: ").lower()
+        if _input == "a":
+            #shutil.rmtree(full_path_b)
+            print(f"Deleted {full_path_b}")
+            return
+        elif _input == "b":
+            #shutil.rmtree(full_path_a)
+            print(f"Deleted {full_path_a}")
+            return
+        elif _input == "c":
+            webbrowser.open(full_path_a)
+            webbrowser.open(full_path_b)
+        elif _input == "":
+            return
+            
+def compare_folders(path_a, path_b, root_path, cutoff):
+    
+    folders_a = get_folders(os.path.join(root_path, path_a))
+    folders_b = get_folders(os.path.join(root_path, path_b))
 
     for folder in folders_a:
         if folder not in folders_b:
-            closest_matches = get_close_matches(folder, folders_b, n=1)
-            closest_match = closest_matches[0] if closest_matches else "No close match found"
-            print(f"Folder '{folder}' is not in {path_b}. Closest match: {closest_match}")
+            closest_matches = get_close_matches(folder, folders_b, n=1, cutoff=cutoff)
+            if not closest_matches: continue
+            score = get_similarity_score(folder, closest_matches[0])
 
-            while True:
-                _input = input(f"Do you want to copy '{folder}' to '{path_c}'? (y/n/c): ").lower()
-                if _input == "y":
-                    src_path = os.path.join(path_a, folder)
-                    dest_path = os.path.join(path_c, folder)
-                    shutil.copytree(src_path, dest_path)
-                    print(f"Copied '{src_path}' to {dest_path}")
-                    break
-                elif _input == "n":
-                    break
-                elif _input == "c":
-                    webbrowser.open(os.path.join(path_a, folder))
+            print(f"\nPotential duplicates (score: {score:.4f}):")
+            full_path_a = os.path.join(root_path, path_a, folder)
+            full_path_b = os.path.join(root_path, path_b, closest_matches[0])
+            print(f"(a) {path_a}  {(get_folder_size(full_path_a)/1024/1024):.2f}mb  '{folder}'")
+            print(f"(b) {path_b}  {(get_folder_size(full_path_b)/1024/1024):.2f}mb  '{closest_matches[0]}'")
+            decide(full_path_a, full_path_a)
+        else:
+            print(f"\nDuplicate:\n {folder}")
+            full_path_a = os.path.join(root_path, path_a, folder)
+            full_path_b = os.path.join(root_path, path_b, folder)
+            print(f"(a) {path_a}  {(get_folder_size(full_path_a)/1024/1024):.2f}mb")
+            print(f"(b) {path_b}  {(get_folder_size(full_path_b)/1024/1024):.2f}mb")
+            decide(full_path_a, full_path_a)
 
-def main(path_a, path_b, path_c):
-
-    if not os.path.exists(path_a) or not os.path.exists(path_b):
-        print("One or both paths do not exist. Please provide valid paths.")
-        return
-
-    os.makedirs(path_c, exist_ok=True)
-    compare_folders(path_a, path_b, path_c)
 
 if __name__ == "__main__":
-    main(path_a, path_b, path_c)
+
+    all_folders = get_folders(root_path)
+    
+    for i in range(len(all_folders)):
+        for j in range(i+1, len(all_folders)):
+            compare_folders(all_folders[i], all_folders[j], root_path, cutoff=cutoff)
