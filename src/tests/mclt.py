6d2ee683e1ab80d666a259e7e3e1af17b8f5d69d
parlance-zz <parlance@fifth-harmonic.com>
2025-03-11 11:06:53 -0600

    add mel_density scaled std statistics to mclt format test

--- src/tests/mclt.py
+++ src/tests/mclt.py
@@ -29,6 +29,7 @@ import random
 import torch
 
 from modules.formats.mclt import DualMCLTFormat, DualMCLTFormatConfig
+from modules.formats.frequency_scale import get_mel_density
 from training.sigma_sampler import SigmaSampler, SigmaSamplerConfig
 from utils.dual_diffusion_utils import (
     load_audio, tensor_info_str, save_audio, init_cuda, save_tensor_raw
@@ -53,6 +54,13 @@ def mclt_test() -> None:
 
     mclt_format: DualMCLTFormat = DualMCLTFormat(cfg.format_config).to(device=cfg.device)
 
+    n_mclt_bins = cfg.format_config.window_len//2
+    mclt_hz = torch.arange(0, n_mclt_bins) + 0.5
+    mclt_hz = mclt_hz / n_mclt_bins * cfg.format_config.sample_rate / 2
+    mel_density = get_mel_density(mclt_hz)
+    mel_density /= mel_density.square().mean().sqrt()
+    mel_density = mel_density.view(1, 1,-1, 1).to(cfg.device)
+
     crop_width = mclt_format.sample_raw_crop_width()
     test_output_path = os.path.join(config.DEBUG_PATH, "mclt_test")
 
@@ -63,6 +71,7 @@ def mclt_test() -> None:
 
     psd = None
     avg_std = 0
+    avg_scaled_std = 0
 
     for i, sample_filename in enumerate(cfg.test_samples):
         base_filename = os.path.splitext(os.path.basename(sample_filename))[0]
@@ -74,14 +83,17 @@ def mclt_test() -> None:
         assert input_raw_sample.shape[1] == mclt_format.config.sample_raw_channels
 
         mclt_sample = mclt_format.raw_to_sample(input_raw_sample)
+        mclt_sample_scaled = mclt_sample / mel_density
         output_raw_sample = mclt_format.sample_to_raw(mclt_sample + torch.randn_like(mclt_sample) * cfg.add_noise)
 
         avg_std += mclt_sample.std()
+        avg_scaled_std += mclt_sample_scaled.std()
         if psd is None: psd = mclt_sample.abs().square().to(dtype=torch.float64)# * (torch.arange(0, 256, device=mclt_sample.device).view(1, 1,-1, 1)+0.5) * (70/256)
         else: psd += mclt_sample.abs().square().to(dtype=torch.float64) #* (torch.arange(0, 256, device=mclt_sample.device).view(1, 1,-1, 1)+0.5) * (70/256)
 
         print(f"({i}/{len(cfg.test_samples)}) {sample_filename}:")
         print("  mlct_sample:", tensor_info_str(mclt_sample))
+        print("  mlct_sample_scaled:", tensor_info_str(mclt_sample_scaled))
         print("  input_raw_sample: ", tensor_info_str(input_raw_sample))
         print("  output_raw_sample:", tensor_info_str(output_raw_sample))
 
@@ -97,6 +109,7 @@ def mclt_test() -> None:
         print("")
 
     print(f"avg mdct std: {avg_std / len(cfg.test_samples)}")
+    print(f"avg scaled mdct std: {avg_scaled_std / len(cfg.test_samples)}")
     #exit()
 
     psd = (psd / len(cfg.test_samples)).mean(dim=(1,3)).sqrt().flatten()
