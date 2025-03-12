750bdfeaedb70b40438f0f7d540bd8ec495eedab
parlance-zz <parlance@fifth-harmonic.com>
2025-03-11 20:24:09 -0600

    add dae_edm2_b1 and dae_trainer_b1

---
+++ src/modules/daes/dae_edm2_b1.py
@@ -0,0 +1,237 @@
+# MIT License
+#
+# Copyright (c) 2023 Christopher Friesen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from typing import Optional, Union, Literal
+from dataclasses import dataclass
+
+import torch
+
+from modules.daes.dae import DualDiffusionDAEConfig, DualDiffusionDAE
+from modules.mp_tools import MPConv3D, normalize, resample_3d, mp_silu, mp_sum
+from utils.dual_diffusion_utils import tensor_4d_to_5d, tensor_5d_to_4d
+
+
+@dataclass
+class DualDiffusionDAE_EDM2_B1_Config(DualDiffusionDAEConfig):
+
+    model_channels: int       = 32           # Base multiplier for the number of channels.
+    latent_channels: int      = 4
+    in_channels_emb: int      = 0
+    channel_mult: list[int]   = (1,2,3,5)    # Per-resolution multipliers for the number of channels.
+    channel_mult_emb: Optional[int] = 5      # Multiplier for final embedding dimensionality.
+    num_layers_per_block: int = 3            # Number of resnet blocks per resolution.
+    res_balance: float        = 0.3          # Balance between main branch (0) and residual branch (1).
+    mlp_multiplier: int   = 2                # Multiplier for the number of channels in the MLP.
+    mlp_groups: int       = 1                # Number of groups for the MLPs.
+  
+class Block(torch.nn.Module):
+
+    def __init__(self,
+        level: int,                             # Resolution level.
+        in_channels: int,                       # Number of input channels.
+        out_channels: int,                      # Number of output channels.
+        emb_channels: int,                      # Number of embedding channels.
+        flavor: Literal["enc", "dec"] = "enc",
+        resample_mode: Literal["keep", "up", "down"] = "keep",
+        res_balance: float     = 0.3,      # Balance between main branch (0) and residual branch (1).
+        clip_act: float        = 256,      # Clip output activations. None = do not clip.
+        mlp_multiplier: int    = 1,        # Multiplier for the number of channels in the MLP.
+        mlp_groups: int        = 1,        # Number of groups for the MLP.
+
+    ) -> None:
+        super().__init__()
+
+        self.level = level
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.emb_channels = emb_channels
+        self.flavor = flavor
+        self.resample_mode = resample_mode
+        self.res_balance = res_balance
+        self.clip_act = clip_act
+        
+        self.conv_res0 = MPConv3D(out_channels if flavor == "enc" else in_channels,
+                                out_channels * mlp_multiplier, kernel=(1,3,3), groups=mlp_groups)
+        self.conv_res1 = MPConv3D(out_channels * mlp_multiplier, out_channels, kernel=(1,3,3), groups=mlp_groups)
+        self.conv_skip = MPConv3D(in_channels, out_channels, kernel=(2,1,1), groups=1) if in_channels != out_channels else None
+
+        self.emb_gain = torch.nn.Parameter(torch.zeros([]))
+        self.emb_linear = MPConv3D(emb_channels, out_channels * mlp_multiplier,
+                                 kernel=(1,1,1), groups=1) if emb_channels != 0 else None
+
+    def forward(self, x: torch.Tensor, emb: torch.Tensor) -> torch.Tensor:
+        
+        x = resample_3d(x, mode=self.resample_mode)
+        if self.flavor == "enc":
+            if self.conv_skip is not None:
+                x = self.conv_skip(x)
+            x = normalize(x, dim=1) # pixel norm
+
+        y = self.conv_res0(mp_silu(x))
+
+        if self.emb_channels > 0:
+            c = self.emb_linear(emb, gain=self.emb_gain) + 1.
+            y = mp_silu(y * c)
+        else:
+            y = mp_silu(y)
+
+        y = self.conv_res1(y)
+
+        if self.flavor == "dec" and self.conv_skip is not None:
+            x = self.conv_skip(x)
+
+        x = mp_sum(x, y, t=self.res_balance)
+
+        if self.clip_act is not None:
+            x = x.clip_(-self.clip_act, self.clip_act)
+        return x
+
+class DualDiffusionDAE_EDM2_B1(DualDiffusionDAE):
+
+    supports_channels_last: Union[bool, Literal["3d"]] = "3d"
+
+    def __init__(self, config: DualDiffusionDAE_EDM2_B1_Config) -> None:
+        super().__init__()
+        self.config = config
+
+        block_kwargs = {"mlp_multiplier": config.mlp_multiplier,
+                        "mlp_groups": config.mlp_groups,
+                        "res_balance": config.res_balance} 
+        
+        cblock = [config.model_channels * x for x in config.channel_mult]
+        cemb = config.model_channels * config.channel_mult_emb if config.channel_mult_emb is not None else max(cblock)
+        cemb *= self.config.mlp_multiplier
+
+        self.num_levels = len(config.channel_mult)
+        self.out_gain = torch.nn.Parameter(torch.ones([]))
+        self.recon_loss_logvar = torch.nn.Parameter(torch.zeros([]))
+
+        # Embedding.
+        if config.in_channels_emb > 0:
+            self.emb_label = MPConv3D(config.in_channels_emb, cemb, kernel=())
+            self.emb_dim = cemb
+        else:
+            cemb = 0
+            self.emb_label = None
+            self.emb_dim = 0
+
+        # Encoder.
+        self.enc = torch.nn.ModuleDict()
+        cout = config.in_channels // 2 + 1
+        for level, channels in enumerate(cblock):
+            
+            if level == 0:
+                cin = cout
+                cout = channels
+                self.enc[f"conv_in"] = MPConv3D(cin, cout, kernel=(2,3,3))
+            else:
+                self.enc[f"block{level}_down"] = Block(level, cout, cout, cemb,
+                                        flavor="enc", resample_mode="down", **block_kwargs)
+            
+            for idx in range(config.num_layers_per_block):
+                cin = cout
+                cout = channels
+                self.enc[f"block{level}_layer{idx}"] = Block(level, cin, cout, cemb,
+                                                             flavor="enc", **block_kwargs)
+
+        self.conv_latents_out = MPConv3D(cout, config.latent_channels, kernel=(2,3,3))
+        self.conv_latents_in = MPConv3D(config.latent_channels + 1, cout, kernel=(2,3,3))
+
+        # Decoder.
+        self.dec = torch.nn.ModuleDict()
+        for level, channels in reversed(list(enumerate(cblock))):
+            
+            if level == len(cblock) - 1:
+                self.dec[f"block{level}_in0"] = Block(level, cout, cout, cemb, flavor="dec", **block_kwargs)
+                self.dec[f"block{level}_in1"] = Block(level, cout, cout, cemb, flavor="dec", **block_kwargs)
+            else:
+                self.dec[f"block{level}_up"] = Block(level, cout, cout, cemb, flavor="dec",
+                                                     resample_mode="up", **block_kwargs)
+            for idx in range(config.num_layers_per_block + 1):
+                cin = cout
+                cout = channels
+                self.dec[f"block{level}_layer{idx}"] = Block(level, cin, cout, cemb, flavor="dec", **block_kwargs)
+        
+        self.conv_out = MPConv3D(cout, config.out_channels // 2, kernel=(2,3,3))
+            
+    def get_embeddings(self, emb_in: torch.Tensor) -> torch.Tensor:
+        if self.emb_label is not None:
+            return self.emb_label(normalize(emb_in).to(device=self.device, dtype=self.dtype))
+        else:
+            return None
+    
+    def get_recon_loss_logvar(self) -> torch.Tensor:
+        return self.recon_loss_logvar
+    
+    def get_latent_shape(self, sample_shape: Union[torch.Size, tuple[int, int, int, int]]) -> torch.Size:
+        if len(sample_shape) == 4:
+            return (sample_shape[0], self.config.latent_channels * self.config.in_channels,
+                    sample_shape[2] // 2 ** (self.num_levels-1),
+                    sample_shape[3] // 2 ** (self.num_levels-1))
+        else:
+            raise ValueError(f"Invalid sample shape: {sample_shape}")
+        
+    def get_sample_shape(self, latent_shape: Union[torch.Size, tuple[int, int, int, int]]) -> torch.Size:
+        if len(latent_shape) == 4:
+            return (latent_shape[0],
+                    self.config.out_channels,
+                    latent_shape[2] * 2 ** (self.num_levels-1),
+                    latent_shape[3] * 2 ** (self.num_levels-1))
+        else:
+            raise ValueError(f"Invalid latent shape: {latent_shape}")
+        
+    def encode(self, x: torch.Tensor, embeddings: torch.Tensor) -> torch.Tensor:
+
+        x = tensor_4d_to_5d(x, num_channels=self.config.in_channels // 2)
+        x = torch.cat((x, torch.ones_like(x[:, :1])), dim=1)
+
+        if embeddings is not None:
+            embeddings = embeddings.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)
+
+        for name, block in self.enc.items():
+            x = block(x) if "conv" in name else block(x, embeddings)
+        
+        latents = normalize(self.conv_latents_out(x))
+        return tensor_5d_to_4d(latents)
+    
+    def decode(self, x: torch.Tensor, embeddings: torch.Tensor) -> torch.Tensor:
+
+        x = tensor_4d_to_5d(x, num_channels=self.config.latent_channels)
+        x = torch.cat((x, torch.ones_like(x[:, :1])), dim=1)
+
+        if embeddings is not None:
+            embeddings = embeddings.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)
+
+        for block in self.dec.values():
+            x = block(x, embeddings)
+
+        return tensor_5d_to_4d(self.conv_out(x, gain=self.out_gain))
+    
+    def forward(self, samples: torch.Tensor, embeddings: torch.Tensor, add_latents_noise: float = 0) -> tuple[torch.Tensor, torch.Tensor]:
+        
+        latents = self.encode(samples, embeddings)
+        if add_latents_noise > 0:
+            latents = normalize(latents + torch.randn_like(latents))
+        
+        reconstructed = self.decode(latents, embeddings)
+
+        return latents, reconstructed
---
+++ src/training/module_trainers/dae_trainer_b1.py
@@ -0,0 +1,149 @@
+# MIT License
+#
+# Copyright (c) 2023 Christopher Friesen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from dataclasses import dataclass
+
+import torch
+
+from modules.formats.format import DualDiffusionFormat
+from modules.daes.dae_edm2_b1 import DualDiffusionDAE_EDM2_B1
+from modules.mp_tools import resample_3d, normalize
+from training.trainer import DualDiffusionTrainer
+from .module_trainer import ModuleTrainerConfig, ModuleTrainer
+from utils.dual_diffusion_utils import dict_str, tensor_4d_to_5d
+
+
+@dataclass
+class DAETrainerB1_Config(ModuleTrainerConfig):
+
+    kl_loss_weight: float = 0.1
+    kl_warmup_steps: int  = 1000
+
+    add_latents_noise: float = 0
+    
+    
+class DAETrainerB1(ModuleTrainer):
+    
+    @torch.no_grad()
+    def __init__(self, config: DAETrainerB1_Config, trainer: DualDiffusionTrainer) -> None:
+
+        self.config = config
+        self.trainer = trainer
+        self.logger = trainer.logger
+        self.dae: DualDiffusionDAE_EDM2_B1 = trainer.module
+        self.format: DualDiffusionFormat = trainer.pipeline.format.to(trainer.accelerator.device)
+
+        if trainer.config.enable_model_compilation:
+            self.format.compile(**trainer.config.compile_params)
+
+        self.logger.info("Training DAE model:")
+        self.logger.info(f"DAE Training params: {dict_str(config.__dict__)}")
+
+    @torch.no_grad()
+    def init_batch(self, validation: bool = False) -> None:
+        pass
+
+    def train_batch(self, batch: dict) -> dict[str, torch.Tensor]:
+
+        samples = self.format.raw_to_sample(batch["audio"]).detach().clone()
+        if self.trainer.config.enable_channels_last == True:
+            samples = samples.to(memory_format=torch.channels_last)
+
+        if "audio_embeddings" in batch:
+            dae_emb = self.dae.get_embeddings(batch["audio_embeddings"])
+        else:
+            dae_emb = None
+
+        latents, output_samples = self.dae(samples, dae_emb, add_latents_noise=self.config.add_latents_noise)
+
+        # output state kl loss
+        latents: torch.Tensor = tensor_4d_to_5d(latents, self.dae.config.latent_channels)
+
+        for state in hidden_states + [latents]:
+            weight = 1 if state is latents else 1 / len(hidden_states)
+
+            state_var = state.var(dim=1).clip(min=0.1)
+            state_mean = state.mean(dim=1)
+            kl_loss = (state_mean.square() + state_var - 1 - state_var.log()).mean(dim=(1,2,3)) * (weight / 2)
+
+            state_var = latents.var(dim=(2,3,4)).clip(min=0.1)
+            state_mean = latents.mean(dim=(2,3,4))
+            kl_loss = kl_loss + (state_mean.square() + state_var - 1 - state_var.log()).mean(dim=1) * (weight / 2)
+
+            #if state is latents:
+            #    state_var = latents.var(dim=4).clip(min=0.1)
+            #    state_mean = latents.mean(dim=4)
+            #    kl_loss = kl_loss + (state_mean.square() + state_var - 1 - state_var.log()).mean(dim=(1,2,3)) * (weight / 2)
+
+        # input/output sample kl loss
+        relative_var = (output_samples.var(dim=(1,2,3)) / samples.var(dim=(1,2,3))).clip(min=0.1, max=10)
+        relative_mean = samples.mean(dim=(1,2,3)) - output_samples.mean(dim=(1,2,3))
+        kl_loss = kl_loss + (relative_mean.square() + relative_var - 1 - relative_var.log())
+
+        # spectral energy kl loss
+        downsampled = latents
+        images = []
+        while downsampled.shape[-1] % 2 == 0 and downsampled.shape[-2] % 2 == 0:
+            images.append(downsampled)
+            downsampled = resample_3d(downsampled, "down")
+
+        energy_logs = {}    
+        octave_energies = []
+        total_octave_energy = torch.zeros_like(kl_loss)
+    
+        for i in range(len(images) - 1):
+            octave_energy = (images[i] - resample_3d(images[i+1], "up")).square().mean(dim=(1,2,3,4))
+            energy_logs[f"octave_energies/{i}"] = octave_energy.mean().detach()
+            octave_energies.append(octave_energy)
+            #total_octave_energy = total_octave_energy + octave_energy
+            total_octave_energy = torch.max(total_octave_energy, octave_energy)
+
+        if self.config.octave_energy_kl_weight > 0:
+            #avg_octave_energy = total_octave_energy / (len(images) - 1)
+            avg_octave_energy = (total_octave_energy * 1.1).detach()
+            for energy in octave_energies:
+                relative_var = (energy / avg_octave_energy).clip(min=0.1, max=10)
+                kl_loss = kl_loss + (relative_var - 1 - relative_var.log()) / len(octave_energies) * self.config.octave_energy_kl_weight
+
+        # recon loss
+        #recon_loss = torch.nn.functional.mse_loss(output_samples, samples, reduction="none").mean(dim=(1,2,3))
+        recon_loss = (samples - output_samples).abs().mean(dim=(1,2,3))
+        recon_loss_logvar = self.dae.get_recon_loss_logvar()
+        recon_loss_nll = recon_loss / recon_loss_logvar.exp() + recon_loss_logvar
+
+        kl_loss_weight = self.config.kl_loss_weight
+        if self.trainer.global_step < self.config.kl_warmup_steps:
+            kl_loss_weight *= self.trainer.global_step / self.config.kl_warmup_steps
+
+        return {
+            "loss": kl_loss * kl_loss_weight + recon_loss_nll,
+            "loss/recon_nll": recon_loss_nll.mean().detach(),
+            "loss/recon": recon_loss.mean().detach(),
+            "loss/kl": kl_loss.mean().detach(),
+            "latents/mean": latents.mean().detach(),
+            "latents/std": latents.std().detach(),
+            **energy_logs
+        }
+
+    @torch.no_grad()
+    def finish_batch(self) -> dict[str, torch.Tensor]:
+        return {}
