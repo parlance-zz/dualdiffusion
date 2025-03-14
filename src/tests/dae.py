5ef8a8d56e6bc0ea4aafe5920784afae78f7b2de
parlance-zz <parlance@fifth-harmonic.com>
2025-03-13 20:57:40 -0600

    ddec_mclt4 fine-tuning

--- src/tests/dae.py
+++ src/tests/dae.py
@@ -57,7 +57,7 @@ def dae_test() -> None:
     model_path = os.path.join(config.MODELS_PATH, model_name)
     print(f"Loading DualDiffusion model from '{model_path}'...")
     pipeline = DualDiffusionPipeline.from_pretrained(model_path, **model_load_options)
-    dae: DualDiffusionDAE_EDM2_A1 = pipeline.dae
+    #dae: DualDiffusionDAE_EDM2_A1 = pipeline.dae
     format: SpectrogramFormat = pipeline.format
     embedding: CLAP_Embedding = pipeline.embedding
 
@@ -72,20 +72,21 @@ def dae_test() -> None:
         ddec: DDec_MCLT_UNet = pipeline.ddec
         last_global_step = ddec.config.last_global_step
     else:
-        last_global_step = dae.config.last_global_step
+        #last_global_step = dae.config.last_global_step
+        pass
     model_metadata = {"model_metadata": dict_str(pipeline.model_metadata)}
     print(f"{model_metadata['model_metadata']}\n")
 
     dataset_path = config.DATASET_PATH
     test_samples: list[str] = test_params["test_samples"] or []
-    sample_shape = pipeline.get_sample_shape(length=length)
-    latent_shape = pipeline.get_latent_shape(sample_shape)
-    print(f"Sample shape: {sample_shape}  Latent shape: {latent_shape}")
+    #sample_shape = pipeline.get_sample_shape(length=length)
+    #latent_shape = pipeline.get_latent_shape(sample_shape)
+    #print(f"Sample shape: {sample_shape}  Latent shape: {latent_shape}")
     
     output_path = os.path.join(model_path, "output", "dae", f"step_{last_global_step}")
     os.makedirs(output_path, exist_ok=True)
     start_time = datetime.datetime.now()
-    avg_point_similarity = avg_latents_mean = avg_latents_std = 0
+    #avg_point_similarity = avg_latents_mean = avg_latents_std = 0
 
     add_random_test_samples = test_params["add_random_test_samples"]
     if add_random_test_samples > 0:
@@ -116,8 +117,8 @@ def dae_test() -> None:
             audio_embedding = normalize(embedding.encode_audio(
                 input_raw_sample, sample_rate=format.config.sample_rate).mean(dim=0, keepdim=True)).float()
 
-        dae_embedding = dae.get_embeddings(audio_embedding)
-        latents = dae.encode(input_sample.to(dtype=dae.dtype), dae_embedding)
+        #dae_embedding = dae.get_embeddings(audio_embedding)
+        #latents = dae.encode(input_sample.to(dtype=dae.dtype), dae_embedding)
         #output_sample = dae.decode(latents, dae_embedding)
         output_sample = format.convert_to_abs_exp1(input_sample)
         #output_sample = input_sample
@@ -132,36 +133,36 @@ def dae_test() -> None:
                 #num_steps=200, length=audio_len, cfg_scale=1.5, input_perturbation=1, input_perturbation_offset=0,
                 #use_heun=False, schedule="edm2", rho=7, sigma_max=12, sigma_min=0.0003 # best
 
-                num_steps=300, length=audio_len, cfg_scale=1.5, input_perturbation=1, input_perturbation_offset=0,
-                use_heun=False, schedule="edm2", rho=7, sigma_max=20, sigma_min=0.005 #0.013
+                num_steps=300, length=audio_len, cfg_scale=5, input_perturbation=1, input_perturbation_offset=0,
+                use_heun=False, schedule="linear", rho=7, sigma_max=12, sigma_min=0.0003 #0.013
             )
             output_sample = pipeline.diffusion_decode(
                 ddec_params, audio_embedding=audio_embedding,
                 x_ref=output_sample.to(dtype=ddec.dtype), module_name="ddec")
 
-        point_similarity = (output_sample - input_sample).abs().mean().item()
+        #point_similarity = (output_sample - input_sample).abs().mean().item()
 
         print(f"input   mean/std: {input_sample.mean().item():.4} {input_sample.std().item():.4}")
         print(f"output  mean/std: {output_sample.mean().item():.4} {output_sample.std().item():.4}")
-        print(f"latents mean/std: {latents.mean().item():.4} {latents.std().item():.4}")
-        print(f"decoded point similarity: {point_similarity}")
+        #print(f"latents mean/std: {latents.mean().item():.4} {latents.std().item():.4}")
+        #print(f"decoded point similarity: {point_similarity}")
         
         #output_raw_sample = format.sample_to_raw(output_sample.type(torch.float32))
         from modules.formats.mclt import DualMCLTFormatConfig, DualMCLTFormat
         mclt = DualMCLTFormat(DualMCLTFormatConfig())
         output_raw_sample = mclt.sample_to_raw(output_sample.float())
         
-        latents_mean = latents.mean().item()
-        latents_std = latents.std().item()
-        avg_point_similarity += point_similarity
-        avg_latents_mean += latents_mean
-        avg_latents_std += latents_std
+        #latents_mean = latents.mean().item()
+        #latents_std = latents.std().item()
+        #avg_point_similarity += point_similarity
+        #avg_latents_mean += latents_mean
+        #avg_latents_std += latents_std
         filename = os.path.basename(filename)
 
         metadata = {**model_metadata}
         metadata["ddec_metadata"] = dict_str(ddec_params.__dict__) if test_params["test_ddec"] == True else "null"
 
-        save_img(dae.latents_to_img(latents), os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_latents.png')}"))
+        #save_img(dae.latents_to_img(latents), os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_latents.png')}"))
         save_img(format.sample_to_img(output_sample), os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_output_sample.png')}"))
         save_img(format.sample_to_img(input_sample), os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_input_sample.png')}"))
 
@@ -179,14 +180,14 @@ def dae_test() -> None:
             save_audio(source_raw_sample, sample_rate, output_flac_file_path, target_lufs=None)
             print(f"Saved flac output to {output_flac_file_path}")
         
-        latents_fft = tensor_4d_to_5d(torch.fft.rfft2(latents.float(), norm="ortho").abs().clip(min=1e-6).log(), num_channels=4)
-        save_img(tensor_to_img(latents_fft, flip_y=True), os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_latents_fft_ln_psd.png')}"))
-        latents_fft.cpu().numpy().tofile(os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_latents_fft_ln_psd.raw')}"))
+        #latents_fft = tensor_4d_to_5d(torch.fft.rfft2(latents.float(), norm="ortho").abs().clip(min=1e-6).log(), num_channels=4)
+        #save_img(tensor_to_img(latents_fft, flip_y=True), os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_latents_fft_ln_psd.png')}"))
+        #latents_fft.cpu().numpy().tofile(os.path.join(output_path, f"step_{last_global_step}_{filename.replace(file_ext, '_latents_fft_ln_psd.raw')}"))
 
     print(f"\nFinished in: {datetime.datetime.now() - start_time}")
-    print(f"Avg Point similarity: {avg_point_similarity / len(test_samples)}")
-    print(f"Latents avg mean: {avg_latents_mean / len(test_samples)}")
-    print(f"Latents avg std: {avg_latents_std / len(test_samples)}")
+    #print(f"Avg Point similarity: {avg_point_similarity / len(test_samples)}")
+    #print(f"Latents avg mean: {avg_latents_mean / len(test_samples)}")
+    #print(f"Latents avg std: {avg_latents_std / len(test_samples)}")
 
 if __name__ == "__main__":
 
