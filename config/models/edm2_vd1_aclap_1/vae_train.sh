3cc7c369f3237ed5df5953e9fcb1fa3c0f2c5913
parlance-zz <parlance@fifth-harmonic.com>
2025-03-14 11:22:07 -0600

    remove old model config templates

--- config/models/edm2_vd1_aclap_1/vae_train.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-accelerate launch --config_file "/home/parlance/dualdiffusion/models/edm2_vae_d1_ddec/training/vae_accelerate.yaml" "/home/parlance/dualdiffusion/src/train.py" --model_path="/home/parlance/dualdiffusion/models/edm2_vae_d1_ddec" --train_config_path="/home/parlance/dualdiffusion/models/edm2_vae_d1_ddec/training/vae_train.json"
