d821fd215003fc47e86dc6febcdd98678246170c
parlance-zz <parlance@fifth-harmonic.com>
2025-03-24 20:52:43 -0600

    add dae e1 and associated trainer

---
+++ src/modules/daes/dae_edm2_e1.py
@@ -0,0 +1,359 @@
+# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# This work is licensed under a Creative Commons
+# Attribution-NonCommercial-ShareAlike 4.0 International License.
+# You should have received a copy of the license along with this
+# work. If not, see http://creativecommons.org/licenses/by-nc-sa/4.0/
+
+"""Improved diffusion model architecture proposed in the paper
+"Analyzing and Improving the Training Dynamics of Diffusion Models"."""
+
+# Modifications under MIT License
+#
+# Copyright (c) 2023 Christopher Friesen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from dataclasses import dataclass
+from typing import Union, Literal
+
+import torch
+
+from utils.dual_diffusion_utils import tensor_4d_to_5d, tensor_5d_to_4d
+from modules.daes.dae import DualDiffusionDAE, DualDiffusionDAEConfig
+from modules.mp_tools import (
+    mp_silu, mp_sum, normalize, MPConv3D, channel_to_space3d,
+    wavelet_decompose2d, wavelet_recompose2d
+)
+
+
+@dataclass
+class DAE_E1_Config(DualDiffusionDAEConfig):
+
+    in_channels: int     = 1
+    in_channels_emb: int = 1024
+    in_num_freqs: int    = 256
+    out_channels: int    = 1
+    latent_channels: int = 4
+
+    model_channels: int       = 32           # Base multiplier for the number of channels.
+    channel_mult_enc: int     = 2 
+    channel_mult_dec: list[int] = (2,2,4,4)  # Per-resolution multipliers for the number of channels.
+    channel_mult_dif: list[int] = (1,1,1,1)  # Per-resolution multipliers for the number of channels.
+    channel_mult_emb: int     = 4
+    channels_per_head: int    = 64           # Number of channels per attention head.
+    num_enc_layers: int       = 8       
+    num_dec_layers_per_block: int = 3
+    num_dif_layers_per_block: int = 1
+    res_balance: float        = 0.35         # Balance between main branch (0) and residual branch (1).
+    attn_balance: float       = 0.35         # Balance between main branch (0) and self-attention (1).
+    attn_levels: list[int]    = ()           # List of resolution levels to use self-attention.
+    mlp_multiplier: int    = 2               # Multiplier for the number of channels in the MLP.
+    mlp_groups: int        = 1               # Number of groups for the MLPs.
+    emb_linear_groups: int = 1
+    add_constant_channel: bool = True
+    add_pixel_norm: bool       = False
+
+class Block(torch.nn.Module):
+
+    def __init__(self,
+        level: int,                             # Resolution level.
+        in_channels: int,                       # Number of input channels.
+        out_channels: int,                      # Number of output channels.
+        emb_channels: int,                      # Number of embedding channels.
+        flavor: Literal["enc", "dec"] = "enc",
+        resample_mode: Literal["keep", "up", "down"] = "keep",
+        dropout: float         = 0.,       # Dropout probability.
+        res_balance: float     = 0.3,      # Balance between main branch (0) and residual branch (1).
+        attn_balance: float    = 0.3,      # Balance between main branch (0) and self-attention (1).
+        clip_act: float        = 256,      # Clip output activations. None = do not clip.
+        mlp_multiplier: int    = 1,        # Multiplier for the number of channels in the MLP.
+        mlp_groups: int        = 1,        # Number of groups for the MLP.
+        emb_linear_groups: int = 1,
+        channels_per_head: int = 64,       # Number of channels per attention head.
+        use_attention: bool    = False,    # Use self-attention in this block.
+        use_pixel_norm: bool   = False,
+        res_kernel: tuple[int, int, int] = (2,3,3),
+    ) -> None:
+        super().__init__()
+
+        self.level = level
+        self.use_attention = use_attention
+        self.use_pixel_norm = use_pixel_norm
+        self.num_heads = out_channels // channels_per_head
+        self.out_channels = out_channels
+        self.flavor = flavor
+        self.resample_mode = resample_mode
+        self.dropout = dropout
+        self.res_balance = res_balance
+        self.attn_balance = attn_balance
+        self.clip_act = clip_act
+
+        if resample_mode == "up":
+            in_channels = in_channels // 4
+
+        self.conv_res0 = MPConv3D(in_channels, out_channels * mlp_multiplier, kernel=res_kernel, groups=mlp_groups)
+        self.conv_res1 = MPConv3D(out_channels * mlp_multiplier, out_channels, kernel=res_kernel, groups=mlp_groups)
+
+        self.emb_gain = torch.nn.Parameter(torch.zeros([]))
+        self.emb_linear = MPConv3D(emb_channels, out_channels * mlp_multiplier,
+            kernel=(1,1,1), groups=emb_linear_groups) if emb_channels != 0 else None
+        
+        if self.use_attention:
+            self.emb_gain_qk = torch.nn.Parameter(torch.zeros([]))
+            self.emb_gain_v = torch.nn.Parameter(torch.zeros([]))
+            if emb_channels != 0:
+                self.emb_linear_qk = MPConv3D(emb_channels, out_channels, kernel=(1,1,1), groups=1)
+                self.emb_linear_v = MPConv3D(emb_channels, out_channels, kernel=(1,1,1), groups=1)
+            else:
+                self.emb_linear_qk = None
+                self.emb_linear_v = None
+
+            self.attn_qk = MPConv3D(out_channels, out_channels * 2, kernel=(1,1,1))
+            self.attn_v = MPConv3D(out_channels, out_channels, kernel=(1,1,1))
+            self.attn_proj = MPConv3D(out_channels, out_channels, kernel=(1,1,1))
+
+    def forward(self, x: torch.Tensor, emb: torch.Tensor) -> torch.Tensor:
+        
+        if self.resample_mode == "up":
+            x = channel_to_space3d(x)
+
+        if self.flavor == "enc":
+            if self.use_pixel_norm == True:
+                x = normalize(x, dim=1)
+
+        y = self.conv_res0(mp_silu(x))
+
+        if self.emb_linear is not None:
+            c = self.emb_linear(emb, gain=self.emb_gain) + 1.
+            y = mp_silu(y * c)
+        else:
+            y = mp_silu(y)
+
+        if self.dropout != 0 and self.training == True: # magnitude preserving fix for dropout
+            y = torch.nn.functional.dropout(y, p=self.dropout) * (1. - self.dropout)**0.5
+
+        y = self.conv_res1(y)
+
+        x = mp_sum(x, y, t=self.res_balance)
+        
+        if self.use_attention:
+
+            c = self.emb_linear_qk(emb, gain=self.emb_gain_qk) + 1.
+
+            qk = self.attn_qk(x * c)
+            qk = qk.reshape(qk.shape[0], self.num_heads, -1, 2, y.shape[2] * y.shape[3])
+            q, k = normalize(qk, dim=2).unbind(3)
+
+            v = self.attn_v(x)
+            v = v.reshape(v.shape[0], self.num_heads, -1, y.shape[2] * y.shape[3])
+            v = normalize(v, dim=2)
+
+            y = torch.nn.functional.scaled_dot_product_attention(q.transpose(-1, -2),
+                                                                 k.transpose(-1, -2),
+                                                                 v.transpose(-1, -2)).transpose(-1, -2)
+            y = y.reshape(*x.shape)
+
+            c = self.emb_linear_v(emb, gain=self.emb_gain_v) + 1.
+            y = mp_silu(y * c)
+
+            y = self.attn_proj(y)
+            x = mp_sum(x, y, t=self.attn_balance)
+
+        if self.clip_act is not None:
+            x = x.clip_(-self.clip_act, self.clip_act)
+
+        if self.clip_act is not None:
+            x = x.clip_(-self.clip_act, self.clip_act)
+        return x
+
+class DAE_E1(DualDiffusionDAE):
+
+    supports_channels_last: Union[bool, Literal["3d"]] = "3d"
+
+    def __init__(self, config: DAE_E1_Config) -> None:
+        super().__init__()
+        self.config = config
+
+        block_kwargs = {"mlp_multiplier": config.mlp_multiplier,
+                        "mlp_groups": config.mlp_groups,
+                        "emb_linear_groups": config.emb_linear_groups,
+                        "res_balance": config.res_balance,
+                        "attn_balance": config.attn_balance,
+                        "channels_per_head": config.channels_per_head,
+                        "use_pixel_norm": config.add_pixel_norm}
+        
+        self.num_levels = len(config.channel_mult_dec)
+
+        cemb = config.model_channels * config.channel_mult_emb * config.mlp_multiplier if config.in_channels_emb > 0 else 0
+        cdec = [config.model_channels * m for m in config.channel_mult_dec]
+        cdif = [config.model_channels * m for m in config.channel_mult_dif]
+
+        assert len(cdec) == len(cdif) == self.num_levels
+
+        self.recon_loss_logvar = torch.nn.Parameter(torch.zeros(self.num_levels))
+        self.recon_loss_logvar_dif = torch.nn.Parameter(torch.zeros(self.num_levels))
+
+        # Embedding.
+        if config.in_channels_emb > 0:
+            self.emb_label = MPConv3D(config.in_channels_emb, cemb, kernel=())
+            self.emb_dim = cemb
+        else:
+            cemb = self.emb_dim = 0
+            self.emb_label = None
+
+        in_channels, out_channels = 1 + int(config.add_constant_channel), 1
+        enc_channels = config.model_channels * config.channel_mult_enc
+
+        # Encoder.
+        self.enc = torch.nn.ModuleDict()
+        self.enc[f"conv_in"] = MPConv3D(in_channels, enc_channels, kernel=(2,3,3))
+        
+        for idx in range(config.num_enc_layers):
+            self.enc[f"block{level}_layer{idx}"] = Block(0, enc_channels, enc_channels, cemb,
+                use_attention=0 in config.attn_levels, flavor="enc", **block_kwargs)
+
+        self.conv_latents_out = MPConv3D(enc_channels, config.latent_channels, kernel=(2,3,3))
+        self.conv_latents_in = MPConv3D(config.latent_channels + int(config.add_constant_channel), cdec[-1], kernel=(2,3,3))
+
+        # Decoder.
+        self.dec = torch.nn.ModuleDict()
+        cin = cdec[-1]
+
+        for level, cout in zip(reversed(range(self.num_levels)), reversed(cdec)):
+            
+            if level == self.num_levels - 1:
+                self.dec[f"block{level}_in"] = Block(level, cin, cout, cemb,
+                    use_attention=level in config.attn_levels, flavor="dec", **block_kwargs)
+            else:
+                self.dec[f"block{level}_up"] = Block(level, cin, cout, cemb,
+                    use_attention=level in config.attn_levels, flavor="dec", resample_mode="up", **block_kwargs)
+                
+            for idx in range(config.num_dec_layers_per_block):
+                self.dec[f"block{level}_layer{idx}"] = Block(level, cout, cout, cemb,
+                    use_attention=level in config.attn_levels, flavor="dec", **block_kwargs)
+
+            dif = torch.nn.ModuleDict()
+            dif.level = level
+            self.dec[f"block{level}_dif"] = dif
+
+            dif["conv_dec_out"] = MPConv3D(cout, out_channels, kernel=(2,3,3), out_gain_param=True)
+            dif["conv_dif_in"] = MPConv3D(out_channels, cdif[-1], kernel=(2,3,3))
+
+            for idx in range(config.num_dif_layers_per_block):
+                dif[f"dif_layer{idx}"] = Block(level, cdif[-1], cdif[-1], cemb,
+                    use_attention=level in config.attn_levels, flavor="dec", **block_kwargs)
+
+            dif[f"conv_dif_out"] = MPConv3D(cdif[-1], out_channels, kernel=(2,3,3), out_gain_param=True)
+
+            cin = cout
+            cdif.pop()
+            
+    def get_embeddings(self, emb_in: torch.Tensor) -> torch.Tensor:
+        if self.emb_label is not None:
+            return self.emb_label(normalize(emb_in).to(device=self.device, dtype=self.dtype))
+        else:
+            return None
+    
+    def get_recon_loss_logvar(self) -> torch.Tensor:
+        return self.recon_loss_logvar
+    
+    def get_latent_shape(self, sample_shape: Union[torch.Size, tuple[int, int, int, int]]) -> torch.Size:
+        if len(sample_shape) == 4:
+            return (sample_shape[0], self.config.latent_channels * 2,
+                    sample_shape[2] // 2 ** (self.num_levels-1),
+                    sample_shape[3] // 2 ** (self.num_levels-1))
+        else:
+            raise ValueError(f"Invalid sample shape: {sample_shape}")
+        
+    def get_sample_shape(self, latent_shape: Union[torch.Size, tuple[int, int, int, int]]) -> torch.Size:
+        if len(latent_shape) == 4:
+            return (latent_shape[0], 2,
+                    latent_shape[2] * 2 ** (self.num_levels-1),
+                    latent_shape[3] * 2 ** (self.num_levels-1))
+        else:
+            raise ValueError(f"Invalid latent shape: {latent_shape}")
+        
+    def encode(self, x: torch.Tensor, embeddings: torch.Tensor) -> torch.Tensor:
+
+        x = tensor_4d_to_5d(x, num_channels=1).to(memory_format=torch.channels_last_3d)
+
+        if self.config.add_constant_channel == True:
+            x = torch.cat((x, torch.ones_like(x[:, :1])), dim=1)
+
+        if embeddings is not None:
+            embeddings = embeddings.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)
+
+        for name, block in self.enc.items():
+            x = block(x) if "conv" in name else block(x, embeddings)
+        
+        latents = tensor_5d_to_4d(self.conv_latents_out(x)).to(memory_format=torch.channels_last)
+        return normalize(torch.nn.functional.avg_pool2d(latents, 2**(self.num_levels-1)))
+
+    def decode(self, x: torch.Tensor, embeddings: torch.Tensor,
+            return_training_output: bool = False) -> Union[torch.Tensor, tuple[torch.Tensor, torch.Tensor, torch.Tensor]]:
+
+        x = tensor_4d_to_5d(x, num_channels=self.config.latent_channels).to(memory_format=torch.channels_last_3d)
+
+        if self.config.add_constant_channel == True:
+            x = torch.cat((x, torch.ones_like(x[:, :1])), dim=1)
+
+        x = self.conv_latents_in(x)
+
+        if embeddings is not None:
+            embeddings = embeddings.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)
+
+        dec_outputs, dif_outputs, dif_noise = [], [], []
+        for name, block in self.dec.items():
+            if "dif" in name:
+                dif: torch.nn.ModuleDict = block
+
+                dec_out = dif["conv_dec_out"](x)
+                dec_outputs.append(tensor_5d_to_4d(dec_out))
+
+                noise = torch.randn_like(dec_out) * (self.recon_loss_logvar[dif.level] / 2).exp().detach()
+                dif_noise.append(tensor_5d_to_4d(noise))
+
+                y = dif["conv_dif_in"](dec_out.detach() + noise)
+                for idx in range(self.config.num_dif_layers_per_block):
+                    y = dif[f"dif_layer{idx}"](y, embeddings)
+                
+                dif_out = dif["conv_dif_out"](y)
+                dif_outputs.append(tensor_5d_to_4d(dif_out))
+
+            else:
+                x = block(x, embeddings)
+
+        dec_outputs.reverse()
+        dif_outputs.reverse()
+
+        if return_training_output == True:
+            dif_noise.reverse()
+            return dec_outputs, dif_outputs, dif_noise
+        else:
+            return wavelet_recompose2d(dec_outputs) + wavelet_recompose2d(dif_noise) - wavelet_recompose2d(dif_outputs)
+    
+    def forward(self, samples: torch.Tensor, dae_embeddings: torch.Tensor) -> tuple[torch.Tensor, ...]:
+        
+        latents = self.encode(samples, dae_embeddings)
+        latents_pre_norm_std = latents.std(dim=(1,2,3))
+        
+        dec_outputs, dif_outputs, dif_noise = self.decode(latents, dae_embeddings, return_training_output=True)
+
+        return (latents, latents_pre_norm_std) + (dec_outputs, dif_outputs, dif_noise)
---
+++ src/training/module_trainers/dae_trainer_e1.py
@@ -0,0 +1,131 @@
+# MIT License
+#
+# Copyright (c) 2023 Christopher Friesen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from dataclasses import dataclass
+
+import torch
+
+from training.trainer import DualDiffusionTrainer
+from .module_trainer import ModuleTrainerConfig, ModuleTrainer
+from modules.daes.dae_edm2_e1 import DAE_E1
+from modules.formats.spectrogram import SpectrogramFormat
+from modules.mp_tools import normalize, wavelet_decompose2d, wavelet_recompose2d
+
+
+@dataclass
+class DAETrainer_E1_Config(ModuleTrainerConfig):
+
+    add_latents_noise: float = 0
+    kl_loss_weight: float = 2e-2
+    kl_warmup_steps: int  = 1000
+
+class DAETrainer_E1(ModuleTrainer):
+    
+    @torch.no_grad()
+    def __init__(self, config: DAETrainer_E1_Config, trainer: DualDiffusionTrainer) -> None:
+
+        self.config = config
+        self.trainer = trainer
+        self.logger = trainer.logger
+        self.module: DAE_E1 = trainer.module
+
+        self.is_validation_batch = False
+        self.device_generator = None
+        self.cpu_generator = None
+
+        self.format: SpectrogramFormat = trainer.pipeline.format.to(self.trainer.accelerator.device)
+
+        if trainer.config.enable_model_compilation:
+            self.module.compile(**trainer.config.compile_params)
+            self.format.compile(**trainer.config.compile_params)
+
+        self.logger.info("Training DAE model:")
+        self.logger.info(f"KL loss weight: {self.config.kl_loss_weight} KL warmup steps: {self.config.kl_warmup_steps}")
+
+    @torch.no_grad()
+    def init_batch(self, validation: bool = False) -> None:
+        pass
+
+    def train_batch(self, batch: dict) -> dict[str, torch.Tensor]:
+
+        if "audio_embeddings" in batch:
+            sample_audio_embeddings = normalize(batch["audio_embeddings"])
+            dae_embeddings = self.module.get_embeddings(sample_audio_embeddings)
+        else:
+            dae_embeddings = None
+        
+        spec_samples = self.format.raw_to_sample(batch["audio"]).clone().detach()
+
+        latents, latents_pre_norm_std, dec_outputs, dif_outputs, dif_noise = self.module(spec_samples, dae_embeddings)
+
+        reconstructed = wavelet_recompose2d(dec_outputs) + wavelet_recompose2d(dif_noise) - wavelet_recompose2d(dif_outputs)
+        spec_wavelets = wavelet_decompose2d(spec_samples, self.module.num_levels)
+    
+        dec_loss = torch.zeros(spec_samples.shape[0], device=spec_samples.device)
+        dec_loss_nll = torch.zeros_like(dec_loss)
+        dif_loss = torch.zeros_like(dec_loss)
+        dif_loss_nll = torch.zeros_like(dec_loss)
+
+        kl_loss = latents.mean(dim=(1,2,3)).square() + latents_pre_norm_std.square() - 1 - latents_pre_norm_std.square().log()
+
+        logs = {}
+
+        for i, (spec, dec, dif, noise) in enumerate(zip(spec_wavelets, dec_outputs, dif_outputs, dif_noise)):
+
+            _dec_loss = torch.nn.functional.mse_loss(dec, spec, reduction="none").mean(dim=(1,2,3))
+            dec_loss = dec_loss + _dec_loss
+            dec_loss_nll = dec_loss_nll + _dec_loss / self.module.recon_loss_logvar[i].exp() + self.module.recon_loss_logvar[i]
+            logs[f"loss/dec_level{i}"] = _dec_loss
+
+            _dif_loss = torch.nn.functional.mse_loss(dif, noise, reduction="none").mean(dim=(1,2,3))
+            dif_loss = dif_loss + _dif_loss
+            dif_loss_nll = dif_loss_nll + _dif_loss / self.module.recon_loss_logvar_dif[i].exp() + self.module.recon_loss_logvar_dif[i]
+            logs[f"loss/dif_level{i}"] = _dec_loss
+
+            logs[f"io_stats/level{i}_noise_std"] = noise.std(dim=(1,2,3))
+            logs[f"io_stats/level{i}_dec_std"] = dec.std(dim=(1,2,3))
+            logs[f"io_stats/level{i}_spec_std"] = spec.std(dim=(1,2,3))
+
+        kl_loss_weight = self.config.kl_loss_weight
+        if self.trainer.global_step < self.config.kl_warmup_steps:
+            kl_loss_weight *= self.trainer.global_step / self.config.kl_warmup_steps
+
+        logs.update({
+            "loss": dec_loss_nll + dif_loss_nll + kl_loss * kl_loss_weight,
+            "loss/dec": dec_loss,
+            "loss/dif": dif_loss,
+            "loss/kl": kl_loss,
+            "loss_weight/kl": kl_loss_weight,
+            "io_stats/input_std": spec_samples.std(dim=(1,2,3)),
+            "io_stats/input_mean": spec_samples.mean(dim=(1,2,3)),
+            "io_stats/output_std": reconstructed.std(dim=(1,2,3)),
+            "io_stats/output_mean": reconstructed.mean(dim=(1,2,3)),
+            "io_stats/latents_std": latents.std(dim=(1,2,3)),
+            "io_stats/latents_mean": latents.mean(dim=(1,2,3)),
+            "io_stats/latents_pre_norm_std": latents_pre_norm_std
+        })
+
+        return logs
+
+    @torch.no_grad()
+    def finish_batch(self) -> dict[str, torch.Tensor]:
+        return {}
