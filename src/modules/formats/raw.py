f610a494bc90d484ad96dc12127e3cde0b17857a
parlance-zz <parlance@fifth-harmonic.com>
2025-06-17 09:26:42 -0600

    make mel density scaling in raw_format optional, add dual_channel option for analytic signal

--- src/modules/formats/raw.py
+++ src/modules/formats/raw.py
@@ -35,6 +35,8 @@ class RawFormatConfig(DualDiffusionFormatConfig):
     sample_rate: int = 32000
     num_raw_channels: int = 2
     default_raw_length: int = 1409024
+    dual_channel: bool = False
+    mel_density_scaling: bool = True
 
     scale: float = 39.05
     width_alignment: int = 2048
@@ -50,8 +52,8 @@ class RawFormat(DualDiffusionFormat):
         return raw_length // self.config.width_alignment * self.config.width_alignment
 
     def get_raw_sample_shape(self, bsz: int = 1, raw_length: Optional[int] = None) -> tuple[int, int, int, int]:
-        crop_width = self.get_raw_crop_width(raw_length)
-        return (bsz, 1, self.config.num_raw_channels, crop_width)
+        return (bsz, int(self.config.dual_channel + 1),
+            self.config.num_raw_channels, self.get_raw_crop_width(raw_length))
 
     @torch.no_grad()
     def scale(self, raw_samples: torch.Tensor, random_phase_augmentation: bool = False) -> torch.Tensor:
@@ -64,25 +66,40 @@ class RawFormat(DualDiffusionFormat):
             phase_rotation = torch.exp(2j * torch.pi * torch.rand(rfft.shape[0], device=rfft.device))
             rfft *= phase_rotation.view(-1, 1, 1)
 
-        rfft_freq = torch.fft.rfftfreq(raw_samples.shape[-1], d=1/self.config.sample_rate, device=raw_samples.device)
-        mel_density = get_mel_density(rfft_freq)
-        mel_density /= mel_density.mean()
-        raw_samples = torch.fft.irfft(rfft / mel_density.view(1, 1,-1), n=raw_samples.shape[-1], dim=-1, norm="ortho")
+        if self.config.mel_density_scaling == True:
+            rfft_freq = torch.fft.rfftfreq(raw_samples.shape[-1], d=1/self.config.sample_rate, device=raw_samples.device)
+            mel_density = get_mel_density(rfft_freq)
+            mel_density /= mel_density.mean()
+            rfft /= mel_density.view(1, 1,-1)
 
-        return raw_samples[..., raw_len//2:-raw_len//2].unsqueeze(1).contiguous() * self.config.scale
+        if self.config.dual_channel == False:
+            raw_samples = torch.fft.irfft(rfft, n=raw_samples.shape[-1], dim=-1, norm="ortho")
+            return raw_samples[..., raw_len//2:-raw_len//2].unsqueeze(1).contiguous() * self.config.scale
+        else:
+            raw_samples = torch.fft.ifft(rfft, n=raw_samples.shape[-1], dim=-1, norm="ortho")
+            raw_samples = torch.stack((raw_samples.real, raw_samples.imag), dim=1)
+            return raw_samples[..., raw_len//2:-raw_len//2].contiguous() * (self.config.scale * 2)
 
     @torch.no_grad()
     def unscale(self, raw_samples: torch.Tensor):
         
-        raw_samples = raw_samples.squeeze(1)
-        
-        raw_len = raw_samples.shape[-1]
-        raw_samples = torch.nn.functional.pad(raw_samples.float(), (raw_len // 2, raw_len // 2), mode="reflect")
-        rfft: torch.Tensor = torch.fft.rfft(raw_samples, dim=-1, norm="ortho")
+        if self.config.dual_channel == False:
+            raw_samples = raw_samples[:, 0]
+            raw_len = raw_samples.shape[-1]
+            raw_samples = torch.nn.functional.pad(raw_samples.float(), (raw_len // 2, raw_len // 2), mode="reflect")
+            rfft: torch.Tensor = torch.fft.rfft(raw_samples, dim=-1, norm="ortho")
+        else:
+            raw_samples = raw_samples[:, 0].float() + 1j * raw_samples[:, 1].float()
+            raw_len = raw_samples.shape[-1]
+            raw_samples = torch.nn.functional.pad(raw_samples, (raw_len // 2, raw_len // 2), mode="reflect")
+            rfft: torch.Tensor = torch.fft.fft(raw_samples, dim=-1, norm="ortho")
+            rfft = rfft[..., :rfft.shape[-1] // 2 + 1]
 
-        rfft_freq = torch.fft.rfftfreq(raw_samples.shape[-1], d=1/self.config.sample_rate, device=raw_samples.device)
-        mel_density = get_mel_density(rfft_freq)
-        mel_density /= mel_density.mean()
-        raw_samples = torch.fft.irfft(rfft * mel_density.view(1, 1,-1), n=raw_samples.shape[-1], dim=-1, norm="ortho")
+        if self.config.mel_density_scaling == True:
+            rfft_freq = torch.fft.rfftfreq(raw_samples.shape[-1], d=1/self.config.sample_rate, device=raw_samples.device)
+            mel_density = get_mel_density(rfft_freq)
+            mel_density /= mel_density.mean()
+            rfft *= mel_density.view(1, 1,-1)
 
+        raw_samples = torch.fft.irfft(rfft, n=raw_samples.shape[-1], dim=-1, norm="ortho")
         return raw_samples[..., raw_len//2:-raw_len//2].contiguous() / self.config.scale
