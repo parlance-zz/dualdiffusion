0a981baea6e869c528d7e0b7611a2bb11fccf767
parlance-zz <parlance@fifth-harmonic.com>
2025-08-06 13:41:56 -0600

    add mdct_psd format and test

---
+++ src/modules/formats/mdct_psd.py
@@ -0,0 +1,237 @@
+# MIT License
+#
+# Copyright (c) 2023 Christopher Friesen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from typing import Optional, Literal
+from dataclasses import dataclass
+
+import torch
+
+from modules.formats.format import DualDiffusionFormat, DualDiffusionFormatConfig
+from modules.formats.frequency_scale import get_mel_density
+from utils.dual_diffusion_utils import tensor_to_img
+from utils.mdct import MDCT, IMDCT, MDCT2, IMDCT2, get_window_fn
+
+
+@dataclass()
+class MDCT_PSD_FormatConfig(DualDiffusionFormatConfig):
+
+    sample_rate: int = 32000
+    num_raw_channels: int = 2
+
+    low_cut_start_hz: float = 28.862
+    low_cut_end_hz: float   = 20
+
+    default_raw_length: int = 1409024
+    width_alignment: int    = 32768
+
+    raw_to_mdct_scale: float = 275.47124 # for stereo audio @ -20 lufs
+    mdct_psd_scale: float = 1.1785113
+    mdct_psd_eps: float   = 1e-2
+    mdct_window_len: int = 512
+    mdct_window_func: Literal["sin", "kbd", "vorbis"] = "sin"
+
+    mdct_psd_to_p2m_scale: float = 30.9832693 # for stereo audio @ -20 lufs with midside transform
+    p2m_psd_scale: float = 1.765726368
+    p2m_psd_eps: float   = 1e-2
+    p2m_use_midside_transform: bool = True
+    p2m_block_width: int = 16
+    p2m_window_func: Literal["sin", "kbd", "vorbis"] = "sin"
+    
+    @property
+    def mdct_num_frequencies(self) -> int:
+        return self.mdct_window_len // 2
+    
+    @property
+    def mdct_frame_hop_length(self) -> int:
+        return self.mdct_window_len // 2
+    
+    @property
+    def p2m_num_frequencies(self) -> int:
+        return self.p2m_block_width ** 2 // 4
+    
+    @property
+    def p2m_block_hop_length(self) -> int:
+        return self.p2m_block_width // 2
+
+class MDCT_PSD_Format(DualDiffusionFormat):
+
+    def __init__(self, config: MDCT_PSD_FormatConfig) -> None:
+        super().__init__()
+        self.config = config
+
+        if self.config.p2m_use_midside_transform == True:
+            assert self.config.num_raw_channels == 2, "P2M Mid-side transform requires 2 raw channels"
+
+        mdct_hz = (torch.arange(config.mdct_num_frequencies) + 0.5) * config.sample_rate / config.mdct_window_len
+        self.register_buffer("mdct_hz", mdct_hz)
+        self.register_buffer("mdct_mel_density", get_mel_density(mdct_hz).view(1, 1,-1, 1))
+
+        mdct_window_fn = get_window_fn(config.mdct_window_func)
+        self.mdct = MDCT(win_length=config.mdct_window_len, window_fn=mdct_window_fn, return_complex=True)
+        self.imdct = IMDCT(win_length=config.mdct_window_len, window_fn=mdct_window_fn)
+
+        p2m_window_fn = get_window_fn(config.p2m_window_func)
+        self.p2m = MDCT2(win_length=config.p2m_block_width, window_fn=p2m_window_fn, return_complex=True)
+        self.ip2m = IMDCT2(win_length=config.p2m_block_width, window_fn=p2m_window_fn)
+
+    def _high_pass(self, raw_samples: torch.Tensor) -> torch.Tensor:
+        
+        cutoff_freq = self.config.low_cut_end_hz
+
+        if cutoff_freq <= 0 or (self.config.low_cut_start_hz - cutoff_freq) <= 0:
+            return raw_samples
+        
+        raw_len = raw_samples.shape[-1]
+
+        raw_samples = torch.nn.functional.pad(raw_samples.float(), (raw_len // 2, raw_len // 2), mode="reflect")
+        rfft: torch.Tensor = torch.fft.rfft(raw_samples, dim=-1, norm="ortho")
+        rfft_freq = torch.fft.rfftfreq(raw_samples.shape[-1], d=1/self.config.sample_rate, device=raw_samples.device)
+
+        filter: torch.Tensor = (rfft_freq - cutoff_freq) / (self.config.low_cut_start_hz - cutoff_freq)
+        filter = filter.clip(min=0., max=1.).view(1, 1,-1)
+
+        raw_samples = torch.fft.irfft(rfft * filter, n=raw_samples.shape[-1], dim=-1, norm="ortho")
+        return raw_samples[..., raw_len//2:-raw_len//2]
+    
+    def get_raw_crop_width(self, raw_length: Optional[int] = None) -> int:
+        raw_length = raw_length or self.config.default_raw_length
+        return raw_length // self.config.width_alignment * self.config.width_alignment - self.config.mdct_num_frequencies
+
+    def get_mdct_shape(self, bsz: int = 1, raw_length: Optional[int] = None) -> tuple[int, int, int, int]:
+        raw_length = raw_length or self.config.default_raw_length
+        raw_crop_width = self.get_raw_crop_width(raw_length=raw_length + self.config.mdct_num_frequencies)
+        num_mdct_bins = self.config.mdct_num_frequencies
+        num_mdct_frames = (raw_crop_width + self.config.mdct_num_frequencies) // num_mdct_bins
+        num_channels = self.config.num_raw_channels
+        return (bsz, num_channels, num_mdct_bins, num_mdct_frames,)
+
+    @torch.no_grad()
+    def raw_to_mdct(self, raw_samples: torch.Tensor, random_phase_augmentation: bool = False) -> torch.Tensor:
+
+        raw_samples = self._high_pass(raw_samples)
+        mclt = self.mdct(raw_samples.float())
+
+        if random_phase_augmentation == True:
+            phase_rotation = torch.exp(2j * torch.pi * torch.rand(mclt.shape[0], device=mclt.device)) 
+            mclt *= phase_rotation.view(-1, 1, 1, 1)
+
+        return mclt.real.contiguous(memory_format=self.memory_format) / self.mdct_mel_density * self.config.raw_to_mdct_scale
+    
+    @torch.no_grad()
+    def raw_to_mdct_psd(self, raw_samples: torch.Tensor) -> torch.Tensor:
+
+        raw_samples = self._high_pass(raw_samples)
+        mclt = self.mdct(raw_samples.float())
+
+        return mclt.abs().contiguous(memory_format=self.memory_format) / self.mdct_mel_density * self.config.raw_to_mdct_scale / 2**0.5
+    
+    @torch.no_grad()
+    def mdct_to_raw(self, mdct: torch.Tensor) -> torch.Tensor:
+
+        mdct = mdct * self.mdct_mel_density / self.config.raw_to_mdct_scale
+        raw_samples = self.imdct(mdct).real.contiguous()
+        
+        return raw_samples
+    
+    @torch.no_grad()
+    def scale_mdct_from_psd(self, mdct: torch.Tensor, mdct_psd: torch.Tensor):
+        return mdct / (mdct_psd + self.config.mdct_psd_eps) * self.config.mdct_psd_scale
+    
+    @torch.no_grad()
+    def unscale_mdct_from_psd(self, mdct: torch.Tensor, mdct_psd: torch.Tensor):
+        return mdct * (mdct_psd + self.config.mdct_psd_eps) / self.config.mdct_psd_scale
+
+    @torch.inference_mode()
+    def psd_to_img(self, psd: torch.Tensor) -> torch.Tensor:
+        psd = psd.clip(min=0) ** 0.25
+
+        # if input is a p2m psd reshape it to a valid 2d image
+        if psd.shape[1] == self.config.num_raw_channels * self.config.p2m_num_frequencies:
+            return self._p2m_to_img(psd)
+        else:
+            return tensor_to_img(psd, flip_y=True)
+    
+    # ************* p2m methods *************
+
+    def get_p2m_shape(self, mdct_shape: tuple[int, int, int, int]) -> tuple[int, int, int, int]:
+        bsz = mdct_shape[0]
+        num_channels = mdct_shape[1] * self.config.p2m_num_frequencies
+        h_blocks = mdct_shape[2] // self.config.p2m_block_hop_length + 1
+        w_blocks = mdct_shape[3] // self.config.p2m_block_hop_length + 1
+        return (bsz, num_channels, h_blocks, w_blocks,)
+    
+    @torch.no_grad()
+    def mdct_psd_to_p2m(self, mdct_psd: torch.Tensor) -> torch.Tensor:
+
+        if self.config.p2m_use_midside_transform == True:
+            mdct_psd = torch.stack((mdct_psd[:, 0] + mdct_psd[:, 1], mdct_psd[:, 0] - mdct_psd[:, 1]), dim=1) / 2**0.5
+
+        p2m = self.p2m(mdct_psd) * self.config.mdct_psd_to_p2m_scale
+        p2m = p2m.real.reshape(p2m.shape[0],
+            self.config.num_raw_channels * self.config.p2m_num_frequencies, p2m.shape[4], p2m.shape[5])
+        
+        return p2m.contiguous(memory_format=self.memory_format)
+    
+    @torch.no_grad()
+    def mdct_psd_to_p2m_psd(self, mdct_psd: torch.Tensor) -> torch.Tensor:
+
+        if self.config.p2m_use_midside_transform == True:
+            mdct_psd = torch.stack((mdct_psd[:, 0] + mdct_psd[:, 1], mdct_psd[:, 0] - mdct_psd[:, 1]), dim=1) / 2**0.5
+
+        p2m = self.p2m(mdct_psd) * self.config.mdct_psd_to_p2m_scale
+        p2m_psd = p2m.abs().reshape(p2m.shape[0],
+            self.config.num_raw_channels * self.config.p2m_num_frequencies, p2m.shape[4], p2m.shape[5])
+        
+        return p2m_psd.contiguous(memory_format=self.memory_format)
+    
+    @torch.no_grad()
+    def p2m_to_mdct_psd(self, p2m: torch.Tensor) -> torch.Tensor:
+
+        p2m = p2m.reshape(p2m.shape[0],
+            self.config.num_raw_channels, self.config.p2m_block_hop_length,
+            self.config.p2m_block_hop_length, p2m.shape[2], p2m.shape[3])
+        
+        mdct_psd = self.ip2m(p2m / self.config.mdct_psd_to_p2m_scale)
+        mdct_psd = mdct_psd.real.contiguous(memory_format=self.memory_format)
+
+        if self.config.p2m_use_midside_transform == True:
+            mdct_psd = torch.stack((mdct_psd[:, 0] + mdct_psd[:, 1], mdct_psd[:, 0] - mdct_psd[:, 1]), dim=1) / 2**0.5
+        
+        return mdct_psd
+    
+    @torch.no_grad()
+    def scale_p2m_from_psd(self, p2m: torch.Tensor, p2m_psd: torch.Tensor):
+        return p2m / (p2m_psd + self.config.p2m_psd_eps) * self.config.p2m_psd_scale
+
+    @torch.no_grad()
+    def unscale_p2m_from_psd(self, p2m: torch.Tensor, p2m_psd: torch.Tensor):
+        return p2m * (p2m_psd + self.config.p2m_psd_eps) / self.config.p2m_psd_scale
+    
+    @torch.inference_mode()
+    def _p2m_to_img(self, p2m: torch.Tensor) -> torch.Tensor:
+
+        p2m = p2m.reshape(p2m.shape[0], self.config.num_raw_channels,
+            self.config.p2m_block_hop_length, self.config.p2m_block_hop_length, p2m.shape[2], p2m.shape[3])
+        p2m = p2m.transpose(3, 4).flip(dims=(3,))
+        p2m = p2m.reshape(p2m.shape[0], p2m.shape[1], p2m.shape[2] * p2m.shape[3], p2m.shape[4] * p2m.shape[5])
+
+        return tensor_to_img(p2m)
---
+++ src/tests/mdct_psd.py
@@ -0,0 +1,170 @@
+# MIT License
+#
+# Copyright (c) 2023 Christopher Friesen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from utils import config
+
+from dataclasses import dataclass
+import os
+import random
+
+import torch
+
+from modules.formats.mdct_psd import MDCT_PSD_Format, MDCT_PSD_FormatConfig
+from training.trainer import TrainLogger as StatLogger
+from utils.dual_diffusion_utils import (
+    init_cuda, save_audio, load_audio,
+    save_img, get_audio_info, dict_str, tensor_info_str
+)
+
+
+@dataclass
+class MDCT_PSD_Format_TestConfig:
+
+    device: str
+    save_output: bool
+    test_sample_verbose: bool
+    add_random_test_samples: int
+    test_samples: list[str]
+
+    format_config: MDCT_PSD_FormatConfig
+
+@torch.inference_mode()
+def mdct_psd_format_test() -> None:
+
+    torch.manual_seed(0)
+    random.seed()
+
+    cfg: MDCT_PSD_Format_TestConfig = config.load_config(MDCT_PSD_Format_TestConfig,
+        os.path.join(config.CONFIG_PATH, "tests", "mdct_psd_format.json"))
+    format: MDCT_PSD_Format = MDCT_PSD_Format(cfg.format_config).to(cfg.device)
+
+    print("Format config:")
+    print(dict_str(format.config.__dict__))
+
+    dataset_path = config.DATASET_PATH
+    test_samples = cfg.test_samples
+
+    if cfg.add_random_test_samples > 0:
+        train_samples = config.load_json(os.path.join(config.DATASET_PATH, "train.jsonl"))
+        test_samples += [sample["file_name"] for sample in random.sample(train_samples, cfg.add_random_test_samples)]
+
+    output_path = os.path.join(config.DEBUG_PATH, "mdct_psd_format_test")
+    os.makedirs(output_path, exist_ok=True)
+
+    mdct_mel_density_scaling = 1 / format.mdct_mel_density.flatten().cpu()
+    print("\nMDCT mel-density scaling coefficients:")
+    print(mdct_mel_density_scaling)
+    mdct_mel_density_scaling.numpy().tofile(os.path.join(output_path, "mdct_mel_density_scaling.raw"))
+    mdct_avg_bin_std = torch.zeros_like(format.mdct_mel_density.flatten())
+
+    stat_logger = StatLogger()
+    print(f"\nNum test_samples: {len(test_samples)}\n")
+
+    for i, filename in enumerate(test_samples):
+        
+        print(f"file {i+1}/{len(test_samples)}: {filename}")
+
+        file_path = os.path.join(dataset_path, filename)
+        if os.path.isfile(file_path) == False:
+            file_path = os.path.join(config.DEBUG_PATH, filename)
+
+        raw_length = min(get_audio_info(file_path).frames, cfg.format_config.default_raw_length)
+        crop_width = format.get_raw_crop_width(raw_length)
+
+        raw_sample = load_audio(file_path, count=crop_width).unsqueeze(0).to(cfg.device)
+
+        mdct = format.raw_to_mdct(raw_sample)
+        mdct_psd = format.raw_to_mdct_psd(raw_sample)
+        mdct_scaled = format.scale_mdct_from_psd(mdct, mdct_psd)
+        mdct_unscaled = format.unscale_mdct_from_psd(mdct_scaled, mdct_psd)
+        raw_sample_mdct = format.mdct_to_raw(mdct)
+
+        p2m = format.mdct_psd_to_p2m(mdct_psd)
+        p2m_psd = format.mdct_psd_to_p2m_psd(mdct_psd)
+        p2m_scaled = format.scale_p2m_from_psd(p2m, p2m_psd)
+        p2m_unscaled = format.unscale_p2m_from_psd(p2m_scaled, p2m_psd)
+        mdct_psd_p2m = format.p2m_to_mdct_psd(p2m)
+
+        mdct_avg_bin_std += mdct.std(dim=(0, 1, 3)) / len(test_samples)
+        stat_logger.add_logs({
+            "raw_sample_std": raw_sample.std(),
+            "raw_sample_mdct_std": raw_sample_mdct.std(),
+            "mdct_std": mdct.std(),
+            "mdct_scaled_std": mdct_scaled.std(),
+            "mdct_unscaled_std": mdct_unscaled.std(),
+            "mdct_psd_std": mdct_psd.std(),
+            "mdct_psd_p2m_std": mdct_psd_p2m.std(),
+            "p2m_std": p2m.std(),
+            "p2m_scaled_std": p2m_scaled.std(),
+            "p2m_unscaled_std": p2m_unscaled.std(),
+            "p2m_psd_std": p2m_psd.std(),
+        })
+
+        if cfg.test_sample_verbose == True:
+            print("raw_sample:", tensor_info_str(raw_sample))
+            print("raw_sample_mdct:", tensor_info_str(raw_sample_mdct))
+            print("mdct:", tensor_info_str(mdct), f"(target shape: {format.get_mdct_shape(raw_length=raw_length)}")
+            print("mdct_psd:", tensor_info_str(mdct_psd))
+            print("mdct_psd_p2m:", tensor_info_str(mdct_psd_p2m))
+            print("p2m:", tensor_info_str(p2m), f"(target shape: {format.get_p2m_shape(mdct.shape)}")
+            print("p2m_psd:", tensor_info_str(p2m_psd), "\n")
+
+        if cfg.save_output == False:
+            continue
+
+        filename = os.path.splitext(os.path.basename(filename))[0]
+
+        raw_sample_output_path = os.path.join(output_path, f"{filename}.flac")
+        save_audio(raw_sample.squeeze(0), cfg.format_config.sample_rate, raw_sample_output_path, target_lufs=None)
+        print(f"Saved raw_sample to {raw_sample_output_path}")
+
+        mdct_output_path = os.path.join(output_path, f"{filename}_mdct.flac")
+        save_audio(raw_sample_mdct.squeeze(0), cfg.format_config.sample_rate, mdct_output_path, target_lufs=None)
+        print(f"Saved raw_sample_mdct to {mdct_output_path}")
+
+        mdct_psd_output_path = os.path.join(output_path, f"{filename}_mdct_psd.png")
+        save_img(format.psd_to_img(mdct_psd), mdct_psd_output_path)
+        print(f"Saved mdct_psd img to {mdct_psd_output_path}")
+
+        mdct_psd_p2m_output_path = os.path.join(output_path, f"{filename}_mdct_psd_p2m.png")
+        save_img(format.psd_to_img(mdct_psd_p2m), mdct_psd_p2m_output_path)
+        print(f"Saved mdct_psd_p2m img to {mdct_psd_p2m_output_path}")
+
+        p2m_psd_output_path = os.path.join(output_path, f"{filename}_p2m_psd.png")
+        save_img(format.psd_to_img(p2m_psd), p2m_psd_output_path)
+        print(f"Saved p2m_psd img to {p2m_psd_output_path}\n")
+
+        p2m_scaled_output_path = os.path.join(output_path, f"{filename}_p2m_scaled.png")
+        save_img(format._p2m_to_img(p2m_scaled), p2m_scaled_output_path)
+        print(f"Saved mdct_psd img to {p2m_scaled_output_path}")
+
+    print(f"\nAverage MDCT bin std (std variance: {mdct_avg_bin_std.var().item()}):")
+    print(mdct_avg_bin_std)
+    mdct_avg_bin_std.cpu().numpy().tofile(os.path.join(output_path, "mdct_avg_bin_std.raw"))
+
+    print("\nAverage stats:")
+    print(dict_str(stat_logger.get_logs()))
+
+if __name__ == "__main__":
+
+    init_cuda()
+    mdct_psd_format_test()
