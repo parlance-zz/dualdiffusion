4b61c145dd095088bc889a8f365d4673461153a8
parlance-zz <parlance@fifth-harmonic.com>
2026-03-01 19:25:54 -0700

    update ddec p5 trainer

--- src/training/module_trainers/ddec_p5_trainer.py
+++ src/training/module_trainers/ddec_p5_trainer.py
@@ -21,19 +21,18 @@
 # SOFTWARE.
 
 from dataclasses import dataclass
-from typing import Literal, Union, Optional, Any
+from typing import Union, Optional, Any
 
 import torch
 
 from training.trainer import DualDiffusionTrainer
 from training.module_trainers.module_trainer import ModuleTrainer, ModuleTrainerConfig
 from training.module_trainers.unet_trainer_p4 import UNetTrainerConfig, UNetTrainer
-from training.sigma_sampler import SigmaSamplerConfig, SigmaSampler
 from modules.daes.dae_edm2_p5 import DAE
 from modules.unets.unet_edm2_p5_ddec import UNet
+from modules.unets.unet_edm2_p5 import UNet as UNet_LDM
 from modules.formats.ms_mdct_dual_3 import MS_MDCT_DualFormat
 from modules.mp_tools import normalize
-from utils.dual_diffusion_utils import dict_str
 
 
 @torch.no_grad()
@@ -48,12 +47,15 @@ def random_stereo_augmentation(x: torch.Tensor) -> torch.Tensor:
 @dataclass
 class DiffusionDecoder_Trainer_Config(ModuleTrainerConfig):
 
-    latents_noise: dict[str, Any]
-    ddec: dict[str, Any]
+    ddecm: dict[str, Any]
+    ddecp: dict[str, Any]
+    unet: dict[str, Any]
 
     kl_loss_weight: float = 1e-2
-    kl_warmup_steps: int  = 300
-    kl_loss_mode: Literal["per_channel", "global"] = "per_channel"
+    kl_warmup_steps: int  = 150
+
+    decoder_loss_multiplier: float = 3
+    add_latents_noise: float = 0.08
 
     random_stereo_augmentation: bool = True
     random_phase_augmentation: bool  = True
@@ -69,50 +71,58 @@ class DiffusionDecoder_Trainer(ModuleTrainer):
         self.trainer = trainer
         self.logger = trainer.logger
 
-        self.ddec: UNet = trainer.get_train_module("ddec")
+        self.unet: UNet_LDM = trainer.get_train_module("unet")
+        self.ddecp: UNet = trainer.get_train_module("ddecp")
+        self.ddecm: UNet = trainer.get_train_module("ddecm")
         self.dae: DAE = trainer.get_train_module("dae")
+
+        assert self.ddecp is not None and self.ddecm is not None
+        
+        if self.dae is None:
+            assert self.dae.config.last_global_step > 0 and self.unet is None
+            self.dae = trainer.pipeline.dae.to(device=trainer.accelerator.device, dtype=torch.bfloat16).requires_grad_(False)
+            self.train_dae = False
+        else:
+            assert self.unet is not None
+            self.train_dae = True
+        
         self.format: MS_MDCT_DualFormat = trainer.pipeline.format.to(self.trainer.accelerator.device)
 
         if trainer.config.enable_model_compilation:
-            self.ddec.compile(**trainer.config.compile_params)
-            self.format.compile(**trainer.config.compile_params)
+            self.ddecp.compile(**trainer.config.compile_params)
+            self.ddecm.compile(**trainer.config.compile_params)
             self.dae.compile(**trainer.config.compile_params)
+            self.format.compile(**trainer.config.compile_params)
 
-        assert config.kl_loss_mode in ("per_channel", "global")
+            if self.unet is not None:
+                self.unet.compile(**trainer.config.compile_params)
 
         self.logger.info(f"Training modules: {trainer.config.train_modules}")
-        self.logger.info(f"KL loss weight: {self.config.kl_loss_weight} KL warmup steps: {self.config.kl_warmup_steps}")
-        self.logger.info(f"KL loss mode: {self.config.kl_loss_mode}")
+        if self.train_dae == True:
+            self.logger.info(f"KL loss weight: {self.config.kl_loss_weight} KL warmup steps: {self.config.kl_warmup_steps}")
+            self.logger.info(f"Decoder loss multiplier: {config.decoder_loss_multiplier}")
+            self.logger.info(f"Add latents noise: {config.add_latents_noise}")
         self.logger.info(f"Crop edges: {self.config.crop_edges}")
 
         if self.config.random_stereo_augmentation == True:
             self.logger.info("Using random stereo augmentation")
         else: self.logger.info("Random stereo augmentation is disabled")
 
-        self.logger.info("DDEC trainer:")
-        self.ddec_trainer = UNetTrainer(UNetTrainerConfig(**config.ddec), trainer, self.ddec, "ddec")
-
-        if config.latents_noise is not None:
-            latents_sigma_sampler_config = SigmaSamplerConfig(**config.latents_noise)
-            self.latents_sigma_sampler = SigmaSampler(latents_sigma_sampler_config)
-            self.logger.info("Latents Noise SigmaSamplerConfig:")
-            self.logger.info(dict_str(latents_sigma_sampler_config.__dict__))
-        else:
-            self.latents_sigma_sampler = None
-            self.logger.info(f"Latents Noise is disabled")
+        self.logger.info("DDEC-P trainer:")
+        self.ddecp_trainer = UNetTrainer(UNetTrainerConfig(**config.ddecp), trainer, self.ddecp, "ddecp")
+        self.logger.info("DDEC-M trainer:")
+        self.ddecm_trainer = UNetTrainer(UNetTrainerConfig(**config.ddecm), trainer, self.ddecm, "ddecm")
+        self.logger.info("UNet trainer:")
+        self.unet_trainer = UNetTrainer(UNetTrainerConfig(**config.unet), trainer, self.unet, "unet")
 
     @torch.no_grad()
     def init_batch(self, validation: bool = False) -> Optional[dict[str, Union[torch.Tensor, float]]]:
         
-        logs = self.ddec_trainer.init_batch(validation)
+        self.ddecp_trainer.init_batch(validation)
+        self.ddecm_trainer.init_batch(validation)
+        self.unet_trainer.init_batch(validation)
 
-        if self.latents_sigma_sampler is not None:
-            self.global_sigma = self.latents_sigma_sampler.sample(self.trainer.total_batch_size, device=self.trainer.accelerator.device)
-            self.global_sigma = self.trainer.accelerator.gather(self.global_sigma.unsqueeze(0))[0]
-        else:
-            self.global_sigma = None
-
-        return logs
+        return None
     
     def train_batch(self, batch: dict) -> Optional[dict[str, Union[torch.Tensor, float]]]:
 
@@ -127,78 +137,74 @@ class DiffusionDecoder_Trainer(ModuleTrainer):
         else:
             raw_samples = batch["audio"]
 
-        logs = {"loss": torch.zeros(raw_samples.shape[0], device=self.trainer.accelerator.device)}
+        mdct_phase, mdct_psd = self.format.raw_to_mdct_phase_psd(raw_samples, random_phase_augmentation=self.config.random_phase_augmentation)
+        mdct_phase = mdct_phase[..., self.config.crop_edges:-self.config.crop_edges]
+        mdct_psd = mdct_psd[..., self.config.crop_edges:-self.config.crop_edges]
 
-        input_mdct_phase, _ = self.format.raw_to_mdct_phase_psd(raw_samples, random_phase_augmentation=self.config.random_phase_augmentation)
-        input_mdct_phase = input_mdct_phase[..., self.config.crop_edges:-self.config.crop_edges]
         input_mel_spec = self.format.raw_to_mel_spec(raw_samples)
         input_mel_spec = input_mel_spec[..., self.config.crop_edges:-self.config.crop_edges]
 
-        logs.update({
-            "io_stats/mdct_phase_var": input_mdct_phase.var(dim=(1,2,3)),
-            "io_stats/mdct_phase_mean": input_mdct_phase.mean(dim=(1,2,3)),
-            "io_stats/mel_spec_var": input_mel_spec.var(dim=(1,2,3)),
-            "io_stats/mel_spec_mean": input_mel_spec.mean(dim=(1,2,3)),
-            "io_stats/mdct_phase_range": input_mdct_phase.amax() - input_mdct_phase.amin()
-        })
-
-        # get the noise level for this sub-batch from the pre-calculated whole-batch sigma (required for stratified sampling)
-        if self.latents_sigma_sampler is not None:
-            device_bsz = self.trainer.config.device_batch_size
-            local_sigma = self.global_sigma[self.trainer.accelerator.process_index::self.trainer.accelerator.num_processes]
-            latents_batch_sigma = local_sigma[self.trainer.accum_step * device_bsz:(self.trainer.accum_step+1) * device_bsz]
+        if self.train_dae == True:
+            latents, ddec_cond = self.trainer.get_ddp_module(self.dae)(input_mel_spec, audio_embeddings, self.config.add_latents_noise)
         else:
-            latents_batch_sigma = None
-        
-        latents, ddec_cond, pre_norm_latents = self.trainer.get_ddp_module(self.dae)(
-            input_mel_spec, audio_embeddings, latents_sigma=latents_batch_sigma)
-
-        ddec_cond: torch.Tensor = ddec_cond.float()
+            latents, ddec_cond = self.dae(input_mel_spec, audio_embeddings, self.config.add_latents_noise)
+            ddec_cond = ddec_cond.detach()
+            
         latents: torch.Tensor = latents.float()
-        pre_norm_latents: torch.Tensor = pre_norm_latents.float()
 
-        if self.config.kl_loss_mode == "global":
-            pre_norm_latents_var = pre_norm_latents.pow(2).mean() + 1e-20
-            var_kl = pre_norm_latents_var - 1 - pre_norm_latents_var.log()
-            kl_loss_latents = var_kl.mean() + 0.5 * pre_norm_latents.mean().pow(2).mean()
-        else:
-            pre_norm_latents_var = pre_norm_latents.pow(2).mean(dim=(0,2,3)) + 1e-20
-            var_kl = pre_norm_latents_var - 1 - pre_norm_latents_var.log()
-            kl_loss_latents = var_kl.mean() + 0.5 * pre_norm_latents.mean(dim=(0,2,3)).pow(2).mean()
-
-        kl_loss_latents = kl_loss_latents.expand(latents.shape[0]) # needed for per-sample logging
+        latents_var = latents.pow(2).mean(dim=(0,2,3)) + 1e-20
+        var_kl = latents_var - 1 - latents_var.log()
+        kl_loss = var_kl.mean() + latents.mean(dim=(0,2,3)).square().mean()
+        kl_loss = kl_loss.expand(latents.shape[0]) # needed for per-sample logging
 
         kl_loss_weight = self.config.kl_loss_weight
         if self.trainer.global_step < self.config.kl_warmup_steps:
             kl_loss_weight *= self.trainer.global_step / self.config.kl_warmup_steps
         
-        logs["loss"] = logs["loss"] + kl_loss_latents * kl_loss_weight
-
-        logs.update({
+        logs = {
+            "loss": kl_loss * kl_loss_weight if self.train_dae == True else torch.zeros_like(kl_loss),
             "io_stats/ddec_cond_var": ddec_cond.var(dim=(1,2,3)),
             "io_stats/ddec_cond_mean": ddec_cond.mean(dim=(1,2,3)),
-            "io_stats/prenorm_latents_var": pre_norm_latents.var(dim=(1,2,3)).detach(),
             "io_stats/latents_var": latents.var(dim=(1,2,3)).detach(),
             "io_stats/latents_mean": latents.mean(dim=(1,2,3)).detach(),
-            "io_stats/latents_sigma": latents_batch_sigma.detach() if latents_batch_sigma is not None else 0,
-            "loss/kl_latents": kl_loss_latents.detach(),
-            "loss_weight/kl_latents": kl_loss_weight,
-        })
 
-        logs.update(self.ddec_trainer.train_batch(input_mdct_phase, audio_embeddings, ddec_cond))
-        logs["loss"] = logs["loss"] + logs["loss/ddec"]
+            "io_stats_ddecp/mdct_phase_var": mdct_phase.var(dim=(1,2,3)),
+            "io_stats_ddecm/mdct_psd_var": mdct_psd.var(dim=(1,2,3)),
+            "io_stats_ddecm/mdct_psd_mean": mdct_psd.mean(dim=(1,2,3)),
+
+            "loss/kl_latents": kl_loss.detach(),
+            "loss_weight/kl_latents": kl_loss_weight,
+        }
+
+        noise = torch.randn_like(mdct_psd)
+        perturb_noise = torch.randn_like(mdct_psd)
+
+        logs.update(self.ddecp_trainer.train_batch(mdct_phase, audio_embeddings, ddec_cond, noise=noise, perturb_noise=perturb_noise))
+        logs.update(self.ddecm_trainer.train_batch(mdct_psd, audio_embeddings, ddec_cond, noise=noise, perturb_noise=perturb_noise))
+        logs["loss"] = logs["loss"] + (logs["loss/ddecp"] + logs["loss/ddecm"]) * (self.config.decoder_loss_multiplier / 2)
+
+        logs.update(self.unet_trainer.train_batch(latents, audio_embeddings))
+        logs["loss"] = logs["loss"] + logs["loss/unet"]
+
+        dynamic_range_ddecm = mdct_psd.amax(dim=(1,2,3)) - mdct_psd.amin(dim=(1,2,3))
+        logs["io_stats_ddecm/dynamic_range"] = dynamic_range_ddecm
+        dynamic_range_ddecp = mdct_phase.amax(dim=(1,2,3)) - mdct_phase.amin(dim=(1,2,3))
+        logs["io_stats_ddecp/dynamic_range"] = dynamic_range_ddecp
 
         if self.trainer.config.enable_debug_mode == True:
-            print("input_mdct_phase.shape:", input_mdct_phase.shape)
-            print("input_mel_spec.shape:", input_mel_spec.shape)
+            print("mdct_phase.shape:", mdct_phase.shape)
+            print("mdct_psd.shape:", mdct_psd.shape)
             print("ddec_cond.shape:", ddec_cond.shape)
             print("latents.shape:", latents.shape)
-            print("pre_norm_latents.shape:", pre_norm_latents.shape)
-            if latents_batch_sigma is not None:
-                print("latents_batch_sigma.shape:", latents_batch_sigma.shape)
+            print("input_mel_spec.shape", input_mel_spec.shape)
 
         return logs
-    
+      
     @torch.no_grad()
     def finish_batch(self) -> Optional[dict[str, Union[torch.Tensor, float]]]:
-        return self.ddec_trainer.finish_batch()
+
+        logs = {}
+        logs.update(self.ddecp_trainer.finish_batch())
+        logs.update(self.ddecm_trainer.finish_batch())
+
+        return logs
