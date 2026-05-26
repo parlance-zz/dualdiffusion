b2d367de1bdc55e4e2fc3bfc94b1051e1f243310
parlance-zz <parlance@fifth-harmonic.com>
2026-05-25 22:50:57 -0600

    remove recon_loss_logvar from dae q5

--- src/modules/daes/dae_edm2_q5.py
+++ src/modules/daes/dae_edm2_q5.py
@@ -299,7 +299,7 @@ class DAE(DualDiffusionDAE):
         dec_channels = [config.model_channels * m for m in config.channel_mult_dec]
 
         self.latents_stats_tracker = LatentStatsTracker(config.latent_channels, static_scale=config.static_latents_scale)
-        self.recon_loss_logvar = torch.nn.Parameter(torch.zeros([]))
+        #self.recon_loss_logvar = torch.nn.Parameter(torch.zeros([]))
 
         # encoder
         self.conv_psd_in = torch.nn.ModuleDict()
